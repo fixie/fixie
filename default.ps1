d861ff8fb6bc5621c7066a855fa96733cbe7eebf
Patrick Lioi <patrick@headspring.com>
2013-03-04 21:24:11 -0600

    Build script creates CommonAssemblyInfo.cs with version number.  All projects share CommonAssemblyInfo.cs.

--- default.ps1
+++ default.ps1
@@ -1,9 +1,14 @@
 Framework '4.0'
 
 properties {
-  $project = 'Fixie'
-  $configuration = 'Release'
-  $src = resolve-path '.\src'
+    $project = 'Fixie'
+    $birthYear = 2013
+    $maintainers = "Patrick Lioi"
+
+    $configuration = 'Release'
+    $src = resolve-path '.\src'
+    $build = if ($env:build_number -ne $NULL) { $env:build_number } else { '0' }
+    $version = [IO.File]::ReadAllText('.\VERSION.txt') + '.' + $build
 }
 
 task default -depends Test
@@ -22,7 +27,24 @@ task Test -depends Compile {
     }
 }
 
-task Compile {
+task Compile -depends CommonAssemblyInfo {
   exec { msbuild /t:clean /v:q /nologo /p:Configuration=$configuration $src\$project.sln }
   exec { msbuild /t:build /v:q /nologo /p:Configuration=$configuration $src\$project.sln }
+}
+
+task CommonAssemblyInfo {
+    $date = Get-Date
+    $year = $date.Year
+    $copyrightSpan = if ($year -eq $birthYear) { $year } else { "$birthYear-$year" }
+    $copyright = "Copyright (c) $copyrightSpan $maintainers"
+
+"using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct(""$project"")]
+[assembly: AssemblyVersion(""$version"")]
+[assembly: AssemblyFileVersion(""$version"")]
+[assembly: AssemblyCopyright(""$copyright"")]
+[assembly: AssemblyConfiguration(""$configuration"")]" | out-file "$src\CommonAssemblyInfo.cs" -encoding "ASCII"
 }
---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Fixie")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyCopyright("Copyright (c) 2013 Patrick Lioi")]
+[assembly: AssemblyConfiguration("Release")]

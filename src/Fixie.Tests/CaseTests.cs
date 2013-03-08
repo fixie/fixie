9c7e6a88e502c3c77b5ec680425a441601c74e25
Patrick Lioi <patrick.lioi@gmail.com>
2013-03-07 20:19:13 -0600

    When the Case for a given method is executed, the method is invoked.

---
+++ src/Fixie.Tests/CaseTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Fixie.Tests
+{
+    [TestFixture]
+    public class CaseTests
+    {
+        [Test]
+        public void ShouldInvokeTheGivenMethodWhenExecuted()
+        {
+            var fixtureClass = typeof(SampleFixture);
+            var method = fixtureClass.GetMethod("Method", BindingFlags.Public | BindingFlags.Instance);
+
+            var c = new Case(fixtureClass, method);
+
+            bool threw = false;
+
+            try
+            {
+                c.Execute();
+            }
+            catch (TargetInvocationException expected)
+            {
+                threw = true;
+                expected.InnerException.ShouldBeTypeOf<MethodInvokedException>();
+            }
+
+            threw.ShouldBe(true);
+        }
+
+        class MethodInvokedException : Exception
+        {
+        }
+
+        class SampleFixture
+        {
+            public void Method()
+            {
+                throw new MethodInvokedException();
+            }
+        }
+    }
+}
---
+++ src/Fixie/Case.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace Fixie
+{
+    public class Case
+    {
+        private readonly Type fixtureClass;
+        private readonly MethodInfo method;
+
+        public Case(Type fixtureClass, MethodInfo method)
+        {
+            this.fixtureClass = fixtureClass;
+            this.method = method;
+        }
+
+        public void Execute()
+        {
+            var instance = Activator.CreateInstance(fixtureClass);
+
+            method.Invoke(instance, null);
+        }
+    }
+}

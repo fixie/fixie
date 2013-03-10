c2a7545041e3095162d62e4693a1bb483b5f6410
Patrick Lioi <patrick@headspring.com>
2013-03-10 16:20:58 -0500

    A CaseResult represents the pass or failure of a single Case.  Failed cases include an explanatory Exception.

---
+++ src/Fixie/CaseResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fixie
+{
+    public class CaseResult
+    {
+        static readonly Lazy<CaseResult> LazyPass = new Lazy<CaseResult>(() => new CaseResult(true, null));
+
+        public static CaseResult Pass()
+        {
+            return LazyPass.Value;
+        }
+
+        public static CaseResult Fail(Exception exception)
+        {
+            return new CaseResult(false, exception);
+        }
+
+        CaseResult(bool passed, Exception exception)
+        {
+            Passed = passed;
+            Exception = exception;
+        }
+
+        public bool Passed { get; private set; }
+
+        public Exception Exception { get; private set; }
+    }
+}

60c5a036c434734e9dc12e62c9b486b9d45c6814
Patrick Lioi <patrick.lioi@gmail.com>
2013-03-30 14:40:38 -0500

    A TypeFilter allows for declarative filtering of a collection of Types.

---
+++ src/Fixie.Tests/TypeFilterTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xunit;
+
+namespace Fixie.Tests
+{
+    public class TypeFilterTests
+    {
+        readonly Type[] candidateTypes;
+
+        public TypeFilterTests()
+        {
+            candidateTypes = new[]
+            {
+                typeof(Decimal),
+                typeof(AbstractClass),
+                typeof(DateTime),
+                typeof(DefaultConstructor),
+                typeof(NoDefaultConstructor),
+                typeof(String),
+                typeof(Interface)
+            };
+        }
+
+        [Fact]
+        public void ShouldIncludeAllTypesByDefault()
+        {
+            new TypeFilter()
+                .Filter(candidateTypes)
+                .ShouldEqual(candidateTypes);
+        }
+
+        [Fact]
+        public void ShouldFilterByAllSpecifiedConditions()
+        {
+            new TypeFilter()
+                .Where(type => type.Namespace == "System")
+                .Where(type => type.Name.StartsWith("D"))
+                .Filter(candidateTypes)
+                .ShouldEqual(typeof(Decimal), typeof(DateTime));
+        }
+
+        [Fact]
+        public void CanFilterToConcreteClasses()
+        {
+            new TypeFilter()
+                .ConcreteClasses()
+                .Filter(candidateTypes)
+                .ShouldEqual(typeof(DefaultConstructor), typeof(NoDefaultConstructor), typeof(String));
+        }
+
+        [Fact]
+        public void CanFilterToClassesWithDefaultConstructors()
+        {
+            new TypeFilter()
+                .HasDefaultConstructor()
+                .Filter(candidateTypes)
+                .ShouldEqual(typeof(DefaultConstructor));
+        }
+
+        [Fact]
+        public void CanFilterByTypeNameSuffix()
+        {
+            new TypeFilter()
+                .NameEndsWith("Constructor")
+                .Filter(candidateTypes)
+                .ShouldEqual(typeof(DefaultConstructor), typeof(NoDefaultConstructor));
+        }
+
+        abstract class AbstractClass { }
+        class DefaultConstructor { }
+        class NoDefaultConstructor { public NoDefaultConstructor(int arg) { } }
+        interface Interface { }
+    }
+}
---
+++ src/Fixie/TypeFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fixie
+{
+    public class TypeFilter
+    {
+        readonly List<Func<Type, bool>> conditions;
+
+        public TypeFilter()
+        {
+            conditions = new List<Func<Type, bool>>();
+        }
+
+        public TypeFilter Where(Func<Type, bool> condition)
+        {
+            conditions.Add(condition);
+            return this;
+        }
+
+        public TypeFilter ConcreteClasses()
+        {
+            return Where(type => type.IsClass && !type.IsAbstract);
+        }
+
+        public TypeFilter HasDefaultConstructor()
+        {
+            return Where(type => type.GetConstructor(Type.EmptyTypes) != null);
+        }
+
+        public TypeFilter NameEndsWith(string suffix)
+        {
+            return Where(type => type.Name.EndsWith(suffix));
+        }
+
+        public IEnumerable<Type> Filter(IEnumerable<Type> candidates)
+        {
+            return candidates.Where(IsMatch);
+        }
+
+        private bool IsMatch(Type candidate)
+        {
+            return conditions.All(condition => condition(candidate));
+        }
+    }
+}

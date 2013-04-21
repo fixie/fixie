1649a3265b45932fe4b60f0ef4a0272233691b1e
Patrick Lioi <patrick.lioi@gmail.com>
2013-04-20 20:50:56 -0500

    ReflectionExtensions provides common reflection helpers for MethodInfo objects.

---
+++ src/Fixie.Tests/ReflectionExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Should;
+using Xunit;
+
+namespace Fixie.Tests
+{
+    public class ReflectionExtensionsTests
+    {
+        [Fact]
+        public void CanDetectVoidReturnType()
+        {
+            Method("ReturnsVoid").Void().ShouldBeTrue();
+            Method("ReturnsInt").Void().ShouldBeFalse();
+        }
+
+        [Fact]
+        public void CanDetectMethodAttributes()
+        {
+            Method("ReturnsVoid").Has<FactAttribute>().ShouldBeFalse();
+            Method("ReturnsInt").Has<FactAttribute>().ShouldBeFalse();
+            Method("CanDetectMethodAttributes").Has<FactAttribute>().ShouldBeTrue();
+        }
+
+        [Fact]
+        public void CanDetectAsyncDeclarations()
+        {
+            Method("ReturnsVoid").Async().ShouldBeFalse();
+            Method("ReturnsInt").Async().ShouldBeFalse();
+            Method("Async").Async().ShouldBeTrue();
+        }
+
+        void ReturnsVoid() { }
+        int ReturnsInt() { return 0; }
+        async Task Async() { await Task.Run(() => { }); }
+
+        static MethodInfo Method(string name)
+        {
+            return typeof(ReflectionExtensionsTests).GetMethod(name, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        }
+    }
+}
---
+++ src/Fixie/ReflectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Fixie
+{
+    public static class ReflectionExtensions
+    {
+        public static bool Void(this MethodInfo method)
+        {
+            return method.ReturnType == typeof(void);
+        }
+
+        public static bool Has<TAttribute>(this MethodInfo method) where TAttribute : Attribute
+        {
+            return method.GetCustomAttributes<TAttribute>(false).Any();
+        }
+
+        public static bool Async(this MethodInfo method)
+        {
+            return method.Has<AsyncStateMachineAttribute>();
+        }
+    }
+}

754af5e9c14bcb9ad55ce70d7f69ebdb84c26c35
Patrick Lioi <patrick.lioi@gmail.com>
2013-05-12 15:58:25 -0500

    Add Invoke : FixtureCommand to invoke a given MethodInfo, unwrapping TargetInvocationExceptions in order to report the original exception.

---
+++ src/Fixie.Tests/InvokeTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Should;
+
+namespace Fixie.Tests
+{
+    public class InvokeTests
+    {
+        int invocationCount = 0;
+        readonly ExceptionList exceptions = new ExceptionList();
+
+        public void ShouldInvokeTheGivenMethod()
+        {
+            var invoke = new Invoke(Method("Pass"));
+            invoke.Execute(this, exceptions);
+
+            invocationCount.ShouldEqual(1);
+            exceptions.Count.ShouldEqual(0);
+        }
+
+        public void ShouldLogExceptionWhenTheGivenMethodCannotBeInvoked()
+        {
+            var invoke = new Invoke(Method("CannotInvoke"));
+            invoke.Execute(this, exceptions);
+            
+            invocationCount.ShouldEqual(0);
+            exceptions.Count.ShouldEqual(1);
+
+            var exception = exceptions.ToArray().Single();
+            exception.GetType().Name.ShouldEqual("TargetParameterCountException");
+            exception.Message.ShouldEqual("Parameter count mismatch.");
+        }
+
+        public void ShouldLogOriginalExceptionWhenTheGivenMethodThrows()
+        {
+            var invoke = new Invoke(Method("Fail"));
+            invoke.Execute(this, exceptions);
+
+            invocationCount.ShouldEqual(1);
+            exceptions.Count.ShouldEqual(1);
+
+            var exception = exceptions.ToArray().Single();
+            exception.GetType().Name.ShouldEqual("FailureException");
+            exception.Message.ShouldEqual("Exception of type 'Fixie.Tests.InvokeTests+FailureException' was thrown.");
+        }
+
+        void Pass()
+        {
+            invocationCount++;
+        }
+
+        void CannotInvoke(int argument)
+        {
+            invocationCount++;
+        }
+
+        void Fail()
+        {
+            invocationCount++;
+            throw new FailureException();
+        }
+
+        class FailureException : Exception { }
+
+        static MethodInfo Method(string name)
+        {
+            return typeof(InvokeTests).GetMethod(name, BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+    }
+}
---
+++ src/Fixie/Invoke.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace Fixie
+{
+    public class Invoke : FixtureCommand
+    {
+        readonly MethodInfo method;
+
+        public Invoke(MethodInfo method)
+        {
+            this.method = method;
+        }
+
+        public void Execute(object fixtureInstance, ExceptionList exceptions)
+        {
+            try
+            {
+                method.Invoke(fixtureInstance, null);
+            }
+            catch (TargetInvocationException ex)
+            {
+                exceptions.Add(ex.InnerException);
+            }
+            catch (Exception ex)
+            {
+                exceptions.Add(ex);
+            }
+        }
+    }
+}

a9a358e1b3c27e308a4cf27d5780e21568cb40d4
Patrick Lioi <patrick.lioi@gmail.com>
2013-05-25 14:29:11 -0500

    xUnit sample test lifecycle constructs one instance per test, while preparing one instance of fixture-wide state for every inherited IUseFixture<T>.

--- src/Fixie.Samples/xUnitStyle/CalculatorTests.cs
+++ src/Fixie.Samples/xUnitStyle/CalculatorTests.cs
@@ -1,26 +1,117 @@
-﻿using Should;
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Should;
 
 namespace Fixie.Samples.xUnitStyle
 {
-    public class CalculatorTests
+    public class CalculatorTests : IUseFixture<FixtureData>, IUseFixture<DisposableFixtureData>, IDisposable
     {
         readonly Calculator calculator;
+        readonly StringBuilder log;
+
+        bool executedAddTest = false;
+        bool executedSubtractTest = false;
+
+        FixtureData fixtureData;
+        DisposableFixtureData disposableFixtureData;
 
         public CalculatorTests()
         {
             calculator = new Calculator();
+            log = new StringBuilder();
+            WhereAmI();
+        }
+
+        public void SetFixture(FixtureData data)
+        {
+            WhereAmI();
+            fixtureData = data;
+            log.AppendLine("   FixtureData " + fixtureData.Instance);
+            data.Instance.ShouldEqual(1);
+        }
+
+        public void SetFixture(DisposableFixtureData data)
+        {
+            WhereAmI();
+            disposableFixtureData = data;
+            log.AppendLine("   DisposableFixtureData " + disposableFixtureData.Instance);
+            data.Instance.ShouldEqual(1);
         }
 
         [Fact]
         public void ShouldAdd()
         {
+            executedAddTest = true;
+            WhereAmI();
             calculator.Add(2, 3).ShouldEqual(5);
         }
 
         [Fact]
         public void ShouldSubtract()
         {
+            executedSubtractTest = true;
+            WhereAmI();
             calculator.Subtract(5, 3).ShouldEqual(2);
+        }
+
+        public void Dispose()
+        {
+            WhereAmI();
+            (executedAddTest && executedSubtractTest).ShouldBeFalse();
+            (executedAddTest || executedSubtractTest).ShouldBeTrue();
+
+            log.ToString().ShouldEqual(
+                new StringBuilder()
+                    .AppendLine(".ctor")
+                    .AppendLine("SetFixture")
+                    .AppendLine("   FixtureData 1")
+                    .AppendLine("SetFixture")
+                    .AppendLine("   DisposableFixtureData 1")
+                    .AppendLine(executedAddTest ? "ShouldAdd": "ShouldSubtract")
+                    .AppendLine("Dispose")
+                    .ToString());
+        }
+
+        private void Fail([CallerMemberName] string method = null)
+        {
+            log.AppendLine(method + " is about to throw...");
+            throw new Exception(method + " Threw!");
+        }
+
+        private void WhereAmI([CallerMemberName] string method = null)
+        {
+            log.AppendLine(method);
+        }
+    }
+
+    public class DisposableFixtureData : IDisposable
+    {
+        static int InstanceCounter;
+        public static int DisposalCount { get; private set; }
+
+        public int Instance { get; private set; }
+
+        public DisposableFixtureData()
+        {
+            Instance = ++InstanceCounter;
+        }
+
+        public void Dispose()
+        {
+            DisposalCount++;
+        }
+    }
+
+    public class FixtureData
+    {
+        static int InstanceCounter;
+
+        public int Instance { get; private set; }
+
+        public FixtureData()
+        {
+            Instance = ++InstanceCounter;
         }
     }
 }
--- src/Fixie.Samples/xUnitStyle/CustomConvention.cs
+++ src/Fixie.Samples/xUnitStyle/CustomConvention.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using Fixie.Behaviors;
 using Fixie.Conventions;
 
 namespace Fixie.Samples.xUnitStyle
@@ -15,11 +18,145 @@ namespace Fixie.Samples.xUnitStyle
 
             Cases
                 .HasOrInherits<FactAttribute>();
+
+            FixtureExecutionBehavior = new CreateInstancePerCaseWithFixtureData();
         }
 
         bool HasAnyFactMethods(Type type)
         {
             return factMethods.Filter(type).Any();
+        }
+    }
+
+    public class CreateInstancePerCaseWithFixtureData : TypeBehavior
+    {
+        public void Execute(Type testClass, Convention convention, Listener listener)
+        {
+            var caseMethods = convention.CaseMethods(testClass).ToArray();
+            var exceptionsByCase = caseMethods.ToDictionary(x => x, x => new ExceptionList());
+
+            var classSetUpExceptions = new ExceptionList();
+            var fixtures = SetUpFixtureInstances(testClass, classSetUpExceptions);
+
+            if (classSetUpExceptions.Any())
+            {
+                foreach (var caseMethod in caseMethods)
+                    exceptionsByCase[caseMethod].Add(classSetUpExceptions);
+            }
+            else
+            {
+                foreach (var caseMethod in caseMethods)
+                {
+                    var exceptions = exceptionsByCase[caseMethod];
+
+                    object instance;
+
+                    if (TryConstruct(testClass, exceptions, out instance))
+                    {
+                        bool injectionFailed = false;
+                        foreach (var injectionMethod in fixtures.Keys)
+                        {
+                            try
+                            {
+                                injectionMethod.Invoke(instance, new[] { fixtures[injectionMethod] });
+                            }
+                            catch (TargetInvocationException ex)
+                            {
+                                exceptions.Add(ex.InnerException);
+                                injectionFailed = true;
+                            }
+                            catch (Exception ex)
+                            {
+                                exceptions.Add(ex);
+                                injectionFailed = true;
+                            }
+                        }
+
+                        if (!injectionFailed)
+                            convention.CaseExecutionBehavior.Execute(caseMethod, instance, exceptions);
+
+                        Dispose(instance, exceptions);
+                    }
+                }
+
+                var classTearDownExceptions = new ExceptionList();
+                foreach (var fixtureInstance in fixtures.Values)
+                    Dispose(fixtureInstance, classTearDownExceptions);
+                foreach (var caseMethod in caseMethods)
+                    exceptionsByCase[caseMethod].Add(classTearDownExceptions);
+            }
+
+            foreach (var caseMethod in caseMethods)
+            {
+                var @case = testClass.FullName + "." + caseMethod.Name;
+                var exceptions = exceptionsByCase[caseMethod];
+
+                if (exceptions.Any())
+                    listener.CaseFailed(@case, exceptions.ToArray());
+                else
+                    listener.CasePassed(@case);
+            }
+        }
+
+        static Dictionary<MethodInfo, object> SetUpFixtureInstances(Type testClass, ExceptionList exceptions)
+        {
+            var fixtures = new Dictionary<MethodInfo, object>();
+
+            foreach (var @interface in FixtureInterfaces(testClass))
+            {
+                var fixtureDataType = @interface.GetGenericArguments()[0];
+
+                object fixtureInstance;
+
+                if (TryConstruct(fixtureDataType, exceptions, out fixtureInstance))
+                {
+                    var method = @interface.GetMethod("SetFixture", new[] { fixtureDataType });
+                    fixtures[method] = fixtureInstance;
+                }
+            }
+
+            return fixtures;
+        }
+
+        static IEnumerable<Type> FixtureInterfaces(Type testClass)
+        {
+            return testClass.GetInterfaces()
+                            .Where(@interface => @interface.IsGenericType &&
+                                                 @interface.GetGenericTypeDefinition() == typeof(IUseFixture<>));
+        }
+
+        static bool TryConstruct(Type @class, ExceptionList exceptions, out object instance)
+        {
+            try
+            {
+                instance = Activator.CreateInstance(@class);
+                return true;
+            }
+            catch (TargetInvocationException ex)
+            {
+                exceptions.Add(ex.InnerException);
+            }
+            catch (Exception ex)
+            {
+                exceptions.Add(ex);
+            }
+
+            instance = null;
+            return false;
+        }
+
+        static void Dispose(object instance, ExceptionList exceptions)
+        {
+            try
+            {
+                var disposable = instance as IDisposable;
+                if (disposable != null)
+                    disposable.Dispose();
+            }
+            catch (Exception ex)
+            {
+                exceptions.Add(ex);
+            }
         }
     }
 }

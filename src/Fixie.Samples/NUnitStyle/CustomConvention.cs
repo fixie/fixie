4edff7abc86630c2ea3d32b6b063820bd9f9c5c7
Patrick Lioi <patrick.lioi@gmail.com>
2013-05-29 19:36:00 -0500

    Refactoring within NUnit and xUnit samples, to reveal common logic.

--- src/Fixie.Samples/NUnitStyle/CustomConvention.cs
+++ src/Fixie.Samples/NUnitStyle/CustomConvention.cs
@@ -8,13 +8,93 @@ namespace Fixie.Samples
     public delegate ExceptionList ClassAction(Type testClass);
     public delegate ExceptionList InstanceAction(Type testClass, object instance);
 
+    public static class MethodFilterExtensions
+    {
+        public static ExceptionList InvokeAll(this MethodFilter methodFilter, Type testClass, object instance)
+        {
+            var invoke = new Invoke();
+            var exceptions = new ExceptionList();
+            foreach (var method in methodFilter.Filter(testClass))
+                invoke.Execute(method, instance, exceptions);
+            return exceptions;
+        }
+    }
 
+    public static class BehaviorExtensions
+    {
+        public static MethodBehavior SetUpTearDown<TSetUpAttribute, TTearDownAttribute>(this MethodBehavior inner)
+            where TSetUpAttribute : Attribute
+            where TTearDownAttribute : Attribute
+        {
+            return new MethodSetUpTearDown(
+                new MethodFilter().HasOrInherits<TSetUpAttribute>(),
+                inner,
+                new MethodFilter().HasOrInherits<TTearDownAttribute>());
+        }
+
+        public static InstanceBehavior SetUpTearDown<TSetUpAttribute, TTearDownAttribute>(this InstanceBehavior inner)
+            where TSetUpAttribute : Attribute
+            where TTearDownAttribute : Attribute
+        {
+            return new InstanceSetUpTearDown(
+                new MethodFilter().HasOrInherits<TSetUpAttribute>(),
+                inner,
+                new MethodFilter().HasOrInherits<TTearDownAttribute>());
+        }
+    }
+
+    public class MethodSetUpTearDown : MethodBehavior
+    {
+        readonly InstanceAction setUp;
+        readonly MethodBehavior inner;
+        readonly InstanceAction tearDown;
+
+        public MethodSetUpTearDown(MethodFilter setUps, MethodBehavior inner, MethodFilter tearDowns)
+        {
+            setUp = setUps.InvokeAll;
+            this.inner = inner;
+            tearDown = tearDowns.InvokeAll;
+        }
+
+        public MethodSetUpTearDown(InstanceAction setUp, MethodBehavior inner, InstanceAction tearDown)
+        {
+            this.setUp = setUp;
+            this.inner = inner;
+            this.tearDown = tearDown;
+        }
+
+        public void Execute(MethodInfo method, object instance, ExceptionList exceptions)
+        {
+            var setUpExceptions = setUp(method.ReflectedType, instance);
+            if (setUpExceptions.Any())
+            {
+                exceptions.Add(setUpExceptions);
+            }
+            else
+            {
+                inner.Execute(method, instance, exceptions);
+
+                var tearDownExceptions = tearDown(method.ReflectedType, instance);
+                if (tearDownExceptions.Any())
+                {
+                    exceptions.Add(tearDownExceptions);
+                }
+            }
+        }
+    }
 
     public class InstanceSetUpTearDown : InstanceBehavior
     {
         readonly InstanceAction setUp;
         readonly InstanceBehavior inner;
         readonly InstanceAction tearDown;
+
+        public InstanceSetUpTearDown(MethodFilter setUps, InstanceBehavior inner, MethodFilter tearDowns)
+        {
+            setUp = setUps.InvokeAll;
+            this.inner = inner;
+            tearDown = tearDowns.InvokeAll;
+        }
 
         public InstanceSetUpTearDown(InstanceAction setUp, InstanceBehavior inner, InstanceAction tearDown)
         {
@@ -82,12 +162,6 @@ namespace Fixie.Samples.NUnitStyle
 {
     public class CustomConvention : Convention
     {
-        readonly MethodFilter fixtureSetUps = new MethodFilter().HasOrInherits<TestFixtureSetUpAttribute>();
-        readonly MethodFilter fixtureTearDowns = new MethodFilter().HasOrInherits<TestFixtureTearDownAttribute>();
-
-        readonly MethodFilter setUps = new MethodFilter().HasOrInherits<SetUpAttribute>();
-        readonly MethodFilter tearDowns = new MethodFilter().HasOrInherits<TearDownAttribute>();
-
         public CustomConvention()
         {
             Fixtures
@@ -98,58 +172,13 @@ namespace Fixie.Samples.NUnitStyle
 
             FixtureExecutionBehavior = new CreateInstancePerFixture();
 
-            InstanceExecutionBehavior = new InstanceSetUpTearDown(InvokeAll(fixtureSetUps), InstanceExecutionBehavior, InvokeAll(fixtureTearDowns));
+            InstanceExecutionBehavior =
+                InstanceExecutionBehavior
+                    .SetUpTearDown<TestFixtureSetUpAttribute, TestFixtureTearDownAttribute>();
 
-            CaseExecutionBehavior = new NUnitSetUpTearDown(setUps, CaseExecutionBehavior, tearDowns);
-        }
-
-        static InstanceAction InvokeAll(MethodFilter methodFilter)
-        {
-            return (testClass, instance) => InvokeAll(testClass, instance, methodFilter);
-        }
-
-        static ExceptionList InvokeAll(Type fixtureClass, object instance, MethodFilter methodFilter)
-        {
-            var invoke = new Invoke();
-            var exceptions = new ExceptionList();
-            foreach (var method in methodFilter.Filter(fixtureClass))
-                invoke.Execute(method, instance, exceptions);
-            return exceptions;
-        }
-    }
-
-    public class NUnitSetUpTearDown : MethodBehavior
-    {
-        readonly Invoke invoke;
-        readonly MethodFilter setUps;
-        readonly MethodBehavior inner;
-        readonly MethodFilter tearDowns;
-
-        public NUnitSetUpTearDown(MethodFilter setUps, MethodBehavior inner, MethodFilter tearDowns)
-        {
-            invoke = new Invoke();
-            this.setUps = setUps;
-            this.inner = inner;
-            this.tearDowns = tearDowns;
-        }
-
-        public void Execute(MethodInfo method, object instance, ExceptionList exceptions)
-        {
-            var setUpExceptions = new ExceptionList();
-
-            foreach (var setUp in setUps.Filter(method.ReflectedType))
-                invoke.Execute(setUp, instance, setUpExceptions);
-
-            if (setUpExceptions.Any())
-            {
-                exceptions.Add(setUpExceptions);
-                return;
-            }
-
-            inner.Execute(method, instance, exceptions);
-
-            foreach (var tearDown in tearDowns.Filter(method.ReflectedType))
-                invoke.Execute(tearDown, instance, exceptions);
+            CaseExecutionBehavior =
+                CaseExecutionBehavior
+                    .SetUpTearDown<SetUpAttribute, TearDownAttribute>();
         }
     }
 }

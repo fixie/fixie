9b83819619718aabc9f05eee0954ce35610bee8a
Patrick Lioi <patrick.lioi@gmail.com>
2013-07-05 15:44:16 -0500

    The CommandLineParser parses Main(...)'s incoming args array into a list of test assembly paths and a dictionary of custom key/value pairs.

---
+++ src/Fixie.Tests/CommandLineParserTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Should;
+
+namespace Fixie.Tests
+{
+    public class CommandLineParserTests
+    {
+        public void EmptyByDefault()
+        {
+            var parser = new CommandLineParser();
+            parser.AssemblyPaths.ShouldBeEmpty();
+            parser.CustomOptions.ShouldBeEmpty();
+        }
+
+        public void ParsesAssemblyPathsList()
+        {
+            var parser = new CommandLineParser("foo.dll", "bar.dll");
+            parser.AssemblyPaths.ShouldEqual("foo.dll", "bar.dll");
+            parser.CustomOptions.ShouldBeEmpty();
+        }
+
+        public void ParsesCustomOptions()
+        {
+            var parser = new CommandLineParser("--key", "value");
+            parser.AssemblyPaths.ShouldBeEmpty();
+            parser.CustomOptions.Count.ShouldEqual(1);
+            parser.CustomOptions["key"].ShouldEqual("value");
+        }
+
+        public void DemandsThatCustomOptionsHaveExplicitValues()
+        {
+            Action keyWithoutValue = () => new CommandLineParser("--key", "value", "--invalid");
+
+            keyWithoutValue.ShouldThrow<Exception>("Option --invalid is missing its required value.");
+        }
+
+        public void DemandsThatCustomOptionValuesCannotLookLikeKeys()
+        {
+            Action keyFollowedByAnotherKey = () => new CommandLineParser("--key", "--anotherKey");
+
+            keyFollowedByAnotherKey.ShouldThrow<Exception>("Option --key is missing its required value.");
+        }
+
+        public void DemandsThatCustomOptionKeysMustBeUnique()
+        {
+            Action duplicatedKey = () => new CommandLineParser("--a", "1", "--b", "2", "--a", "2");
+
+            duplicatedKey.ShouldThrow<Exception>("Option --a was specified twice.");
+        }
+
+        public void ParsesAssemblyPathsMixedWithCustomOptions()
+        {
+            var parser = new CommandLineParser("a.dll", "b.dll", "--oops", "c.dll", "d.dll", "--mode", "integration", "e.dll");
+
+            parser.AssemblyPaths.ShouldEqual("a.dll", "b.dll", "d.dll", "e.dll");
+
+            parser.CustomOptions.Count.ShouldEqual(2);
+            parser.CustomOptions["oops"].ShouldEqual("c.dll");
+            parser.CustomOptions["mode"].ShouldEqual("integration");
+        }
+    }
+}
---
+++ src/Fixie/CommandLineParser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Fixie
+{
+    public class CommandLineParser
+    {
+        public IEnumerable<string> AssemblyPaths { get; private set; }
+        public IReadOnlyDictionary<string, string> CustomOptions { get; private set; }
+
+        public CommandLineParser(params string[] args)
+        {
+            var queue = new Queue<string>(args);
+
+            var assemblyPaths = new List<string>();
+            var customOptions = new Dictionary<string, string>();
+
+            while (queue.Any())
+            {
+                var item = queue.Dequeue();
+
+                if (IsKey(item))
+                {
+                    if (!queue.Any() || IsKey(queue.Peek()))
+                        throw new Exception(string.Format("Option {0} is missing its required value.", item));
+
+                    var key = KeyName(item);
+                    var value = queue.Dequeue();
+                    
+                    if (customOptions.ContainsKey(key))
+                        throw new Exception(string.Format("Option {0} was specified twice.", item));
+
+                    customOptions.Add(key, value);
+                }
+                else
+                {
+                    assemblyPaths.Add(item);
+                }
+            }
+
+            AssemblyPaths = assemblyPaths.ToArray();
+            CustomOptions = new ReadOnlyDictionary<string, string>(customOptions);
+        }
+
+        static bool IsKey(string item)
+        {
+            return item.StartsWith("--");
+        }
+
+        static string KeyName(string item)
+        {
+            return item.Substring("--".Length);
+        }
+    }
+}

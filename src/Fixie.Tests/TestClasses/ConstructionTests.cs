d93d4735419676c23de566f23e6a16a44c0e003e
Patrick Lioi <patrick.lioi@gmail.com>
2013-08-02 22:34:16 -0500

    Drop Fixie.Tests.TestClasses.ConstructionTests tests which are made redundant by the new Fixie.Tests.Lifecycle.ConstructionTests.

--- src/Fixie.Tests/TestClasses/ConstructionTests.cs
+++ src/Fixie.Tests/TestClasses/ConstructionTests.cs
@@ -1,27 +1,13 @@
 ﻿using Fixie.Conventions;
-using Should;
 
 namespace Fixie.Tests.TestClasses
 {
     public class ConstructionTests
     {
-        public void ShouldConstructInstancePerCase()
-        {
-            var listener = new StubListener();
-
-            ConstructibleTestClass.ConstructionCount = 0;
-
-             new SelfTestConvention().Execute(listener, typeof(ConstructibleTestClass));
-
-            listener.ShouldHaveEntries(
-                "Fixie.Tests.TestClasses.ConstructionTests+ConstructibleTestClass.Fail failed: 'Fail' failed!",
-                "Fixie.Tests.TestClasses.ConstructionTests+ConstructibleTestClass.Pass passed.");
-
-            ConstructibleTestClass.ConstructionCount.ShouldEqual(2);
-        }
-
         public void ShouldFailAllCasesWhenTestClassConstructorCannotBeInvoked()
         {
+            //This ensures we handle a case that LifecycleTests CANNOT test.
+
             var listener = new StubListener();
 
             new SelfTestConvention().Execute(listener, typeof(CannotInvokeConstructorTestClass));
@@ -31,51 +17,10 @@ namespace Fixie.Tests.TestClasses
                 "Fixie.Tests.TestClasses.ConstructionTests+CannotInvokeConstructorTestClass.UnreachableCaseB failed: No parameterless constructor defined for this object.");
         }
 
-        public void ShouldFailAllCasesWithOriginalExceptionWhenTestClassConstructorThrowsException()
-        {
-            var listener = new StubListener();
-
-            new SelfTestConvention().Execute(listener, typeof(ConstructorThrowsTestClass));
-
-            listener.ShouldHaveEntries(
-                "Fixie.Tests.TestClasses.ConstructionTests+ConstructorThrowsTestClass.UnreachableCaseA failed: '.ctor' failed!",
-                "Fixie.Tests.TestClasses.ConstructionTests+ConstructorThrowsTestClass.UnreachableCaseB failed: '.ctor' failed!");
-        }
-
-        class ConstructibleTestClass
-        {
-            public static int ConstructionCount { get; set; }
-
-            public ConstructibleTestClass()
-            {
-                ConstructionCount++;
-            }
-
-            public void Fail()
-            {
-                throw new FailureException();
-            }
-
-            public void Pass()
-            {
-            }
-        }
-
         class CannotInvokeConstructorTestClass
         {
             public CannotInvokeConstructorTestClass(int argument)
             {
-            }
-
-            public void UnreachableCaseA() { }
-            public void UnreachableCaseB() { }
-        }
-
-        class ConstructorThrowsTestClass
-        {
-            public ConstructorThrowsTestClass()
-            {
-                throw new FailureException();
             }
 
             public void UnreachableCaseA() { }

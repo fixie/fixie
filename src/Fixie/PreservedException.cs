ed71a6921580a6161e119314eb0dc79c80120ff4
Patrick Lioi <patrick.lioi@gmail.com>
2013-08-04 22:54:27 -0500

    Add PreservedException to allow rethrowing consumer-thrown exceptions without losing the original stack trace.

---
+++ src/Fixie/PreservedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fixie
+{
+    public class PreservedException : Exception
+    {
+        public PreservedException(Exception originalException)
+        {
+            OriginalException = originalException;
+        }
+
+        public Exception OriginalException { get; private set; }
+    }
+}

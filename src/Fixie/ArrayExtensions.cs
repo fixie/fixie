0db2a5ed63c643917cef124bd70e7e4a5e74451f
Patrick Lioi <patrick@headspring.com>
2013-08-30 19:13:18 -0500

    Add array shuffle extension method, using teh Fisher-Yates Shuffle algorithm.

---
+++ src/Fixie/ArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fixie
+{
+    public static class ArrayExtensions
+    {
+        //Fisher-Yates Shuffle
+        //  C# implementation from http://stackoverflow.com/a/110570
+        public static void Shuffle<T>(this T[] array, Random random)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = random.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
+}

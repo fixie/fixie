315d5de26b4cf8db87603b5eb7809e2d04b4e96c
Joshua Flanagan <joshuaflanagan@gmail.com>
2013-09-18 21:27:04 -0500

    Sample demonstrating repeatable shuffled test runs.

---
+++ src/Fixie.Samples/Samples/CustomConvention.cs
@@ -0,0 +1,28 @@
+﻿using Fixie.Conventions;
+using System;
+using System.Linq;
+
+namespace Fixie.Samples.Samples
+{
+    public class CustomConvention : Convention
+    {
+        public CustomConvention(RunContext runContext)
+        {
+            var customSeed = runContext.Options["seed"].LastOrDefault();
+            int seed = Environment.TickCount;
+            if (customSeed != null)
+            {
+                seed = Int32.Parse(customSeed);
+            }
+            Console.WriteLine("Running shuffled tests using seed: {0}", seed);
+
+            Classes
+                .Where(type => type.IsInNamespace(GetType().Namespace));
+
+            Cases
+                .Where(method => method.IsVoid());
+
+            ClassExecution.ShuffleCases(new Random(seed));
+        }
+    }
+}

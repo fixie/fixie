dd8f66483bb44336fc2c24e9179ca4c80b418f9b
Patrick Lioi <patrick.lioi@gmail.com>
2013-09-21 09:56:30 -0500

    Move Invoke.Execute's body to new method Case.Execute.  Invoke defers to Case.Execute.

--- src/Fixie/Behaviors/Invoke.cs
+++ src/Fixie/Behaviors/Invoke.cs
@@ -1,57 +1,10 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-
-namespace Fixie.Behaviors
+﻿namespace Fixie.Behaviors
 {
     public class Invoke : CaseBehavior
     {
         public void Execute(Case @case, object instance)
         {
-            try
-            {
-                var method = @case.Method;
-
-                bool isDeclaredAsync = method.IsAsync();
-
-                if (isDeclaredAsync && method.IsVoid())
-                    ThrowForUnsupportedAsyncVoid();
-
-                object result;
-                try
-                {
-                    result = method.Invoke(instance, null);
-                }
-                catch (TargetInvocationException exception)
-                {
-                    throw new PreservedException(exception.InnerException);
-                }
-
-                if (isDeclaredAsync)
-                {
-                    var task = (Task)result;
-                    try
-                    {
-                        task.Wait();
-                    }
-                    catch (AggregateException exception)
-                    {
-                        throw new PreservedException(exception.InnerExceptions.First());
-                    }
-                }
-            }
-            catch (Exception exception)
-            {
-                @case.Fail(exception);
-            }
-        }
-
-        static void ThrowForUnsupportedAsyncVoid()
-        {
-            throw new NotSupportedException(
-                "Async void methods are not supported. Declare async methods with a " +
-                "return type of Task to ensure the task actually runs to completion.");
+            @case.Execute(instance);
         }
     }
 }
--- src/Fixie/Case.cs
+++ src/Fixie/Case.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Fixie
 {
@@ -26,6 +28,51 @@ namespace Fixie
         public Type Class { get; private set; }
         public MethodInfo Method { get; private set; }
         public IReadOnlyList<Exception> Exceptions { get { return exceptions; } }
+
+        public void Execute(object instance)
+        {
+            try
+            {
+                bool isDeclaredAsync = Method.IsAsync();
+
+                if (isDeclaredAsync && Method.IsVoid())
+                    ThrowForUnsupportedAsyncVoid();
+
+                object result;
+                try
+                {
+                    result = Method.Invoke(instance, null);
+                }
+                catch (TargetInvocationException exception)
+                {
+                    throw new PreservedException(exception.InnerException);
+                }
+
+                if (isDeclaredAsync)
+                {
+                    var task = (Task)result;
+                    try
+                    {
+                        task.Wait();
+                    }
+                    catch (AggregateException exception)
+                    {
+                        throw new PreservedException(exception.InnerExceptions.First());
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Fail(exception);
+            }
+        }
+
+        static void ThrowForUnsupportedAsyncVoid()
+        {
+            throw new NotSupportedException(
+                "Async void methods are not supported. Declare async methods with a " +
+                "return type of Task to ensure the task actually runs to completion.");
+        }
 
         public void StartTimer()
         {

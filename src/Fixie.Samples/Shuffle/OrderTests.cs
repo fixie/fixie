d381a638ae771fb42cc0a544fba60a985b746e69
Patrick Lioi <patrick.lioi@gmail.com>
2013-10-19 10:26:03 -0500

    OrderTests write to the console, in order to verify that TeamCity can correctly report on test console output.

--- src/Fixie.Samples/Shuffle/OrderTests.cs
+++ src/Fixie.Samples/Shuffle/OrderTests.cs
@@ -1,15 +1,23 @@
-﻿namespace Fixie.Samples.Shuffle
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Fixie.Samples.Shuffle
 {
     public class OrderTests
     {
-        public void TestA() { }
-        public void TestB() { }
-        public void TestC() { }
-        public void TestD() { }
-        public void TestE() { }
-        public void TestF() { }
-        public void TestG() { }
-        public void TestH() { }
-        public void TestI() { }
+        public void TestA() { WhereAmI(); }
+        public void TestB() { WhereAmI(); }
+        public void TestC() { WhereAmI(); }
+        public void TestD() { WhereAmI(); }
+        public void TestE() { WhereAmI(); }
+        public void TestF() { WhereAmI(); }
+        public void TestG() { WhereAmI(); }
+        public void TestH() { WhereAmI(); }
+        public void TestI() { WhereAmI(); }
+
+        void WhereAmI([CallerMemberName] string method = null)
+        {
+            Console.WriteLine(method);
+        }
     }
 }

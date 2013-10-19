d34ef0814cecf904be87fb527dbccef6f785fcb0
Patrick Lioi <patrick.lioi@gmail.com>
2013-10-19 09:40:58 -0500

    Add RedirectedConsole, which captures any Console.Out and Console.Error output within a using block.

---
+++ src/Fixie/Behaviors/RedirectedConsole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Fixie.Behaviors
+{
+    public class RedirectedConsole : IDisposable
+    {
+        readonly TextWriter outBefore;
+        readonly TextWriter errBefore;
+        readonly StringWriter console;
+
+        public RedirectedConsole()
+        {
+            console = new StringWriter();
+            outBefore = Console.Out;
+            errBefore = Console.Error;
+            Console.SetOut(console);
+            Console.SetError(console);
+        }
+
+        public string Output
+        {
+            get { return console.ToString(); }
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(outBefore);
+            Console.SetError(errBefore);
+            console.Dispose();
+        }
+    }
+}

7e36a0121a919325188ca3810809b3397342b96f
Patrick Lioi <patrick.lioi@gmail.com>
2013-11-25 21:07:31 -0600

    Superficial style and whitespace tweaks.

--- src/Fixie/GenericArgumentResolver.cs
+++ src/Fixie/GenericArgumentResolver.cs
@@ -9,30 +9,26 @@ namespace Fixie
     {
         public static Type[] ResolveTypeArguments(MethodInfo method, object[] parameters)
         {
-            Type[] genericArguments = method.GetGenericArguments();
-            Dictionary<Type, Type> typeMapping = new Dictionary<Type, Type>();
-            List<Type> parameterTypes = method.GetParameters().Select(p => p.ParameterType).ToList();
+            var genericArguments = method.GetGenericArguments();
+            var typeMapping = new Dictionary<Type, Type>();
+            var parameterTypes = method.GetParameters().Select(p => p.ParameterType).ToList();
 
-            foreach (Type genericArgument in genericArguments)
-            {
+            foreach (var genericArgument in genericArguments)
                 typeMapping.Add(genericArgument, GetArgumentType(genericArgument, parameterTypes, parameters));
-            }
 
-            Type[] typeParameters = new Type[genericArguments.Length];
+            var typeParameters = new Type[genericArguments.Length];
             for (int i = 0; i < typeParameters.Length; i++)
                 typeParameters[i] = typeMapping[genericArguments[i]];
             return typeParameters;
         }
 
-
-        private static Type GetArgumentType(Type genericArgumentType, IList<Type> parameterTypes, object[] parameterValues)
+        static Type GetArgumentType(Type genericArgumentType, IList<Type> parameterTypes, object[] parameterValues)
         {
-            List<int> matchingArguments = new List<int>();
+            var matchingArguments = new List<int>();
             for (int i = 0; i < parameterTypes.Count; i++)
-            {
                 if (parameterTypes[i] == genericArgumentType)
                     matchingArguments.Add(i);
-            }
+
             if (matchingArguments.Count == 0)
                 return typeof(object);
 
@@ -45,7 +41,7 @@ namespace Fixie
             return result == null ? typeof(object) : result.GetType();
         }
 
-        private static object Combine(object a, object b)
+        static object Combine(object a, object b)
         {
             if (a == null)
                 return b == null ? null : b.GetType().IsValueType ? null : b;

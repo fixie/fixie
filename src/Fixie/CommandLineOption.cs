ac55f5c8f80062a11d9240894c5eef1ebe4dbb8a
Patrick Lioi <patrick.lioi@gmail.com>
2014-01-11 14:43:18 -0600

    The console runner supports zero or more --fixi:listener command line arguments.  When provided, the values are treated as the namespace-qualified names of Listener classes to use.  When zero are provided, a reasonable default Listener like ConsoleListener is used.

---
+++ src/Fixie/CommandLineOption.cs
@@ -0,0 +1,7 @@
+﻿namespace Fixie
+{
+    public class CommandLineOption
+    {
+        public const string Listener = "fixie:listener";
+    }
+}
--- src/Fixie/ConsoleRunner.cs
+++ src/Fixie/ConsoleRunner.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Fixie.Listeners;
 
@@ -12,11 +14,34 @@ namespace Fixie
 
             var options = new CommandLineParser(args).Options;
 
-            var runner = new Runner(CreateListener(), options);
+            var runner = new Runner(CreateListener(options), options);
             return runner.RunAssembly(assembly);
         }
 
-        static Listener CreateListener()
+        static Listener CreateListener(ILookup<string, string> options)
+        {
+            var listenerNames = options[CommandLineOption.Listener].ToArray();
+
+            return listenerNames.Any()
+                ? CreateCompoundListener(listenerNames)
+                : CreateDefaultListener();
+        }
+
+        static Listener CreateCompoundListener(IEnumerable<string> listenerNames)
+        {
+            var compoundListener = new CompoundListener();
+
+            foreach (var listenerName in listenerNames)
+            {
+                var listenerType = Assembly.GetExecutingAssembly().GetType(listenerName);
+                var listener = (Listener)Activator.CreateInstance(listenerType);
+                compoundListener.Add(listener);
+            }
+
+            return compoundListener;
+        }
+
+        static Listener CreateDefaultListener()
         {
             var runningUnderTeamCity = Environment.GetEnvironmentVariable("TEAMCITY_PROJECT_NAME") != null;
 

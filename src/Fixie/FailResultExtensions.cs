fe5e4f56a7ca85637c2f7f23d9835e4b6c349f24
Patrick Lioi <patrick.lioi@gmail.com>
2014-02-10 18:15:24 -0600

    Move ConsoleExtensions.WriteCompoundStackTrace to the one place it is ever called from.

--- src/Fixie/FailResultExtensions.cs
+++ src/Fixie/FailResultExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Fixie
@@ -16,7 +18,7 @@ namespace Fixie
 
         public static void WriteCompoundStackTraceTo(this FailResult failResult, TextWriter writer)
         {
-            writer.WriteCompoundStackTrace(failResult.Exceptions);
+            WriteCompoundStackTrace(writer, failResult.Exceptions);
         }
 
         public static string PrimaryExceptionMessage(this FailResult failResult)
@@ -27,6 +29,43 @@ namespace Fixie
         public static string PrimaryExceptionTypeName(this FailResult failResult)
         {
             return failResult.Exceptions.First().GetType().FullName;
+        }
+
+        static void WriteCompoundStackTrace(TextWriter console, IEnumerable<Exception> exceptions)
+        {
+            bool isPrimaryException = true;
+
+            foreach (var ex in exceptions)
+            {
+                if (isPrimaryException)
+                {
+                    console.WriteLine(ex.Message);
+                    console.Write(ex.StackTrace);
+                }
+                else
+                {
+                    console.WriteLine();
+                    console.WriteLine();
+                    using (Foreground.DarkGray)
+                        console.WriteLine("===== Secondary Exception: {0} =====", ex.GetType().FullName);
+                    console.WriteLine(ex.Message);
+                    console.Write(ex.StackTrace);
+                }
+
+                var walk = ex;
+                while (walk.InnerException != null)
+                {
+                    walk = walk.InnerException;
+                    console.WriteLine();
+                    console.WriteLine();
+                    using (Foreground.DarkGray)
+                        console.WriteLine("------- Inner Exception: {0} -------", walk.GetType().FullName);
+                    console.WriteLine(walk.Message);
+                    console.Write(walk.StackTrace);
+                }
+
+                isPrimaryException = false;
+            }
         }
     }
 }

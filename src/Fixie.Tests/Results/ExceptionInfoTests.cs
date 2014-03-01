820d18eeca3cf8acb423ada0d1787693cff348fd
Patrick Lioi <patrick.lioi@gmail.com>
2014-03-01 13:14:38 -0600

    Simplify ExceptionInfoTests in preparation for upcoming API change.

--- src/Fixie.Tests/Results/ExceptionInfoTests.cs
+++ src/Fixie.Tests/Results/ExceptionInfoTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
@@ -15,18 +14,18 @@ namespace Fixie.Tests.Results
 
         public void ShouldSummarizeTheGivenException()
         {
-            var exception = GetNestedException();
+            var exception = GetPrimaryException();
 
             var exceptionInfo = new ExceptionInfo(exception, assertionLibrary);
 
-            exceptionInfo.DisplayName.ShouldEqual("System.NullReferenceException");
-            exceptionInfo.Type.ShouldEqual("System.NullReferenceException");
-            exceptionInfo.Message.ShouldEqual("Null reference!");
+            exceptionInfo.DisplayName.ShouldEqual("Fixie.Tests.Results.ExceptionInfoTests+PrimaryException");
+            exceptionInfo.Type.ShouldEqual("Fixie.Tests.Results.ExceptionInfoTests+PrimaryException");
+            exceptionInfo.Message.ShouldEqual("Primary Exception!");
             exceptionInfo.StackTrace.ShouldEqual(exception.StackTrace);
 
             exceptionInfo.InnerException.DisplayName.ShouldEqual("System.DivideByZeroException");
             exceptionInfo.InnerException.Type.ShouldEqual("System.DivideByZeroException");
-            exceptionInfo.InnerException.Message.ShouldEqual("Divide by zero!");
+            exceptionInfo.InnerException.Message.ShouldEqual("Divide by Zero Exception!");
             exceptionInfo.InnerException.StackTrace.ShouldEqual(exception.InnerException.StackTrace);
 
             exceptionInfo.InnerException.InnerException.ShouldBeNull();
@@ -34,101 +33,100 @@ namespace Fixie.Tests.Results
 
         public void ShouldSummarizeCollectionsOfExceptionsComprisedOfPrimaryAndSecondaryExceptions()
         {
-            var primaryException = GetNestedException();
+            var primaryException = GetPrimaryException();
             var secondaryExceptionA = new NotImplementedException();
-            var secondaryExceptionB = GetSecondaryNestedException();
+            var secondaryExceptionB = GetSecondaryException();
 
             var exceptionInfo = new ExceptionInfo(new[] { primaryException, secondaryExceptionA, secondaryExceptionB }, assertionLibrary);
 
-            exceptionInfo.DisplayName.ShouldEqual("System.NullReferenceException");
-            exceptionInfo.Type.ShouldEqual("System.NullReferenceException");
-            exceptionInfo.Message.ShouldEqual("Null reference!");
+            exceptionInfo.DisplayName.ShouldEqual("Fixie.Tests.Results.ExceptionInfoTests+PrimaryException");
+            exceptionInfo.Type.ShouldEqual("Fixie.Tests.Results.ExceptionInfoTests+PrimaryException");
+            exceptionInfo.Message.ShouldEqual("Primary Exception!");
             exceptionInfo.StackTrace
                 .Split(new[] { Environment.NewLine }, StringSplitOptions.None)
                 .Select(x => Regex.Replace(x, @":line \d+", ":line #")) //Avoid brittle assertion introduced by stack trace line numbers.
                 .ShouldEqual(
-                    "Null reference!",
-                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetNestedException() in " + PathToThisFile() + ":line #",
+                    "Primary Exception!",
+                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetPrimaryException() in " + PathToThisFile() + ":line #",
                     "",
                     "------- Inner Exception: System.DivideByZeroException -------",
-                    "Divide by zero!",
-                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetNestedException() in " + PathToThisFile() + ":line #",
+                    "Divide by Zero Exception!",
+                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetPrimaryException() in " + PathToThisFile() + ":line #",
                     "",
                     "===== Secondary Exception: System.NotImplementedException =====",
                     "The method or operation is not implemented.",
                     "",
                     "",
-                    "===== Secondary Exception: System.ArgumentException =====",
-                    "Argument!",
-                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetSecondaryNestedException() in " + PathToThisFile() + ":line #",
+                    "===== Secondary Exception: Fixie.Tests.Results.ExceptionInfoTests+SecondaryException =====",
+                    "Secondary Exception!",
+                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetSecondaryException() in " + PathToThisFile() + ":line #",
                     "",
                     "------- Inner Exception: System.ApplicationException -------",
-                    "Application!",
-                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetSecondaryNestedException() in " + PathToThisFile() + ":line #",
+                    "Application Exception!",
+                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetSecondaryException() in " + PathToThisFile() + ":line #",
                     "",
                     "------- Inner Exception: System.NotImplementedException -------",
-                    "Not implemented!",
-                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetSecondaryNestedException() in " + PathToThisFile() + ":line #");
+                    "Not Implemented Exception!",
+                    "   at Fixie.Tests.Results.ExceptionInfoTests.GetSecondaryException() in " + PathToThisFile() + ":line #");
 
             exceptionInfo.InnerException.ShouldBeNull();
         }
 
         public void ShouldFilterAssertionLibraryImplementationDetails()
         {
-            var primaryException = GetNestedException();
+            var primaryException = GetPrimaryException();
             var secondaryExceptionA = new NotImplementedException();
-            var secondaryExceptionB = GetSecondaryNestedException();
+            var secondaryExceptionB = GetSecondaryException();
 
             assertionLibrary
-                .Namespace("Fixie.Tests.Results")
-                .Namespace("System");
+                .Namespace("Fixie.Tests.Results");
 
             var exceptionInfo = new ExceptionInfo(new[] { primaryException, secondaryExceptionA, secondaryExceptionB }, assertionLibrary);
 
             exceptionInfo.DisplayName.ShouldEqual("");
-            exceptionInfo.Type.ShouldEqual("System.NullReferenceException");
-            exceptionInfo.Message.ShouldEqual("Null reference!");
+            exceptionInfo.Type.ShouldEqual("Fixie.Tests.Results.ExceptionInfoTests+PrimaryException");
+            exceptionInfo.Message.ShouldEqual("Primary Exception!");
             exceptionInfo.StackTrace
                 .Split(new[] { Environment.NewLine }, StringSplitOptions.None)
                 .Select(x => Regex.Replace(x, @":line \d+", ":line #")) //Avoid brittle assertion introduced by stack trace line numbers.
                 .ShouldEqual(
-                    "Null reference!",
+                    "Primary Exception!",
                     "",
                     "",
                     "------- Inner Exception: System.DivideByZeroException -------",
-                    "Divide by zero!",
+                    "Divide by Zero Exception!",
                     "",
                     "",
                     "===== Secondary Exception: System.NotImplementedException =====",
                     "The method or operation is not implemented.",
                     "",
                     "",
-                    "===== Secondary Exception: System.ArgumentException =====",
-                    "Argument!",
+                    "===== Secondary Exception: Fixie.Tests.Results.ExceptionInfoTests+SecondaryException =====",
+                    "Secondary Exception!",
                     "",
                     "",
                     "------- Inner Exception: System.ApplicationException -------",
-                    "Application!",
+                    "Application Exception!",
                     "",
                     "",
                     "------- Inner Exception: System.NotImplementedException -------",
-                    "Not implemented!",
+                    "Not Implemented Exception!",
                     "");
 
             exceptionInfo.InnerException.ShouldBeNull();
         }
 
-        static Exception GetNestedException()
+        static Exception GetPrimaryException()
         {
             try
             {
                 try
                 {
-                    throw new DivideByZeroException("Divide by zero!");
+                    throw new DivideByZeroException("Divide by Zero Exception!");
                 }
                 catch (Exception exception)
                 {
-                    throw new NullReferenceException("Null reference!", exception);
+                    throw new PrimaryException(exception);
                 }
             }
             catch (Exception exception)
@@ -137,7 +135,7 @@ namespace Fixie.Tests.Results
             }
         }
 
-        static Exception GetSecondaryNestedException()
+        static Exception GetSecondaryException()
         {
             try
             {
@@ -145,22 +143,34 @@ namespace Fixie.Tests.Results
                 {
                     try
                     {
-                        throw new NotImplementedException("Not implemented!");
+                        throw new NotImplementedException("Not Implemented Exception!");
                     }
                     catch (Exception exception)
                     {
-                        throw new ApplicationException("Application!", exception);
+                        throw new ApplicationException("Application Exception!", exception);
                     }
                 }
                 catch (Exception exception)
                 {
-                    throw new ArgumentException("Argument!", exception);
+                    throw new SecondaryException(exception);
                 }
             }
             catch (Exception exception)
             {
                 return exception;
             }
+        }
+
+        class PrimaryException : Exception
+        {
+            public PrimaryException(Exception innerException)
+                : base("Primary Exception!", innerException) { }
+        }
+
+        class SecondaryException : Exception
+        {
+            public SecondaryException(Exception innerException)
+                : base("Secondary Exception!", innerException) { }
         }
 
         static string PathToThisFile([CallerFilePath] string path = null)

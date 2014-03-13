0ecd8a52edf5cd2e6eba2a7e3498242cc0ceb8fe
Patrick Lioi <patrick.lioi@gmail.com>
2014-03-12 21:33:04 -0500

    Move tests of ConventionRunner to their own test class (no substantive change).

---
+++ src/Fixie.Tests/Conventions/ConventionRunnerTests.cs
@@ -0,0 +1,65 @@
+using System;
+using Fixie.Conventions;
+
+namespace Fixie.Tests.Conventions
+{
+    public class ConventionRunnerTests
+    {
+        public void ShouldExecuteAllCasesInAllDiscoveredTestClasses()
+        {
+            var listener = new StubListener();
+            var convention = new SelfTestConvention();
+
+            var conventionRunner = new ConventionRunner();
+            conventionRunner.Run(convention, listener, typeof(SampleIrrelevantClass), typeof(PassTestClass), typeof(int), typeof(PassFailTestClass), typeof(SkipTestClass));
+
+            listener.Entries.ShouldEqual("Fixie.Tests.Conventions.ConventionRunnerTests+PassTestClass.PassA passed.",
+                "Fixie.Tests.Conventions.ConventionRunnerTests+PassTestClass.PassB passed.",
+                "Fixie.Tests.Conventions.ConventionRunnerTests+PassFailTestClass.Fail failed: 'Fail' failed!",
+                "Fixie.Tests.Conventions.ConventionRunnerTests+PassFailTestClass.Pass passed.",
+                "Fixie.Tests.Conventions.ConventionRunnerTests+SkipTestClass.Skip skipped.");
+        }
+
+        public void ShouldAllowRandomShufflingOfCaseExecutionOrder()
+        {
+            var listener = new StubListener();
+            var convention = new SelfTestConvention();
+
+            convention.ClassExecution
+                .CreateInstancePerTestClass()
+                .ShuffleCases(new Random(1));
+
+            var conventionRunner = new ConventionRunner();
+            conventionRunner.Run(convention, listener, typeof(SampleIrrelevantClass), typeof(PassTestClass), typeof(int), typeof(PassFailTestClass), typeof(SkipTestClass));
+
+            listener.Entries.ShouldEqual("Fixie.Tests.Conventions.ConventionRunnerTests+PassTestClass.PassB passed.",
+                "Fixie.Tests.Conventions.ConventionRunnerTests+PassTestClass.PassA passed.",
+                "Fixie.Tests.Conventions.ConventionRunnerTests+PassFailTestClass.Fail failed: 'Fail' failed!",
+                "Fixie.Tests.Conventions.ConventionRunnerTests+PassFailTestClass.Pass passed.",
+                "Fixie.Tests.Conventions.ConventionRunnerTests+SkipTestClass.Skip skipped.");
+        }
+
+        class SampleIrrelevantClass
+        {
+            public void PassA() { }
+            public void PassB() { }
+        }
+
+        class PassTestClass
+        {
+            public void PassA() { }
+            public void PassB() { }
+        }
+
+        class PassFailTestClass
+        {
+            public void Pass() { }
+            public void Fail() { throw new FailureException(); }
+        }
+
+        class SkipTestClass
+        {
+            public void Skip() { throw new ShouldBeUnreachableException(); }
+        }
+    }
+}

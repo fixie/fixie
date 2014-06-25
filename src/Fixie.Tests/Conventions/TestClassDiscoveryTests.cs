6dc51f0a7a8f45fab25bfa4f83aa2860e4498a53
Patrick Lioi <patrick.lioi@gmail.com>
2014-06-24 21:35:03 -0500

    Rephrase, consolidate, and rename tests of test class/method discovery for clarity.

---
+++ src/Fixie.Tests/Conventions/TestClassDiscoveryTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using Fixie.Conventions;
+
+namespace Fixie.Tests.Conventions
+{
+    public class TestClassDiscoveryTests
+    {
+        static readonly Type[] CandidateTypes =
+        {
+            typeof(Decimal),
+            typeof(AbstractClass),
+            typeof(DateTime),
+            typeof(DefaultConstructor),
+            typeof(NoDefaultConstructor),
+            typeof(NameEndsWithTests),
+            typeof(String),
+            typeof(Interface),
+            typeof(AttributeSampleBase),
+            typeof(AttributeSample)
+        };
+        
+        public void ShouldConsiderOnlyConcreteClasses()
+        {
+            var customConvention = new Convention();
+
+            DiscoveredTestClasses(customConvention)
+                .ShouldEqual(
+                    typeof(DefaultConstructor),
+                    typeof(NoDefaultConstructor),
+                    typeof(NameEndsWithTests),
+                    typeof(String),
+                    typeof(AttributeSampleBase),
+                    typeof(AttributeSample));
+        }
+
+        public void ShouldDiscoverClassesSatisfyingAllSpecifiedConditions()
+        {
+            var customConvention = new Convention();
+
+            customConvention
+                .Classes
+                .Where(type => type.IsInNamespace("Fixie.Tests"))
+                .Where(type => type.Name.StartsWith("No"));
+
+            DiscoveredTestClasses(customConvention)
+                .ShouldEqual(typeof(NoDefaultConstructor));
+        }
+
+        public void CanDiscoverMethodsByNonInheritedAttributes()
+        {
+            var customConvention = new Convention();
+
+            customConvention
+                .Classes
+                .Has<NonInheritedAttribute>();
+
+            DiscoveredTestClasses(customConvention)
+                .ShouldEqual(typeof(AttributeSample));
+        }
+
+        public void CanDiscoverClassesByInheritedAttributes()
+        {
+            var customConvention = new Convention();
+
+            customConvention
+                .Classes
+                .HasOrInherits<InheritedAttribute>();
+
+            DiscoveredTestClasses(customConvention)
+                .ShouldEqual(
+                    typeof(AttributeSampleBase),
+                    typeof(AttributeSample));
+        }
+
+        public void CanDiscoverClassesByTypeNameSuffix()
+        {
+            var convention = new Convention();
+
+            convention
+                .Classes
+                .NameEndsWith("Constructor");
+
+            DiscoveredTestClasses(convention)
+                .ShouldEqual(
+                    typeof(DefaultConstructor),
+                    typeof(NoDefaultConstructor));
+        }
+
+        public void TheDefaultConventionShouldDiscoverClassesWhoseNameEndsWithTests()
+        {
+            var defaultConvention = new DefaultConvention();
+
+            DiscoveredTestClasses(defaultConvention)
+                .ShouldEqual(
+                    typeof(NameEndsWithTests));
+        }
+
+        static IEnumerable<Type> DiscoveredTestClasses(Convention convention)
+        {
+            return new DiscoveryModel(convention.Config)
+                .TestClasses(CandidateTypes);
+        }
+
+        abstract class AbstractClass { }
+        class DefaultConstructor { }
+        class NoDefaultConstructor { public NoDefaultConstructor(int arg) { } }
+        class NameEndsWithTests { }
+        interface Interface { }
+
+        class InheritedAttribute : Attribute { }
+        class NonInheritedAttribute : Attribute { }
+
+        [Inherited]
+        class AttributeSampleBase { }
+
+        [NonInheritedAttribute]
+        class AttributeSample : AttributeSampleBase { }
+    }
+}

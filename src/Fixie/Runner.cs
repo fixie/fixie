32a6f372f87232a3c0d75ad560a0ca49d3017ce2
Patrick Lioi <patrick.lioi@gmail.com>
2014-06-30 21:04:53 -0500

    Move convention running logic from deprecated ConventionRunner to Runner to enable further refactoring of convention running.

--- src/Fixie/Runner.cs
+++ src/Fixie/Runner.cs
@@ -126,15 +126,13 @@ namespace Fixie
 
         AssemblyResult Run(RunContext runContext, IEnumerable<Convention> conventions, params Type[] candidateTypes)
         {
-            var conventionRunner = new ConventionRunner();
-
             var assemblyResult = new AssemblyResult(runContext.Assembly.Location);
             
             listener.AssemblyStarted(runContext.Assembly);
 
             foreach (var convention in conventions)
             {
-                var conventionResult = conventionRunner.Run(convention, listener, candidateTypes);
+                var conventionResult = RunConvention(convention, candidateTypes);
 
                 assemblyResult.Add(conventionResult);
             }
@@ -173,6 +171,57 @@ namespace Fixie
             throw new Exception(
                 String.Format("Could not construct an instance of type '{0}'.  Expected to find exactly 1 public constructor, but found {1}.",
                     type.FullName, constructors.Length));
+        }
+
+        ConventionResult RunConvention(Convention convention, params Type[] candidateTypes)
+        {
+            var config = convention.Config;
+            var discoveryModel = new DiscoveryModel(config);
+            var executionModel = new ExecutionModel(config);
+            var conventionResult = new ConventionResult(convention.GetType().FullName);
+
+            foreach (var testClass in discoveryModel.TestClasses(candidateTypes))
+            {
+                var classResult = new ClassResult(testClass.FullName);
+
+                var cases = discoveryModel.TestCases(testClass);
+                var casesBySkipState = cases.ToLookup(executionModel.SkipCase);
+                var casesToSkip = casesBySkipState[true];
+                var casesToExecute = casesBySkipState[false].ToArray();
+                foreach (var @case in casesToSkip)
+                {
+                    var skipResult = new SkipResult(@case, executionModel.GetSkipReason(@case));
+                    listener.CaseSkipped(skipResult);
+                    classResult.Add(CaseResult.Skipped(skipResult.Case.Name, skipResult.Reason));
+                }
+
+                if (casesToExecute.Any())
+                {
+                    executionModel.OrderCases(casesToExecute);
+
+                    var caseExecutions = executionModel.Execute(testClass, casesToExecute);
+
+                    foreach (var caseExecution in caseExecutions)
+                    {
+                        if (caseExecution.Exceptions.Any())
+                        {
+                            var failResult = new FailResult(caseExecution, executionModel.AssertionLibraryFilter);
+                            listener.CaseFailed(failResult);
+                            classResult.Add(CaseResult.Failed(failResult.Case.Name, failResult.Duration, failResult.ExceptionSummary));
+                        }
+                        else
+                        {
+                            var passResult = new PassResult(caseExecution);
+                            listener.CasePassed(passResult);
+                            classResult.Add(CaseResult.Passed(passResult.Case.Name, passResult.Duration));
+                        }
+                    }
+                }
+
+                conventionResult.Add(classResult);
+            }
+
+            return conventionResult;
         }
     }
 }

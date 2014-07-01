152a023ffa2fe8ff8b36cbd62c930589bffd5650
Patrick Lioi <patrick.lioi@gmail.com>
2014-06-30 20:37:32 -0500

    AssertionLibraryFilterTests no longer uses deprecated ConventionRunner.

--- src/Fixie.Tests/AssertionLibraryFilteringTests.cs
+++ src/Fixie.Tests/AssertionLibraryFilteringTests.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
-using Fixie.Conventions;
 using Fixie.Listeners;
 
 namespace Fixie.Tests
@@ -15,15 +14,14 @@ namespace Fixie.Tests
             {
                 var listener = new ConsoleListener();
 
-                var convention = SelfTestConvention.Build();
-
-                var conventionRunner = new ConventionRunner();
-                conventionRunner.Run(convention, listener, typeof(SampleTestClass));
+                typeof(SampleTestClass).Run(listener, SelfTestConvention.Build());
 
                 console
                     .Output.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
-                    .Select(x => Regex.Replace(x, @":line \d+", ":line #")) //Avoid brittle assertion introduced by stack trace line numbers.
+                    .Select(CleanBrittleValues)
                     .ShouldEqual(
+                        "------ Testing Assembly Fixie.Tests.dll ------",
+                        "",
                         "Test 'Fixie.Tests.AssertionLibraryFilteringTests+SampleTestClass.DivideByZero' failed: System.DivideByZeroException",
                         "Attempted to divide by zero.",
                         "   at Fixie.Tests.AssertionLibraryFilteringTests.SampleTestClass.DivideByZero() in " + PathToThisFile() + ":line #",
@@ -32,6 +30,8 @@ namespace Fixie.Tests
                         "Expected 1, but was 0.",
                         "   at Fixie.Tests.SampleAssertionLibrary.SampleAssert.AreEqual(Int32 expected, Int32 actual) in " + PathToThisFile() + ":line #",
                         "   at Fixie.Tests.AssertionLibraryFilteringTests.SampleTestClass.FailedAssertion() in " + PathToThisFile() + ":line #",
+                        "",
+                        "0 passed, 2 failed, took 1.23 seconds (Fixie 1.2.3.4).",
                         "",
                         "");
             }
@@ -50,13 +50,14 @@ namespace Fixie.Tests
                     .For<SampleAssertionLibrary.AssertionException>()
                     .For(typeof(SampleAssertionLibrary.SampleAssert));
 
-                var conventionRunner = new ConventionRunner();
-                conventionRunner.Run(convention, listener, typeof(SampleTestClass));
+                typeof(SampleTestClass).Run(listener, convention);
 
                 console
                     .Output.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
-                    .Select(x => Regex.Replace(x, @":line \d+", ":line #")) //Avoid brittle assertion introduced by stack trace line numbers.
+                    .Select(CleanBrittleValues)
                     .ShouldEqual(
+                        "------ Testing Assembly Fixie.Tests.dll ------",
+                        "",
                         "Test 'Fixie.Tests.AssertionLibraryFilteringTests+SampleTestClass.DivideByZero' failed: System.DivideByZeroException",
                         "Attempted to divide by zero.",
                         "   at Fixie.Tests.AssertionLibraryFilteringTests.SampleTestClass.DivideByZero() in " + PathToThisFile() + ":line #",
@@ -65,8 +66,24 @@ namespace Fixie.Tests
                         "Expected 1, but was 0.",
                         "   at Fixie.Tests.AssertionLibraryFilteringTests.SampleTestClass.FailedAssertion() in " + PathToThisFile() + ":line #",
                         "",
+                        "0 passed, 2 failed, took 1.23 seconds (Fixie 1.2.3.4).",
+                        "",
                         "");
             }
+        }
+
+        static string CleanBrittleValues(string actualRawContent)
+        {
+            //Avoid brittle assertion introduced by fixie version.
+            var cleaned = Regex.Replace(actualRawContent, @"\(Fixie \d+\.\d+\.\d+\.\d+\)", @"(Fixie 1.2.3.4)");
+
+            //Avoid brittle assertion introduced by test duration.
+            cleaned = Regex.Replace(cleaned, @"took [\d\.]+ seconds", @"took 1.23 seconds");
+
+            //Avoid brittle assertion introduced by stack trace line numbers.
+            cleaned = Regex.Replace(cleaned, @":line \d+", ":line #");
+
+            return cleaned;
         }
 
         static string PathToThisFile([CallerFilePath] string path = null)

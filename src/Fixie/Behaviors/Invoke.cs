03c6e0ac5eac8d34f3e97f2a17c8289bea1cc519
Patrick Lioi <patrick.lioi@gmail.com>
2014-07-10 20:32:53 -0500

    Move case execution logic out of Case and into the Invoke behavior.

--- src/Fixie/Behaviors/Invoke.cs
+++ src/Fixie/Behaviors/Invoke.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Fixie.Behaviors
 {
@@ -6,7 +9,63 @@ namespace Fixie.Behaviors
     {
         public void Execute(CaseExecution caseExecution, Action next)
         {
-            caseExecution.Case.Execute(caseExecution.Instance, caseExecution);
+            try
+            {
+                var @case = caseExecution.Case;
+                var method = @case.Method;
+
+                bool isDeclaredAsync = method.IsAsync();
+
+                if (isDeclaredAsync && method.IsVoid())
+                    ThrowForUnsupportedAsyncVoid();
+
+                object result;
+                try
+                {
+                    result = method.Invoke(caseExecution.Instance, @case.Parameters);
+                }
+                catch (TargetInvocationException exception)
+                {
+                    throw new PreservedException(exception.InnerException);
+                }
+
+                if (isDeclaredAsync)
+                {
+                    var task = (Task)result;
+                    try
+                    {
+                        task.Wait();
+                    }
+                    catch (AggregateException exception)
+                    {
+                        throw new PreservedException(exception.InnerExceptions.First());
+                    }
+
+                    if (method.ReturnType.IsGenericType)
+                    {
+                        var property = task.GetType().GetProperty("Result", BindingFlags.Instance | BindingFlags.Public);
+
+                        result = property.GetValue(task, null);
+                    }
+                    else
+                    {
+                        result = null;
+                    }
+                }
+
+                caseExecution.Result = result;
+            }
+            catch (Exception exception)
+            {
+                caseExecution.Fail(exception);
+            }
+        }
+
+        static void ThrowForUnsupportedAsyncVoid()
+        {
+            throw new NotSupportedException(
+                "Async void methods are not supported. Declare async methods with a " +
+                "return type of Task to ensure the task actually runs to completion.");
         }
     }
 }
--- src/Fixie/Case.cs
+++ src/Fixie/Case.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace Fixie
 {
     public class Case
     {
-        readonly object[] parameters;
-
         public Case(MethodInfo caseMethod, params object[] parameters)
         {
-            this.parameters = parameters != null && parameters.Length == 0 ? null : parameters;
+            Parameters = parameters != null && parameters.Length == 0 ? null : parameters;
             Class = caseMethod.ReflectedType;
 
             Method = caseMethod.IsGenericMethodDefinition
@@ -28,72 +25,15 @@ namespace Fixie
             if (Method.IsGenericMethod)            
                 name = string.Format("{0}<{1}>", name, string.Join(", ", Method.GetGenericArguments().Select(x => x.FullName)));
 
-            if (parameters != null && parameters.Length > 0)
-                name = string.Format("{0}({1})", name, string.Join(", ", parameters.Select(x => x.ToDisplayString())));
+            if (Parameters != null && Parameters.Length > 0)
+                name = string.Format("{0}({1})", name, string.Join(", ", Parameters.Select(x => x.ToDisplayString())));
 
             return name;
         }
 
         public string Name { get; private set; }
         public Type Class { get; private set; }
-        public MethodInfo Method { get; private set; }        
-
-        public virtual void Execute(object instance, CaseExecution caseExecution)
-        {
-            try
-            {
-                bool isDeclaredAsync = Method.IsAsync();
-
-                if (isDeclaredAsync && Method.IsVoid())
-                    ThrowForUnsupportedAsyncVoid();
-
-                object result;
-                try
-                {
-                    result = Method.Invoke(instance, parameters);
-                }
-                catch (TargetInvocationException exception)
-                {
-                    throw new PreservedException(exception.InnerException);
-                }
-
-                if (isDeclaredAsync)
-                {
-                    var task = (Task)result;
-                    try
-                    {
-                        task.Wait();
-                    }
-                    catch (AggregateException exception)
-                    {
-                        throw new PreservedException(exception.InnerExceptions.First());
-                    }
-
-                    if (Method.ReturnType.IsGenericType)
-                    {
-                        var property = task.GetType().GetProperty("Result", BindingFlags.Instance | BindingFlags.Public);
-
-                        result = property.GetValue(task, null);
-                    }
-                    else
-                    {
-                        result = null;
-                    }
-                }
-
-                caseExecution.Result = result;
-            }
-            catch (Exception exception)
-            {
-                caseExecution.Fail(exception);
-            }
-        }
-        
-        static void ThrowForUnsupportedAsyncVoid()
-        {
-            throw new NotSupportedException(
-                "Async void methods are not supported. Declare async methods with a " +
-                "return type of Task to ensure the task actually runs to completion.");
-        }
+        public MethodInfo Method { get; private set; }
+        public object[] Parameters { get; private set; }
     }
 }

b76d29234e13e22d48daaeb356c92eb251712c32
Patrick Lioi <patrick.lioi@gmail.com>
2014-07-10 19:52:26 -0500

    Drop redundant tests from CaseTests that are already fully covered by AsyncCaseTests.

--- src/Fixie.Tests/CaseTests.cs
+++ src/Fixie.Tests/CaseTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Should;
 
 namespace Fixie.Tests
@@ -96,70 +93,6 @@ namespace Fixie.Tests
             ExpectException(caseExecution, "FailureException", "'Throws' failed!");
         }
 
-        public void ShouldInvokeAsyncMethods()
-        {
-            var @case = Case("Await");
-            var caseExecution = new CaseExecution(@case);
-
-            @case.Execute(this, caseExecution);
-
-            invoked.ShouldBeTrue();
-
-            caseExecution.Exceptions.Count.ShouldEqual(0);
-        }
-
-        public void ShouldLogOriginalExceptionWhenAsyncMethodThrowsAfterAwaiting()
-        {
-            var @case = Case("AwaitThenThrow");
-            var caseExecution = new CaseExecution(@case);
-
-            @case.Execute(this, caseExecution);
-
-            invoked.ShouldBeTrue();
-
-            ExpectException(caseExecution, "EqualException", "Assert.Equal() Failure" + Environment.NewLine +
-                                                     "Expected: 0" + Environment.NewLine +
-                                                     "Actual:   3");
-        }
-
-        public void ShouldLogOriginalExceptionWhenAsyncMethodThrowsWithinTheAwaitedTask()
-        {
-            var @case = Case("AwaitOnTaskThatThrows");
-            var caseExecution = new CaseExecution(@case);
-
-            @case.Execute(this, caseExecution);
-
-            invoked.ShouldBeTrue();
-
-            ExpectException(caseExecution, "DivideByZeroException", "Attempted to divide by zero.");
-        }
-
-        public void ShouldLogOriginalExceptionWhenAsyncMethodThrowsBeforeAwaitingOnAnyTask()
-        {
-            var @case = Case("ThrowBeforeAwait");
-            var caseExecution = new CaseExecution(@case);
-
-            @case.Execute(this, caseExecution);
-
-            invoked.ShouldBeTrue();
-
-            ExpectException(caseExecution, "FailureException", "'ThrowBeforeAwait' failed!");
-        }
-
-        public void ShouldLogExceptionWhenMethodIsUnsupportedAsyncVoid()
-        {
-            var @case = Case("UnsupportedAsyncVoid");
-            var caseExecution = new CaseExecution(@case);
-
-            @case.Execute(this, caseExecution);
-
-            invoked.ShouldBeFalse();
-
-            ExpectException(caseExecution, "NotSupportedException",
-                            "Async void methods are not supported. Declare async methods with a " +
-                            "return type of Task to ensure the task actually runs to completion.");
-        }
-
         public void ShouldInferAppropriateClassGivenCaseMethod()
         {
             var methodDeclaredInChildClass = new Case(typeof(SampleChildTestClass).GetInstanceMethod("TestMethodDefinedWithinChildClass"));
@@ -222,61 +155,6 @@ namespace Fixie.Tests
         void Generic<T1, T2>(int i, T1 t1, T2 t2a, T2 t2b)
         {
             invoked = true;
-        }
-
-        static Task<int> Divide(int numerator, int denominator)
-        {
-            return Task.Run(() => numerator/denominator);
-        }
-
-        async Task Await()
-        {
-            invoked = true;
-
-            var result = await Divide(15, 5);
-
-            result.ShouldEqual(3);
-        }
-
-        async Task AwaitThenThrow()
-        {
-            invoked = true;
-
-            var result = await Divide(15, 5);
-
-            result.ShouldEqual(0);
-        }
-
-        async Task AwaitOnTaskThatThrows()
-        {
-            invoked = true;
-
-            await Divide(15, 0);
-
-            throw new ShouldBeUnreachableException();
-        }
-
-        async Task ThrowBeforeAwait()
-        {
-            invoked = true;
-
-            ThrowException();
-
-            await Divide(15, 5);
-        }
-
-        async void UnsupportedAsyncVoid()
-        {
-            invoked = true;
-
-            await Divide(15, 5);
-
-            throw new ShouldBeUnreachableException();
-        }
-
-        static void ThrowException([CallerMemberName] string member = null)
-        {
-            throw new FailureException(member);
         }
     }
 }

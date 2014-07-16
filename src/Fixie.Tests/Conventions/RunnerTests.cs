31015890e31fa0d215fbcf1e46286fd04ce8bb4b
Patrick Lioi <patrick.lioi@gmail.com>
2014-07-15 20:20:06 -0500

    Fix name of RunnerTests.

--- src/Fixie.Tests/Conventions/RunnerTests.cs
+++ src/Fixie.Tests/Conventions/RunnerTests.cs
@@ -2,44 +2,42 @@ using System;
 
 namespace Fixie.Tests.Conventions
 {
-    public class ConventionRunnerTests
+    public class RunnerTests
     {
         public void ShouldExecuteAllCasesInAllDiscoveredTestClasses()
         {
             var listener = new StubListener();
-            var runner = new Runner(listener);
             var convention = SelfTestConvention.Build();
 
-            runner.RunTypes(GetType().Assembly, convention,
+            new Runner(listener).RunTypes(GetType().Assembly, convention,
                 typeof(SampleIrrelevantClass), typeof(PassTestClass), typeof(int),
                 typeof(PassFailTestClass), typeof(SkipTestClass));
 
-            listener.Entries.ShouldEqual("Fixie.Tests.Conventions.ConventionRunnerTests+PassTestClass.PassA passed.",
-                "Fixie.Tests.Conventions.ConventionRunnerTests+PassTestClass.PassB passed.",
-                "Fixie.Tests.Conventions.ConventionRunnerTests+PassFailTestClass.Fail failed: 'Fail' failed!",
-                "Fixie.Tests.Conventions.ConventionRunnerTests+PassFailTestClass.Pass passed.",
-                "Fixie.Tests.Conventions.ConventionRunnerTests+SkipTestClass.Skip skipped.");
+            listener.Entries.ShouldEqual("Fixie.Tests.Conventions.RunnerTests+PassTestClass.PassA passed.",
+                "Fixie.Tests.Conventions.RunnerTests+PassTestClass.PassB passed.",
+                "Fixie.Tests.Conventions.RunnerTests+PassFailTestClass.Fail failed: 'Fail' failed!",
+                "Fixie.Tests.Conventions.RunnerTests+PassFailTestClass.Pass passed.",
+                "Fixie.Tests.Conventions.RunnerTests+SkipTestClass.Skip skipped.");
         }
 
         public void ShouldAllowRandomShufflingOfCaseExecutionOrder()
         {
             var listener = new StubListener();
-            var runner = new Runner(listener);
             var convention = SelfTestConvention.Build();
 
             convention.ClassExecution
                 .CreateInstancePerClass()
                 .ShuffleCases(new Random(1));
 
-            runner.RunTypes(GetType().Assembly, convention,
+            new Runner(listener).RunTypes(GetType().Assembly, convention,
                 typeof(SampleIrrelevantClass), typeof(PassTestClass), typeof(int),
                 typeof(PassFailTestClass), typeof(SkipTestClass));
 
-            listener.Entries.ShouldEqual("Fixie.Tests.Conventions.ConventionRunnerTests+PassTestClass.PassB passed.",
-                "Fixie.Tests.Conventions.ConventionRunnerTests+PassTestClass.PassA passed.",
-                "Fixie.Tests.Conventions.ConventionRunnerTests+PassFailTestClass.Fail failed: 'Fail' failed!",
-                "Fixie.Tests.Conventions.ConventionRunnerTests+PassFailTestClass.Pass passed.",
-                "Fixie.Tests.Conventions.ConventionRunnerTests+SkipTestClass.Skip skipped.");
+            listener.Entries.ShouldEqual("Fixie.Tests.Conventions.RunnerTests+PassTestClass.PassB passed.",
+                "Fixie.Tests.Conventions.RunnerTests+PassTestClass.PassA passed.",
+                "Fixie.Tests.Conventions.RunnerTests+PassFailTestClass.Fail failed: 'Fail' failed!",
+                "Fixie.Tests.Conventions.RunnerTests+PassFailTestClass.Pass passed.",
+                "Fixie.Tests.Conventions.RunnerTests+SkipTestClass.Skip skipped.");
         }
 
         class SampleIrrelevantClass

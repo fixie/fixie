4c553cc8cf8449fd8937d7a5bc723cc9b0094d1a
Patrick Lioi <patrick.lioi@gmail.com>
2014-07-15 21:31:03 -0500

    Move ExecutionPlan to its own file.

--- src/Fixie/ExecutionModel.cs
+++ src/Fixie/ExecutionModel.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using Fixie.Behaviors;
 using Fixie.Conventions;
 using Fixie.Discovery;
 using Fixie.Results;
@@ -85,92 +83,6 @@ namespace Fixie
             var result = new FailResult(caseExecution, assertionLibraryFilter);
             listener.CaseFailed(result);
             return CaseResult.Failed(result.Case.Name, result.Duration, result.ExceptionSummary);
-        }
-    }
-
-    public class ExecutionPlan
-    {
-        readonly ConfigModel config;
-
-        readonly BehaviorChain<ClassExecution> classBehaviorChain;
-        readonly BehaviorChain<InstanceExecution> instanceBehaviorChain;
-        readonly BehaviorChain<CaseExecution> caseBehaviorChain;
-
-        public ExecutionPlan(ConfigModel config)
-        {
-            this.config = config;
-
-            classBehaviorChain = BuildClassBehaviorChain();
-            instanceBehaviorChain = BuildInstanceBehaviorChain();
-            caseBehaviorChain = BuildCaseBehaviorChain();
-        }
-
-        public IReadOnlyList<CaseExecution> Execute(Type testClass, Case[] casesToExecute)
-        {
-            var caseExecutions = casesToExecute.Select(@case => new CaseExecution(@case)).ToArray();
-            var classExecution = new ClassExecution(testClass, caseExecutions);
-
-            classBehaviorChain.Execute(classExecution);
-
-            return caseExecutions;
-        }
-
-        public void PerformClassLifecycle(Type testClass, IReadOnlyList<CaseExecution> caseExecutionsForThisInstance)
-        {
-            var instance = config.TestClassFactory(testClass);
-
-            var instanceExecution = new InstanceExecution(testClass, instance, caseExecutionsForThisInstance);
-            instanceBehaviorChain.Execute(instanceExecution);
-
-            var disposable = instance as IDisposable;
-            if (disposable != null)
-                disposable.Dispose();
-        }
-
-        public void ExecuteCaseBehaviors(CaseExecution caseExecution)
-        {
-            caseBehaviorChain.Execute(caseExecution);
-        }
-
-        BehaviorChain<ClassExecution> BuildClassBehaviorChain()
-        {
-            var chain = config.CustomClassBehaviors
-                .Select(customBehavior => (ClassBehavior)Activator.CreateInstance(customBehavior))
-                .ToList();
-
-            chain.Add(GetInnermostBehavior());
-
-            return new BehaviorChain<ClassExecution>(chain);
-        }
-
-        BehaviorChain<InstanceExecution> BuildInstanceBehaviorChain()
-        {
-            var chain = config.CustomInstanceBehaviors
-                .Select(customBehavior => (InstanceBehavior)Activator.CreateInstance(customBehavior))
-                .ToList();
-
-            chain.Add(new ExecuteCases(this));
-
-            return new BehaviorChain<InstanceExecution>(chain);
-        }
-
-        BehaviorChain<CaseExecution> BuildCaseBehaviorChain()
-        {
-            var chain = config.CustomCaseBehaviors
-                .Select(customBehavior => (CaseBehavior)Activator.CreateInstance(customBehavior))
-                .ToList();
-
-            chain.Add(new InvokeMethod());
-
-            return new BehaviorChain<CaseExecution>(chain);
-        }
-
-        ClassBehavior GetInnermostBehavior()
-        {
-            if (config.ConstructionFrequency == ConstructionFrequency.PerCase)
-                return new CreateInstancePerCase(this);
-
-            return new CreateInstancePerClass(this);
         }
     }
 }

5c13053d8421c867c3f2caa1f2b07cdfd4e883eb
Patrick Lioi <patrick.lioi@gmail.com>
2014-07-22 21:33:58 -0500

    Move CompoundException to its own file.

--- src/Fixie/Results/ExceptionInfo.cs
+++ src/Fixie/Results/ExceptionInfo.cs
@@ -1,68 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using Fixie.Execution;
 
 namespace Fixie.Results
 {
-    [Serializable]
-    public class CompoundException
-    {
-        public ExceptionInfo PrimaryException { get; private set; }
-        public IReadOnlyList<ExceptionInfo> SecondaryExceptions { get; private set; }
-        public string CompoundStackTrace { get; private set; }
-
-        public CompoundException(IEnumerable<Exception> exceptions, AssertionLibraryFilter filter)
-        {
-            var all = exceptions.Select(x => new ExceptionInfo(x, filter)).ToArray();
-            PrimaryException = all.First();
-            SecondaryExceptions = all.Skip(1).ToArray();
-            CompoundStackTrace = GetCompoundStackTrace(all);
-        }
-
-        static string GetCompoundStackTrace(IEnumerable<ExceptionInfo> exceptions)
-        {
-            using (var console = new StringWriter())
-            {
-                bool isPrimaryException = true;
-
-                foreach (var ex in exceptions)
-                {
-                    if (isPrimaryException)
-                    {
-                        console.WriteLine(ex.Message);
-                        console.Write(ex.StackTrace);
-                    }
-                    else
-                    {
-                        console.WriteLine();
-                        console.WriteLine();
-                        using (Foreground.DarkGray)
-                            console.WriteLine("===== Secondary Exception: {0} =====", ex.Type);
-                        console.WriteLine(ex.Message);
-                        console.Write(ex.StackTrace);
-                    }
-
-                    var walk = ex;
-                    while (walk.InnerException != null)
-                    {
-                        walk = walk.InnerException;
-                        console.WriteLine();
-                        console.WriteLine();
-                        using (Foreground.DarkGray)
-                            console.WriteLine("------- Inner Exception: {0} -------", walk.Type);
-                        console.WriteLine(walk.Message);
-                        console.Write(walk.StackTrace);
-                    }
-
-                    isPrimaryException = false;
-                }
-                return console.ToString();
-            }
-        }
-    }
-
     [Serializable]
     public class ExceptionInfo
     {

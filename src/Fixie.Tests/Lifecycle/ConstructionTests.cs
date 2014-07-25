3aca557fcb82ad3780020ccf1370e97e17ac5c77
Patrick Lioi <patrick.lioi@gmail.com>
2014-07-24 21:25:51 -0500

    Test coverage for the fact that when all of the tests in a test class fail during custom parameter generation, the test class should not really be executed and therefore the test class should never even be constructed (much like a test class in which all the tests are skipped).

--- src/Fixie.Tests/Lifecycle/ConstructionTests.cs
+++ src/Fixie.Tests/Lifecycle/ConstructionTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Should;
 
 namespace Fixie.Tests.Lifecycle
@@ -224,11 +226,82 @@ namespace Fixie.Tests.Lifecycle
             output.ShouldHaveLifecycle();
         }
 
+        public void ShouldSkipConstructingPerCaseWhenAllCasesFailCustomParameterGeneration()
+        {
+            Convention.ClassExecution
+                      .CreateInstancePerCase();
+
+            Convention.Parameters(ParametersFromBuggySource);
+
+            var output = Run();
+
+            output.ShouldHaveResults(
+                "SampleTestClass.Pass failed: Exception thrown while attempting to yield input parameters for method: Pass",
+                "SampleTestClass.Fail failed: Exception thrown while attempting to yield input parameters for method: Fail");
+
+            output.ShouldHaveLifecycle();
+        }
+
+        public void ShouldSkipConstructingPerClassWhenAllCasesFailCustomParameterGeneration()
+        {
+            Convention.ClassExecution
+                      .CreateInstancePerClass();
+
+            Convention.Parameters(ParametersFromBuggySource);
+
+            var output = Run();
+
+            output.ShouldHaveResults(
+                "SampleTestClass.Pass failed: Exception thrown while attempting to yield input parameters for method: Pass",
+                "SampleTestClass.Fail failed: Exception thrown while attempting to yield input parameters for method: Fail");
+
+            output.ShouldHaveLifecycle();
+        }
+
+        public void ShouldSkipConstructingPerCaseUsingCustomFactoryWhenAllCasesFailCustomParameterGeneration()
+        {
+            Convention.ClassExecution
+                      .CreateInstancePerCase()
+                      .UsingFactory(Factory);
+
+            Convention.Parameters(ParametersFromBuggySource);
+
+            var output = Run();
+
+            output.ShouldHaveResults(
+                "SampleTestClass.Pass failed: Exception thrown while attempting to yield input parameters for method: Pass",
+                "SampleTestClass.Fail failed: Exception thrown while attempting to yield input parameters for method: Fail");
+
+            output.ShouldHaveLifecycle();
+        }
+
+        public void ShouldSkipConstructingPerClassUsingCustomFactoryWhenAllCasesFailCustomParameterGeneration()
+        {
+            Convention.ClassExecution
+                      .CreateInstancePerClass()
+                      .UsingFactory(Factory);
+
+            Convention.Parameters(ParametersFromBuggySource);
+
+            var output = Run();
+
+            output.ShouldHaveResults(
+                "SampleTestClass.Pass failed: Exception thrown while attempting to yield input parameters for method: Pass",
+                "SampleTestClass.Fail failed: Exception thrown while attempting to yield input parameters for method: Fail");
+
+            output.ShouldHaveLifecycle();
+        }
+
         static object Factory(Type testClass)
         {
             WhereAmI();
             testClass.ShouldEqual(typeof(SampleTestClass));
             return new SampleTestClass();
+        }
+
+        static IEnumerable<object[]> ParametersFromBuggySource(MethodInfo method)
+        {
+            throw new Exception("Exception thrown while attempting to yield input parameters for method: " + method.Name);
         }
     }
 }

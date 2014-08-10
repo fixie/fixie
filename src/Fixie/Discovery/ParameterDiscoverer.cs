e71ba53dac8b02513f98ef8a04a1dba005887d4c
Patrick Lioi <patrick.lioi@gmail.com>
2014-08-10 16:41:17 -0500

    Add ParameterDiscoverer, which produces test method input parameters from any number of sources named in a custom convention.

---
+++ src/Fixie/Discovery/ParameterDiscoverer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Fixie.Conventions;
+
+namespace Fixie.Discovery
+{
+    public class ParameterDiscoverer
+    {
+        readonly ParameterSource[] parameterSources;
+
+        public ParameterDiscoverer(Configuration config)
+        {
+            parameterSources = config.ParameterSourceTypes
+                .Select(customBehavior => (ParameterSource)Activator.CreateInstance((Type)customBehavior))
+                .ToArray();
+        }
+
+        public IEnumerable<object[]> GetParameters(MethodInfo method)
+        {
+            //TODO: Shortcut when the method has no parameters anyway?
+            return parameterSources.SelectMany(source => source.GetParameters(method));
+        }
+    }
+}

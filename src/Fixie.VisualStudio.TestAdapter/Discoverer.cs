9e09203c22cb1e741604f58ae9c627c72ffc164e
Patrick Lioi <patrick.lioi@gmail.com>
2014-10-22 22:58:04 -0500

    First pass at a fake/minimal Visual Studio adapter implementation.

---
+++ src/Fixie.VisualStudio.TestAdapter/Discoverer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace Fixie.VisualStudio.TestAdapter
+{
+    [DefaultExecutorUri(Executor.Id)]
+    [FileExtension(".exe")]
+    [FileExtension(".dll")]
+    public class Discoverer : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            foreach (var assemblyPath in sources)
+            {
+                discoverySink.SendTestCase(ToVsTestCase("Pass", assemblyPath));
+                discoverySink.SendTestCase(ToVsTestCase("Fail", assemblyPath));
+                discoverySink.SendTestCase(ToVsTestCase("Skip", assemblyPath));
+            }
+        }
+
+        static TestCase ToVsTestCase(string name, string assemblyPath)
+        {
+            var fullyQualifiedName = "Test Case " + name;
+
+            return new TestCase(fullyQualifiedName, Executor.Uri, assemblyPath)
+            {
+                DisplayName = name
+            };
+        }
+    }
+}
---
+++ src/Fixie.VisualStudio.TestAdapter/Executor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+
+namespace Fixie.VisualStudio.TestAdapter
+{
+    [ExtensionUri(Id)]
+    public class Executor : ITestExecutor
+    {
+        public const string Id = "executor://Fixie.VisualStudio";
+        public static Uri Uri = new Uri(Id);
+
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            foreach (var test in tests)
+            {
+                frameworkHandle.RecordResult(ToVsTestResult(test));
+            }
+        }
+
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            foreach (var assemblyPath in sources)
+            {
+                var test = new TestCase("From source " + assemblyPath, Uri, assemblyPath);
+                frameworkHandle.RecordResult(new TestResult(test)
+                {
+                    Duration = TimeSpan.FromSeconds(2),
+                    DisplayName = test.DisplayName,
+                    Outcome = TestOutcome.Passed
+                });
+                frameworkHandle.RecordResult(new TestResult(test)
+                {
+                    Duration = TimeSpan.FromSeconds(2),
+                    DisplayName = test.DisplayName,
+                    Outcome = TestOutcome.Failed
+                });
+            }
+        }
+
+        public void Cancel()
+        {
+        }
+
+        static TestResult ToVsTestResult(TestCase test)
+        {
+            return new TestResult(test)
+            {
+                Duration = TimeSpan.FromSeconds(2),
+                DisplayName = test.DisplayName,
+                Outcome = (TestOutcome)Enum.Parse(typeof(TestOutcome), test.DisplayName)
+            };
+        }
+    }
+}

2027549daefdcee774ebd6bda0f052bf14410088
Patrick Lioi <patrick.lioi@gmail.com>
2014-10-25 16:28:34 -0500

    Add ExecutionProxy, which runners can use to interact with test execution across an appdomain boundary.

---
+++ src/Fixie/ExecutionProxy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using Fixie.Execution;
+using Fixie.Results;
+
+namespace Fixie
+{
+    public class ExecutionProxy : MarshalByRefObject
+    {
+        public AssemblyResult RunAssembly(string assemblyFullPath, string[] args, Listener listener)
+        {
+            var assembly = Assembly.Load(AssemblyName.GetAssemblyName(assemblyFullPath));
+
+            var options = new CommandLineParser(args).Options;
+
+            var runner = new Runner(listener, options);
+            return runner.RunAssembly(assembly);
+        }
+    }
+}

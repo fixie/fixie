b9dcdfac35b5b0d40573cd8e680fd0054e029e2a
Patrick Lioi <patrick.lioi@gmail.com>
2014-12-15 17:02:28 -0600

    Allow runners to supply their own TextWriter as the target or Console output.  Visual Studio runner provides such a TextWriter in order to redirect all console output to the VS Output window along with other VS Test Adapter output.  Due to severe limitations in the VS Test Adapter API, all such output must be line-buffered and must be prefixed with a nonempty string (in this case "[Console] ".

---
+++ src/Fixie.VisualStudio.TestAdapter/VsOutputWindow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace Fixie.VisualStudio.TestAdapter
+{
+    public class VsOutputWindow : TextWriter
+    {
+        readonly IMessageLogger logger;
+        string lineBuffer;
+
+        public VsOutputWindow(IMessageLogger logger)
+        {
+            this.logger = logger;
+            lineBuffer = "";
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+
+        public override void Write(char value)
+        {
+            if (lineBuffer == null)
+                lineBuffer = "";
+            lineBuffer += value;
+            if (lineBuffer.EndsWith(Environment.NewLine))
+            {
+                lineBuffer = lineBuffer.Substring(0, lineBuffer.Length - Environment.NewLine.Length);
+                Flush();
+            }
+        }
+
+        public override void Flush()
+        {
+            if (lineBuffer != null)
+                logger.Info("[Console] " + lineBuffer);
+            lineBuffer = null;
+        }
+    }
+}

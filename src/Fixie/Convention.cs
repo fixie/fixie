90cd304cf73a28243c83fbb28af04b46a3f2269f
Patrick Lioi <patrick.lioi@gmail.com>
2014-12-17 16:59:39 -0600

    Summary documentation for the Convention and TestAssembly test discovery classes.

--- src/Fixie/Convention.cs
+++ src/Fixie/Convention.cs
@@ -3,6 +3,9 @@ using Fixie.Internal;
 
 namespace Fixie
 {
+    /// <summary>
+    /// Base class for all Fixie conventions.  Subclass Convention to customize test discovery and execution.
+    /// </summary>
     public class Convention
     {
         public Convention()
@@ -18,15 +21,46 @@ namespace Fixie
             HideExceptionDetails = new AssertionLibraryExpression(Config);
         }
 
+        /// <summary>
+        /// The current state describing the convention. This state can be manipulated through
+        /// the other properties on Convention.
+        /// </summary>
         public Configuration Config { get; private set; }
 
+        /// <summary>
+        /// Defines the set of conditions that describe which classes are test classes.
+        /// </summary>
         public ClassExpression Classes { get; private set; }
+
+        /// <summary>
+        /// Defines the set of conditions that describe which test class methods are test methods.
+        /// </summary>
         public MethodExpression Methods { get; private set; }
+
+        /// <summary>
+        /// Defines the set of parameter sources, which provide inputs to parameterized test methods.
+        /// </summary>
         public ParameterSourceExpression Parameters { get; private set; }
 
+        /// <summary>
+        /// Customizes the execution of each test case.
+        /// </summary>
         public CaseBehaviorExpression CaseExecution { get; private set; }
+
+        /// <summary>
+        /// Customizes the execution of each test fixture (test class instance).
+        /// </summary>
         public FixtureBehaviorExpression FixtureExecution { get; private set; }
+
+        /// <summary>
+        /// Customizes the execution of each test class.
+        /// </summary>
         public ClassBehaviorExpression ClassExecution { get; private set; }
+
+        /// <summary>
+        /// Defines the set of types which make up an assertion library, so that test case failure stack
+        /// traces can be simplified for readability.
+        /// </summary>
         public AssertionLibraryExpression HideExceptionDetails { get; private set; }
     }
 }
--- src/Fixie/TestAssembly.cs
+++ src/Fixie/TestAssembly.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 
 namespace Fixie
 {
+    /// <summary>
+    /// Subclass TestAssembly within your test assembly in order to explicitly list which conventions to apply.
+    /// </summary>
     public class TestAssembly
     {
         readonly List<Type> conventionTypes = new List<Type>();
 
+        /// <summary>
+        /// Identifies the given convention type as being applicable to this test assembly.
+        /// </summary>
         public void Apply<TConvention>() where TConvention : Convention
         {
             conventionTypes.Add(typeof(TConvention));

6403556fff75e57aafb8ab17aed86aebd5815916
Patrick Lioi <patrick.lioi@gmail.com>
2014-12-17 15:25:44 -0600

    Summary documentation for DefaultConvention.

--- src/Fixie/Conventions/DefaultConvention.cs
+++ src/Fixie/Conventions/DefaultConvention.cs
@@ -1,5 +1,12 @@
 ﻿namespace Fixie.Conventions
 {
+    /// <summary>
+    /// The default convention is applied to a test assembly whenever the test assembly specifies no custom convention to use.
+    ///
+    /// <para>A class is a test class if its name ends with "Tests".</para>
+    ///
+    /// <para>A public instance method in a test class is a test method if it is declared void or async.</para>
+    /// </summary>
     public class DefaultConvention : Convention
     {
         public DefaultConvention()

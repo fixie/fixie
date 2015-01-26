d18edaa6f6c87bee4c6d40569a45ee4fbe8e3bef
Patrick Lioi <patrick.lioi@gmail.com>
2015-01-26 11:40:42 -0600

    Copyright notices updated by automated build to reflect the new year.

--- src/Fixie/Properties/AssemblyInfo.cs
+++ src/Fixie/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Fixie")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyCopyright("Copyright © 2013-2014 Patrick Lioi")]
+[assembly: AssemblyCopyright("Copyright © 2013-2015 Patrick Lioi")]
 [assembly: AssemblyCompany("Patrick Lioi")]
 [assembly: AssemblyConfiguration("Release")]

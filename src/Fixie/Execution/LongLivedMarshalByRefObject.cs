4de0d13cd7c777ef8168f01a78a03b82a356a207
Patrick Lioi <patrick.lioi@gmail.com>
2015-02-23 22:16:14 -0600

    Add LongLivedMarshalByRefObject, a safe alternative to MarshalByRefObject for which instances live until disposed of.

---
+++ src/Fixie/Execution/LongLivedMarshalByRefObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Remoting;
+
+namespace Fixie.Execution
+{
+    /// <summary>
+    /// Normally, inheriting from MarshalByRefObject introduces an unexpected defect
+    /// in which your instances cease to exist after they get a few minutes old.
+    /// 
+    /// For instance, when a runner provides a MarshalByRefObject to act as the test
+    /// Listener, that Listener may disappear after a long-running test finally finishes,
+    /// causing a runtime exception that interrupts the run. The Listener would no longer
+    /// be available to receive the test's results.
+    /// 
+    /// Inheriting from this alternative to MarshalByRefObject allows the instance
+    /// to be kept alive indefinitely, but you must Dispose() of it yourself when
+    /// its natural lifetime has been reached.
+    /// </summary>
+    public class LongLivedMarshalByRefObject : MarshalByRefObject, IDisposable
+    {
+        public override object InitializeLifetimeService()
+        {
+            //Returning null here causes the instance to live indefinitely.
+
+            //A consequence of keeping a MarshalByRefObject alive like this is that
+            //it must be explicitly cleaned up with a call to RemotingServices.Disconnect(this).
+            //See Dispose().
+
+            return null;
+        }
+
+        public void Dispose()
+        {
+            RemotingServices.Disconnect(this);
+        }
+    }
+}

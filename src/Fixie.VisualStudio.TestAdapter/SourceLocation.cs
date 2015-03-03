14ac1961223e5b6be487102e32ee3686be6d3324
Patrick Lioi <patrick.lioi@gmail.com>
2015-03-02 22:00:34 -0600

    SourceLocationProvider determines the starting line number of a MethodGroup.

---
+++ src/Fixie.VisualStudio.TestAdapter/SourceLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace Fixie.VisualStudio.TestAdapter
+{
+    public class SourceLocation
+    {
+        public SourceLocation(string codeFilePath, int lineNumber)
+        {
+            CodeFilePath = codeFilePath;
+            LineNumber = lineNumber;
+        }
+
+        public string CodeFilePath { get; private set; }
+        public int LineNumber { get; private set; }
+    }
+}

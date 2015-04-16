571aefb38a198da0ac7b1cf7d32e944929892823
Mark Henke <gilligan128@gmail.com>
2015-04-15 20:20:25 -0400

    Added Trait and TraitSource

---
+++ src/Fixie/Trait.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fixie
+{
+    /// <summary>
+    /// Represents a named oiece of metadata of a test method.
+    /// </summary>
+    ///<remarks>This is what you would use to define categories.</remarks>
+    public class Trait
+    {
+        public Trait(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; private set; }
+        public string Value { get; private set; }
+    }
+}
---
+++ src/Fixie/TraitSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fixie
+{
+    /// <summary>
+    /// Defines a source of traits for a test method.
+    /// </summary>
+    public interface TraitSource
+    {
+        IEnumerable<Trait> GetTraits(MethodInfo methodInfo);
+    }
+
+    /// <summary>
+    /// Defines a source of traits for a test method.
+    /// </summary>
+    /// <param name="method"></param>
+    /// <returns></returns>
+    public delegate IEnumerable<Trait> TraitSourceFunc   (MethodInfo method);
+}

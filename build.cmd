d71701e3696531e02906d22354bccf0669a6ab75
Patrick Lioi <patrick.lioi@gmail.com>
2015-11-11 20:43:48 -0600

    The build script restores NuGet packages prior to running the psake script.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-.\src\.nuget\nuget.exe restore src\Fixie.sln -source "https://nuget.org/api/v2/" -RequireConsent -o "src\packages"
+.\tools\NuGet.exe restore src\Fixie.sln -source "https://nuget.org/api/v2/" -RequireConsent -o "src\packages"
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0\src\packages\psake.4.4.2\tools\psake.ps1' %*; if ($psake.build_success -eq $false) { write-host "Build Failed!" -fore RED; exit 1 } else { exit 0 }"

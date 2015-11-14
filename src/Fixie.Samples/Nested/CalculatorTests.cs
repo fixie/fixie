6c8bb54fff062df897195308afa92d17335f2146
Patrick Lioi <patrick.lioi@gmail.com>
2015-11-14 11:31:38 -0600

    Add sample of a test class with nested test classes, to enable integration testing of TD.NET integration in the event that a user targets a class containing nested test classes.

---
+++ src/Fixie.Samples/Nested/CalculatorTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using Should;
+
+namespace Fixie.Samples.Nested
+{
+    public class CalculatorTests
+    {
+        class AddingTests : IDisposable
+        {
+            readonly Calculator calculator;
+            readonly StringBuilder log;
+
+            public AddingTests()
+            {
+                calculator = new Calculator();
+                log = new StringBuilder();
+                log.WhereAmI();
+            }
+
+            public void ShouldAdd()
+            {
+                log.WhereAmI();
+                calculator.Add(2, 3).ShouldEqual(5);
+            }
+
+            public void Dispose()
+            {
+                log.WhereAmI();
+
+                log.ShouldHaveLines(
+                    ".ctor",
+                    "ShouldAdd",
+                    "Dispose");
+            }
+        }
+
+        class SubtractingTests : IDisposable
+        {
+            readonly Calculator calculator;
+            readonly StringBuilder log;
+
+            public SubtractingTests()
+            {
+                calculator = new Calculator();
+                log = new StringBuilder();
+                log.WhereAmI();
+            }
+
+            public void ShouldSubtract()
+            {
+                log.WhereAmI();
+                calculator.Subtract(5, 3).ShouldEqual(2);
+            }
+
+            public void Dispose()
+            {
+                log.WhereAmI();
+
+                log.ShouldHaveLines(
+                    ".ctor",
+                    "ShouldSubtract",
+                    "Dispose");
+            }
+        }
+    }
+}

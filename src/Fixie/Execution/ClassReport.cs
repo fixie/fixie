50e5ebfdf6d7d23b85a7a5d2abdf991070d9a81b
Patrick Lioi <patrick.lioi@gmail.com>
2015-12-21 12:17:05 -0600

    Now that report objects live entirely within the original AppDomain, they no longer need to be marked [Serializable].

--- src/Fixie/Execution/ClassReport.cs
+++ src/Fixie/Execution/ClassReport.cs
@@ -4,7 +4,6 @@ using System.Linq;
 
 namespace Fixie.Execution
 {
-    [Serializable]
     public class ClassReport
     {
         readonly List<CaseResult> cases;
--- src/Fixie/Execution/Report.cs
+++ src/Fixie/Execution/Report.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Fixie.Execution
 {
-    [Serializable]
     public class Report
     {
         readonly List<AssemblyReport> assemblies;

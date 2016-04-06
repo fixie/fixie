29fdd297e84f06928d33be6db12c713028f7038c
Patrick Lioi <patrick.lioi@gmail.com>
2016-04-05 21:15:25 -0500

    Collapse redundant Bus members, breaking its dependency on specific message types.

--- src/Fixie/Internal/Bus.cs
+++ src/Fixie/Internal/Bus.cs
@@ -6,24 +6,12 @@ namespace Fixie.Internal
     {
         readonly object listener;
 
-        public Bus(object listener) { this.listener = listener; }
+        public Bus(object listener)
+        {
+            this.listener = listener;
+        }
 
-        public void Publish(AssemblyInfo message)
-            => Publish<AssemblyInfo>(message);
-
-        public void Publish(SkipResult message)
-            => Publish<SkipResult>(message);
-
-        public void Publish(PassResult message)
-            => Publish<PassResult>(message);
-
-        public void Publish(FailResult message)
-            => Publish<FailResult>(message);
-
-        public void Publish(AssemblyCompleted message)
-            => Publish<AssemblyCompleted>(message);
-
-        void Publish<TMessage>(TMessage message) where TMessage : Message
+        public void Publish<TMessage>(TMessage message) where TMessage : Message
             => (listener as Handler<TMessage>)?.Handle(message);
     }
 }

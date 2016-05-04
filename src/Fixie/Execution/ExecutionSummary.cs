9555514c3abf9b48432aea48d190d606bd29d86d
Patrick Lioi <patrick.lioi@gmail.com>
2016-05-04 16:52:40 -0500

    An ExecutionSummary can accumulate CaseStatus counts and Case durations.

---
+++ src/Fixie/Execution/ExecutionSummary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fixie.Execution
+{
+    public class ExecutionSummary
+    {
+        public ExecutionSummary()
+        {
+            Passed = 0;
+            Failed = 0;
+            Skipped = 0;
+            Duration = TimeSpan.Zero;
+        }
+
+        public int Passed { get; private set; }
+        public int Failed { get; private set; }
+        public int Skipped { get; private set; }
+        public int Total => Passed + Failed + Skipped;
+        public TimeSpan Duration { get; private set; }
+
+        public void Add(CaseCompleted message)
+        {
+            if (message.Status == CaseStatus.Passed)
+                Passed += 1;
+            else if (message.Status == CaseStatus.Failed)
+                Failed += 1;
+            else if (message.Status == CaseStatus.Skipped)
+                Skipped += 1;
+
+            Duration += message.Duration;
+        }
+    }
+}

1dc4049894d3b63c2fc609ee46b5dcdecd59f0b7
Patrick Lioi <patrick.lioi@gmail.com>
2016-05-20 08:00:49 -0500

    Remove deprecated test from ConsoleListenerTests: the behavior under test is better covered already by ExecutionSummaryTests, as ConsoleListener is no longer directly responsible for the behavior.

--- src/Fixie.Tests/ConsoleRunner/ConsoleListenerTests.cs
+++ src/Fixie.Tests/ConsoleRunner/ConsoleListenerTests.cs
@@ -52,47 +52,6 @@
             }
         }
 
-        public void ShouldNotReportSkipCountsWhenZeroTestsHaveBeenSkipped()
-        {
-            using (var console = new RedirectedConsole())
-            {
-                var listener = new ConsoleListener();
-                var convention = SampleTestClassConvention.Build();
-
-                convention
-                    .Methods
-                    .Where(method => !method.Has<SkipAttribute>());
-
-                typeof(SampleTestClass).Run(listener, convention);
-
-                var testClass = FullName<SampleTestClass>();
-
-                console.Lines()
-                       .Select(CleanBrittleValues)
-                       .ShouldEqual(
-                           "------ Testing Assembly Fixie.Tests.dll ------",
-                           "",
-                           "Console.Out: Fail",
-                           "Console.Error: Fail",
-                           "Console.Out: FailByAssertion",
-                           "Console.Error: FailByAssertion",
-                           "Console.Out: Pass",
-                           "Console.Error: Pass",
-
-                           "Test '" + testClass + ".Fail' failed: Fixie.Tests.FailureException",
-                           "'Fail' failed!",
-                           At<SampleTestClass>("Fail()"),
-                           "",
-                           "Test '" + testClass + ".FailByAssertion' failed: Should.Core.Exceptions.EqualException",
-                           "Assert.Equal() Failure",
-                           "Expected: 2",
-                           "Actual:   1",
-                           At<SampleTestClass>("FailByAssertion()"),
-                           "",
-                           "1 passed, 2 failed, took 1.23 seconds (" + Framework.Version + ").");
-            }
-        }
-
         static string CleanBrittleValues(string actualRawContent)
         {
             //Avoid brittle assertion introduced by test duration.

887ef1461c651b69cde42056de935d8144d620dc
Patrick Lioi <patrick.lioi@gmail.com>
2016-06-29 09:50:51 -0500

    Reintroduce C# files to the Fixie.VisualStudio.TestAdapter project.

---
+++ src/Fixie.VisualStudio.TestAdapter/LoggingExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Fixie.VisualStudio.TestAdapter
+{
+    using System;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+    public static class LoggingExtensions
+    {
+        public static void Info(this IMessageLogger logger, string message)
+            => logger.SendMessage(TestMessageLevel.Informational, message);
+
+        public static void Error(this IMessageLogger logger, Exception exception)
+            => logger.SendMessage(TestMessageLevel.Error, exception.ToString());
+
+        public static void Version(this IMessageLogger logger)
+            => logger.Info(Framework.Version);
+    }
+}
---
+++ src/Fixie.VisualStudio.TestAdapter/VisualStudioDiscoveryListener.cs
@@ -0,0 +1,47 @@
+namespace Fixie.VisualStudio.TestAdapter
+{
+    using System;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+    using Execution;
+
+    public class VisualStudioDiscoveryListener : Handler<MethodGroupDiscovered>
+    {
+        readonly IMessageLogger log;
+        readonly ITestCaseDiscoverySink discoverySink;
+        readonly string assemblyPath;
+        readonly SourceLocationProvider sourceLocationProvider;
+
+        public VisualStudioDiscoveryListener(IMessageLogger log, ITestCaseDiscoverySink discoverySink, string assemblyPath)
+        {
+            this.log = log;
+            this.discoverySink = discoverySink;
+            this.assemblyPath = assemblyPath;
+            sourceLocationProvider = new SourceLocationProvider(assemblyPath);
+        }
+
+        public void Handle(MethodGroupDiscovered message)
+        {
+            var methodGroup = message.MethodGroup;
+
+            var testCase = new TestCase(methodGroup.FullName, VsTestExecutor.Uri, assemblyPath);
+
+            try
+            {
+                SourceLocation sourceLocation;
+                if (sourceLocationProvider.TryGetSourceLocation(methodGroup, out sourceLocation))
+                {
+                    testCase.CodeFilePath = sourceLocation.CodeFilePath;
+                    testCase.LineNumber = sourceLocation.LineNumber;
+                }
+            }
+            catch (Exception exception)
+            {
+                log.Error(exception);
+            }
+
+            discoverySink.SendTestCase(testCase);
+        }
+    }
+}

53d4df3d7f2c9b3a594b8462a34d1178c1bc5d53
Patrick Lioi <patrick.lioi@gmail.com>
2016-08-06 20:16:00 -0500

    Now that Message types don't cross the AppDomain boundary, there is no longer any need to sanity check that they could cross the boundary successfully.

--- src/Fixie.Tests/Execution/RunnerAppDomainCommunicationTests.cs
+++ src/Fixie.Tests/Execution/RunnerAppDomainCommunicationTests.cs
@@ -1,28 +1,10 @@
 namespace Fixie.Tests.Execution
 {
-    using System.Linq;
-    using Fixie.Execution;
     using Fixie.Internal;
     using Fixie.VisualStudio.TestAdapter.Wrappers;
-    using Should;
 
     public class RunnerAppDomainCommunicationTests
     {
-        public void ShouldAllowRunnersInOtherAppDomainsToProvideTheirOwnMessageHandlers()
-        {
-            var handlerTypes = typeof(Message)
-                .Assembly
-                .GetTypes()
-                .Where(type => type.IsClass && typeof(Message).IsAssignableFrom(type))
-                .Select(messageType => typeof(Handler<>).MakeGenericType(messageType))
-                .ToArray();
-
-            handlerTypes.ShouldNotBeEmpty();
-
-            foreach (var handlerType in handlerTypes)
-                handlerType.ShouldBeSafeAppDomainCommunicationInterface();
-        }
-
         public void ShouldAllowRunnersInOtherAppDomainsToPerformTestDiscoveryAndExecutionThroughExecutionProxy()
         {
             typeof(ExecutionProxy).ShouldBeSafeAppDomainCommunicationInterface();

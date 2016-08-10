b3f78b38eaf17805bc42031aed9c7dc3bf86774e
Patrick Lioi <patrick.lioi@gmail.com>
2016-08-10 13:36:31 -0500

    The DesignTimeSink can write "dotnet test protocol" messages to an underlying BinaryWriter, and such messages can be written from either side of an AppDomain boundary.

---
+++ src/Fixie.Runner/DesignTimeSink.cs
@@ -0,0 +1,54 @@
+﻿namespace Fixie.Runner
+{
+    using System.IO;
+    using Execution;
+    using Microsoft.Extensions.Testing.Abstractions;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using Message = Microsoft.Extensions.Testing.Abstractions.Message;
+
+    public interface IDesignTimeSink
+    {
+        void Send(string message);
+    }
+
+    public class DesignTimeSink : LongLivedMarshalByRefObject, IDesignTimeSink
+    {
+        readonly BinaryWriter writer;
+
+        public DesignTimeSink(BinaryWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public void Send(string message)
+            => writer.Write(message);
+    }
+
+    public static class DesignTimeSinkExtensions
+    {
+        public static void SendWaitingCommand(this IDesignTimeSink sink)
+           => Send(sink, "TestRunner.WaitingCommand");
+
+        public static void SendTestCompleted(this IDesignTimeSink sink)
+            => Send(sink, "TestRunner.TestCompleted");
+
+        public static void SendTestFound(this IDesignTimeSink sink, Test test)
+            => Send(sink, "TestDiscovery.TestFound", test);
+
+        public static void SendTestStarted(this IDesignTimeSink sink, Test test)
+            => Send(sink, "TestExecution.TestStarted", test);
+
+        public static void SendTestResult(this IDesignTimeSink sink, TestResult testResult)
+            => Send(sink, "TestExecution.TestResult", testResult);
+
+        static void Send(IDesignTimeSink sink, string messageType, object payload = null)
+        {
+            sink.Send(JsonConvert.SerializeObject(new Message
+            {
+                MessageType = messageType,
+                Payload = payload == null ? null : JToken.FromObject(payload)
+            }));
+        }
+    }
+}

d4ee618c2401c66040942efc1ec8fde0311b6304
Patrick Lioi <patrick.lioi@gmail.com>
2016-09-20 20:31:56 -0500

    Move ExecutionPlan from Fixie to Fixie.Execution.

--- src/Fixie.Execution/ExecutionPlan.cs
+++ src/Fixie.Execution/ExecutionPlan.cs
@@ -1,8 +1,8 @@
-namespace Fixie.Internal
+namespace Fixie.Execution
 {
     using System.Linq;
-    using Behaviors;
-    using System.Collections.Generic;
+    using Internal;
+    using Internal.Behaviors;
 
     public class ExecutionPlan
     {

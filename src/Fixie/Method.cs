1e456433df2dd4b77aca53f472a9baf189493ae4
Patrick Lioi <patrick.lioi@gmail.com>
2016-09-28 22:32:02 -0500

    The Method class wraps a Type and MethodInfo, to phase out the need to use the deprecated MethodInfo.ReflectedType property.

---
+++ src/Fixie/Method.cs
@@ -0,0 +1,17 @@
+﻿namespace Fixie
+{
+    using System;
+    using System.Reflection;
+
+    public class Method
+    {
+        public Type Class { get; }
+        public MethodInfo MethodInfo { get; }
+
+        public Method(Type @class, MethodInfo method)
+        {
+            Class = @class;
+            MethodInfo = method;
+        }
+    }
+}

c41753df7f65eecc30d1e85c7d58498a4b869cae
Patrick Lioi <patrick.lioi@gmail.com>
2016-10-03 21:14:06 -0500

    The primary Fixie assembly cross-compiles for both net45 and netstandard1.3.

---
+++ src/Fixie/ReflectionShim.cs
@@ -0,0 +1,41 @@
+﻿namespace Fixie
+{
+#if NET45
+    using System;
+    using System.Reflection;
+
+    static class ReflectionShim
+    {
+        public static Assembly Assembly(this Type type)
+            => type.Assembly;
+
+        public static bool IsEnum(this Type conversionType)
+            => conversionType.IsEnum;
+
+        public static bool IsValueType(this Type type)
+            => type.IsValueType;
+    }
+#elif NETSTANDARD1_3
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    static class ReflectionShim
+    {
+        public static Assembly Assembly(this Type type)
+            => type.GetTypeInfo().Assembly;
+
+        public static bool IsEnum(this Type conversionType)
+            => conversionType.GetTypeInfo().IsEnum;
+
+        public static bool IsValueType(this Type type)
+            => type.GetTypeInfo().IsValueType;
+
+        public static IEnumerable<T> GetCustomAttributes<T>(this Type type, bool inherit) where T : Attribute
+            => type.GetTypeInfo().GetCustomAttributes<T>(inherit);
+
+        public static PropertyInfo GetProperty(this Type type, String name)
+            => TypeExtensions.GetProperty(type, name);
+    }
+#endif
+}

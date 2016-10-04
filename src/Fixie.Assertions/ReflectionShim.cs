99735830d3762f321589ccd1ae54fcfe77ec1859
Patrick Lioi <patrick.lioi@gmail.com>
2016-10-03 20:23:10 -0500

    Fixie.Assertions cross-compiles for both net45 and netstandard1.3.

--- src/Fixie.Assertions/ReflectionShim.cs
+++ src/Fixie.Assertions/ReflectionShim.cs
@@ -1,5 +1,6 @@
 ﻿namespace Fixie.Assertions
 {
+#if NET45
     using System;
 
     static class ReflectionShim
@@ -10,4 +11,26 @@
         public static bool IsValueType(this Type type)
             => type.IsValueType;
     }
+#elif NETSTANDARD1_3
+    using System;
+    using System.Reflection;
+
+    static class ReflectionShim
+    {
+        public static bool IsGenericType(this Type type)
+            => type.GetTypeInfo().IsGenericType;
+
+        public static bool IsValueType(this Type type)
+            => type.GetTypeInfo().IsValueType;
+
+        public static MethodInfo GetMethod(this Type type, String name)
+           => TypeExtensions.GetMethod(type, name);
+
+        public static MethodInfo GetMethod(this Type type, String name, Type[] types)
+            => TypeExtensions.GetMethod(type, name, types);
+
+        public static bool IsAssignableFrom(this Type type, Type c)
+            => TypeExtensions.IsAssignableFrom(type, c);
+    }
+#endif
 }

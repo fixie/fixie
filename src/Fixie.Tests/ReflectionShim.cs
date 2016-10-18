a38342babbb42b0f7addf5fefcf6248b913133e5
Patrick Lioi <patrick.lioi@gmail.com>
2016-10-17 21:37:29 -0500

    Define reflection shims within Fixie.Tests for Type.GetCustomAttribute<T>(bool inherit) and Type.GetProperties().

--- src/Fixie.Tests/ReflectionShim.cs
+++ src/Fixie.Tests/ReflectionShim.cs
@@ -17,6 +17,7 @@
     }
 #else
     using System;
+    using System.Collections.Generic;
     using System.Reflection;
 
     static class ReflectionShim
@@ -29,6 +30,12 @@
 
         public static bool IsNestedPrivate(this Type type)
             => type.GetTypeInfo().IsNestedPrivate;
+
+        public static T GetCustomAttribute<T>(this Type type, bool inherit) where T : Attribute
+            => type.GetTypeInfo().GetCustomAttribute<T>(inherit);
+
+        public static PropertyInfo[] GetProperties(this Type type)
+            => type.GetTypeInfo().GetProperties();
     }
 #endif
 }

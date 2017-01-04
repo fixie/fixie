553fb7a8032b40a54daefb26a5522c8865bfb432
Patrick Lioi <patrick.lioi@gmail.com>
2017-01-04 08:12:02 -0600

    An ExecutionSummary can accumulate CaseStatus counts and Case durations.

---
+++ src/Fixie.Execution/ExecutionSummary.cs
@@ -0,0 +1,34 @@
+﻿namespace Fixie.Execution
+{
+    using System;
+    using System.Text;
+
+    public class ExecutionSummary
+    {
+        public ExecutionSummary()
+        {
+            Passed = 0;
+            Failed = 0;
+            Skipped = 0;
+            Duration = TimeSpan.Zero;
+        }
+
+        public int Passed { get; private set; }
+        public int Failed { get; private set; }
+        public int Skipped { get; private set; }
+        public int Total => Passed + Failed + Skipped;
+        public TimeSpan Duration { get; private set; }
+
+        public void Add(CaseCompleted message)
+        {
+            if (message.Status == CaseStatus.Passed)
+                Passed += 1;
+            else if (message.Status == CaseStatus.Failed)
+                Failed += 1;
+            else if (message.Status == CaseStatus.Skipped)
+                Skipped += 1;
+
+            Duration += message.Duration;
+        }
+    }
+}
---
+++ src/Fixie.Tests/Execution/ExecutionSummaryTests.cs
@@ -0,0 +1,49 @@
+namespace Fixie.Tests.Execution
+{
+    using System;
+    using Fixie.Execution;
+    using Should;
+
+    public class ExecutionSummaryTests
+    {
+        public void ShouldAccumulateCaseStatusCountsAndDurations()
+        {
+            var convention = SelfTestConvention.Build();
+            convention.CaseExecution.Skip(x => x.Method.Name.StartsWith("Skip"));
+
+            var listener = new StubExecutionSummaryListener();
+
+            typeof(SampleTestClass).Run(listener, convention);
+
+            var summary = listener.Summary;
+
+            summary.Passed.ShouldEqual(1);
+            summary.Failed.ShouldEqual(2);
+            summary.Skipped.ShouldEqual(3);
+            summary.Total.ShouldEqual(6);
+            summary.Duration.ShouldEqual(listener.ExpectedDuration);
+        }
+
+        class StubExecutionSummaryListener : Handler<CaseCompleted>
+        {
+            public TimeSpan ExpectedDuration { get; private set; }
+            public ExecutionSummary Summary { get; } = new ExecutionSummary();
+
+            public void Handle(CaseCompleted message)
+            {
+                ExpectedDuration += message.Duration;
+                Summary.Add(message);
+            }
+        }
+
+        class SampleTestClass
+        {
+            public void Pass() { }
+            public void FailA() { throw new FailureException(); }
+            public void FailB() { throw new FailureException(); }
+            public void SkipA() { }
+            public void SkipB() { }
+            public void SkipC() { }
+        }
+    }
+}

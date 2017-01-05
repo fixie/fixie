5e868e3f4089715bd54b715be1f6950be290aeb7
Patrick Lioi <patrick.lioi@gmail.com>
2017-01-05 10:49:52 -0600

    Phase out deprecated property ExceptionInfo.DisplayName by instead having CompoundException calculate the value itself directly against the original Exception object.

--- src/Fixie.Execution/CompoundException.cs
+++ src/Fixie.Execution/CompoundException.cs
@@ -9,15 +9,18 @@ namespace Fixie.Execution
     {
         public CompoundException(IReadOnlyCollection<Exception> exceptions, AssertionLibraryFilter filter)
         {
+            var primary = exceptions.First();
             var all = exceptions.Select(x => new ExceptionInfo(x, filter)).ToArray();
             PrimaryException = all.First();
             SecondaryExceptions = all.Skip(1).ToArray();
             CompoundStackTrace = GetCompoundStackTrace(all);
+
+            DisplayName = filter.DisplayName(primary);
         }
 
         [Obsolete]
         public ExceptionInfo PrimaryException { get; private set; }
-        public string DisplayName => PrimaryException.DisplayName;
+        public string DisplayName { get; }
         public string Type => PrimaryException.Type;
         public string Message => PrimaryException.Message;
 
--- src/Fixie.Execution/ExceptionInfo.cs
+++ src/Fixie.Execution/ExceptionInfo.cs
@@ -7,14 +7,12 @@ namespace Fixie.Execution
         public ExceptionInfo(Exception exception, AssertionLibraryFilter filter)
         {
             Type = exception.GetType().FullName;
-            DisplayName = filter.DisplayName(exception);
             Message = exception.Message;
             StackTrace = filter.FilterStackTrace(exception);
             InnerException = exception.InnerException == null ? null : new ExceptionInfo(exception.InnerException, filter);
         }
 
         public string Type { get; private set; }
-        public string DisplayName { get; private set; }
         public string Message { get; private set; }
         public string StackTrace { get; private set; }
         public ExceptionInfo InnerException { get; private set; }

b7fe00b08ed0b7406aef6fcbe99aad0e1cc51eff
Patrick Lioi <patrick.lioi@gmail.com>
2017-01-25 09:11:05 -0600

    Simplify AppDomainCommunicationAssertions to better match the current minimal state of AppDomain communication.

--- src/Fixie.Tests/Execution/AppDomainCommunicationAssertions.cs
+++ src/Fixie.Tests/Execution/AppDomainCommunicationAssertions.cs
@@ -1,15 +1,24 @@
 namespace Fixie.Tests.Execution
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Reflection;
     using Assertions;
 
     public static class AppDomainCommunicationAssertions
     {
-        const BindingFlags AllMembers =
-            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+        static readonly Type[] KnownSafeTypes =
+        {
+            typeof(string),
+            typeof(string[]),
+            typeof(int),
+            typeof(TimeSpan),
+
+            //Because we cannot fully automate verification of all cross-AppDomain
+            //argument/return types, anything declared as object[] is assumed to be
+            //OK and it is the caller's responsibility to pass safe types from
+            //loadable assemblies.
+            typeof(object[])
+        };
 
         public static void ShouldBeSafeAppDomainCommunicationInterface(this Type crossAppDomainInterfaceType)
         {
@@ -23,7 +32,7 @@ namespace Fixie.Tests.Execution
 
                 if (!method.IsVoid())
                 {
-                    IsSafeForAppDomainCommunication(method.ReturnType)
+                    KnownSafeTypes.Contains(method.ReturnType)
                         .ShouldBeTrue(
                             $"{method.ReturnType} is not an acceptable return type for method {crossAppDomainInterfaceType.FullName}.{method.Name} " +
                             "because it will not successfully cross AppDomain boundaries.");
@@ -31,79 +40,12 @@ namespace Fixie.Tests.Execution
 
                 foreach (var parameterType in method.GetParameters().Select(x => x.ParameterType))
                 {
-                    IsSafeForAppDomainCommunication(parameterType)
+                    KnownSafeTypes.Contains(parameterType)
                         .ShouldBeTrue(
                             $"{parameterType} is not an acceptable parameter type for method {crossAppDomainInterfaceType.FullName}.{method.Name} " +
                             "because it will not successfully cross AppDomain boundaries.");
                 }
             }
-        }
-
-        static bool IsSafeForAppDomainCommunication(this Type type)
-        {
-            return IsSafeForAppDomainCommunication(type, new HashSet<Type>());
-        }
-
-        static bool IsSafeForAppDomainCommunication(Type type, HashSet<Type> visitedTypes)
-        {
-            //This test is meant to catch *typical* mistakes with AppDomain communication, but does not
-            //perfectly vet all possible candiate types:
-            //
-            //  1. Checking for [Serializable] does not guarantee a type is truly serializable.
-            //  2. Not every IReadOnlyList<T> will necessarily serialize, but it is convenient to
-            //     allow it as the most likely implementations *are* serializable.
-            //  3. Although reflection types like Assembly and Type are serializable, they are
-            //     also not acceptable for AppDomain communication because they can cause
-            //     assembly load failures at runtime.
-
-            if (type == typeof(object[]))
-            {
-                //Because we cannot fully automate all of these scenarios programmatically,
-                //and because params object[] are quite useful to allow for the sake of
-                //passing Listener constructor arguments across the AppDomain boundary,
-                //we assume these are OK and depend on the caller to pass safe types
-                //from loadable assemblies.
-                return true;
-            }
-
-            visitedTypes.Add(type);
-
-            if (type == typeof(object))
-                return false;
-
-            if (type == typeof(Type))
-                return false;
-
-            if (type.IsInNamespace("System.Reflection"))
-                return false;
-
-            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IReadOnlyList<>))
-            {
-                if (!IsSafeForAppDomainCommunication(type.GetGenericArguments().Single(), visitedTypes))
-                    return false;
-            }
-            else if (type.IsArray)
-            {
-                if (!IsSafeForAppDomainCommunication(type.GetElementType(), visitedTypes))
-                    return false;
-            }
-            else if (!type.HasOrInherits<SerializableAttribute>())
-            {
-                return false;
-            }
-
-            if (!type.IsArray && type.IsInNamespace("Fixie"))
-            {
-                foreach (var property in type.GetProperties(AllMembers))
-                {
-                    var propertyType = property.PropertyType;
-
-                    if (!visitedTypes.Contains(propertyType) && !IsSafeForAppDomainCommunication(propertyType, visitedTypes))
-                        return false;
-                }
-            }
-
-            return true;
         }
     }
 }

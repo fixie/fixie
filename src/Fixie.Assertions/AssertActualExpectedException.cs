348a5986bdf82fd9ad92b30e2e523156623e1c84
Patrick Lioi <patrick.lioi@gmail.com>
2017-04-22 14:33:35 -0700

    Reintroduce code files to the Fixie.Assertions project.

---
+++ src/Fixie.Assertions/AssertActualExpectedException.cs
@@ -0,0 +1,79 @@
+namespace Fixie.Assertions
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class AssertActualExpectedException : AssertException
+    {
+        readonly string expected;
+        readonly string actual;
+        readonly string differencePosition = "";
+
+        public AssertActualExpectedException(object expected, object actual)
+            : this(expected, actual, "Assertion Failure")
+        {
+        }
+
+        public AssertActualExpectedException(object expected,
+                                             object actual,
+                                             string userMessage)
+            : base(userMessage)
+        {
+            var enumerableActual = actual as IEnumerable;
+            var enumerableExpected = expected as IEnumerable;
+
+            if (enumerableActual != null && enumerableExpected != null)
+            {
+                var comparer = new EnumerableEqualityComparer();
+                comparer.Equals(enumerableActual, enumerableExpected);
+
+                differencePosition = "Position: First difference is at position " + comparer.Position + Environment.NewLine;
+            }
+
+            this.actual = actual == null ? null : ConvertToString(actual);
+            this.expected = expected == null ? null : ConvertToString(expected);
+
+            if (actual != null &&
+                expected != null &&
+                actual.ToString() == expected.ToString() &&
+                actual.GetType() != expected.GetType())
+            {
+                this.actual += $" ({actual.GetType().FullName})";
+                this.expected += $" ({expected.GetType().FullName})";
+            }
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return string.Format("{0}{4}{1}Expected: {2}{4}Actual:   {3}",
+                                     base.Message,
+                                     differencePosition,
+                                     FormatMultiLine(expected ?? "(null)"),
+                                     FormatMultiLine(actual ?? "(null)"),
+                                     Environment.NewLine);
+            }
+        }
+
+        static string ConvertToString(object value)
+        {
+            var valueArray = value as Array;
+            if (valueArray == null)
+                return value.ToString();
+
+            var valueStrings = new List<string>();
+
+            foreach (object valueObject in valueArray)
+                valueStrings.Add(valueObject == null ? "(null)" : valueObject.ToString());
+
+            return value.GetType().FullName + " { " + String.Join(", ", valueStrings.ToArray()) + " }";
+        }
+
+        static string FormatMultiLine(string value)
+        {
+            return value.Replace(Environment.NewLine, Environment.NewLine + "          ");
+        }
+    }
+}
---
+++ src/Fixie.Assertions/EnumerableEqualityComparer.cs
@@ -0,0 +1,69 @@
+namespace Fixie.Assertions
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    class EnumerableEqualityComparer : IEqualityComparer<IEnumerable>
+    {
+        public int Position { get; set; }
+
+        public bool Equals(IEnumerable x, IEnumerable y)
+        {
+            var enumeratorX = x.GetEnumerator();
+            var enumeratorY = y.GetEnumerator();
+
+            Position = 0;
+
+            while (true)
+            {
+                bool hasNextX = enumeratorX.MoveNext();
+                bool hasNextY = enumeratorY.MoveNext();
+
+                if (!hasNextX || !hasNextY)
+                    return hasNextX == hasNextY;
+
+                if (enumeratorX.Current != null || enumeratorY.Current != null)
+                {
+                    if (enumeratorX.Current != null && enumeratorY.Current == null)
+                        return false;
+
+                    if (enumeratorX.Current == null)
+                        return false;
+
+                    var xType = enumeratorX.Current.GetType();
+                    var yType = enumeratorY.Current.GetType();
+
+                    if (xType.IsAssignableFrom(yType))
+                    {
+                        if (!Equals(enumeratorX.Current, enumeratorY.Current, xType))
+                            return false;
+                    }
+                    else if (yType.IsAssignableFrom(xType))
+                    {
+                        if (!Equals(enumeratorY.Current, enumeratorX.Current, yType))
+                            return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                Position++;
+            }
+        }
+
+        public int GetHashCode(IEnumerable obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        static bool Equals(object a, object b, Type baseType)
+        {
+            var assertComparerType = typeof(AssertEqualityComparer<>).MakeGenericType(baseType);
+            var assertComparer = Activator.CreateInstance(assertComparerType);
+            var compareMethod = assertComparerType.GetMethod("Equals", new [] { baseType, baseType });
+            return (bool)compareMethod.Invoke(assertComparer, new[] { a, b });
+        }
+    }
+}

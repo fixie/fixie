1ca3fe9bd1c003a3f7ea6417b6f446b055e4ca86
Patrick Lioi <patrick.lioi@gmail.com>
2017-05-23 22:48:00 -0700

    The Fixie.Samples project targets netstandard1.3

---
+++ src/Fixie.Samples/ReflectionShim.cs
@@ -0,0 +1,11 @@
+﻿namespace Fixie.Samples
+{
+    using System;
+    using System.Reflection;
+
+    static class ReflectionShim
+    {
+        public static bool IsGenericType(this Type type)
+            => type.GetTypeInfo().IsGenericType;
+    }
+}

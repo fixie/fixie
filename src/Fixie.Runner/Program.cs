2fc87865b2a60b9b89ee4081ef6e4cb99159544a
Patrick Lioi <patrick.lioi@gmail.com>
2017-06-16 19:10:31 -0700

    Add project Fixie.Runner to implement a `dotnet fixie` CLI tool, using the "Console App (.NET Core)" Visual Studio Template.

---
+++ src/Fixie.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fixie.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

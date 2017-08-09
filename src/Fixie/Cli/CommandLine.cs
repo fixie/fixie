1f421ab352f697ad8a48d5e57fc3100a193f3cd4
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-09 08:01:36 -0500

    Command line argument parsing methods are overloaded to allow for deserializing to a compile-time type T or a run-time Type.

--- src/Fixie/Cli/CommandLine.cs
+++ src/Fixie/Cli/CommandLine.cs
@@ -1,15 +1,27 @@
 ﻿namespace Fixie.Cli
 {
+    using System;
+
     class CommandLine
     {
         public static T Parse<T>(string[] arguments) where T : class
         {
-            return Parse<T>(arguments, out string[] _);
+            return (T)Parse(typeof(T), arguments);
         }
 
         public static T Parse<T>(string[] arguments, out string[] unusedArguments) where T : class
         {
-            var parser = new Parser<T>(arguments);
+            return (T)Parse(typeof(T), arguments, out unusedArguments);
+        }
+
+        public static object Parse(Type type, string[] arguments)
+        {
+            return Parse(type, arguments, out string[] _);
+        }
+
+        public static object Parse(Type type, string[] arguments, out string[] unusedArguments)
+        {
+            var parser = new Parser(type, arguments);
 
             unusedArguments = parser.UnusedArguments.ToArray();
 

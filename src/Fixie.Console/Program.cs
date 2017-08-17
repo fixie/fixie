2b1e5c4652d5199a952fb9be80f9040c6f116af3
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-17 08:20:38 -0500

    Now that all console running is performed through the `dotnet fixie` command, the secondary Fixie.Console.exe invoked by `dotnet fixie` need not output its own command line argument usage text. Such output would only take place if there were a bug in `dotnet fixie`'s own passing of arguments to the secondary process, and would be confusing to the user because it would suggest the problem was on their end and that the solution would be to supply arguments incorrectly to `dotnet fixie`.

--- src/Fixie.Console/Program.cs
+++ src/Fixie.Console/Program.cs
@@ -20,15 +20,6 @@
                 using (var environment = new ExecutionEnvironment(options.AssemblyPath))
                     return environment.RunAssembly(arguments);
             }
-            catch (CommandLineException exception)
-            {
-                using (Foreground.Red)
-                    WriteLine(exception.Message);
-
-                Help();
-
-                return FatalError;
-            }
             catch (Exception exception)
             {
                 using (Foreground.Red)
@@ -36,33 +27,6 @@
 
                 return FatalError;
             }
-        }
-
-        static void Help()
-        {
-            WriteLine();
-            WriteLine("Usage: Fixie.Console.exe assembly-path [patterns]... [--report path] [--team-city <on|off>] [convention arguments]...");
-            WriteLine();
-            WriteLine();
-            WriteLine("    assembly-path");
-            WriteLine("        A path indicating the test assembly file. Exactly one test");
-            WriteLine("        assembly must be specified.");
-            WriteLine();
-            WriteLine("    patterns");
-            WriteLine("        Zero or more test name patterns. When provided, a test");
-            WriteLine("        will run only if it matches at least one of the patterns.");
-            WriteLine();
-            WriteLine("    --report path");
-            WriteLine("        Write test results to the specified path, using the xUnit XML format.");
-            WriteLine();
-            WriteLine("    --team-city <on|off>");
-            WriteLine("        When this option is omitted, the runner detects the need for");
-            WriteLine("        TeamCity-formatted console output. Use this option to force");
-            WriteLine("        TeamCity output on or off.");
-            WriteLine();
-            WriteLine("    convention arguments");
-            WriteLine("        Arbitrary arguments made available to conventions at runtime.");
-            WriteLine();
         }
     }
 }

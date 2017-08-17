b08b023f23f0eac1841177597e39b85dadbb0466
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-16 21:30:15 -0500

    Apply msbuild /nologo option on applicable `dotnet` commands to avoid clutter in the build output.

--- build.ps1
+++ build.ps1
@@ -35,7 +35,7 @@ function Assembly-Properties {
 }
 
 function Clean {
-    exec { dotnet clean src -c $configuration }
+    exec { dotnet clean src -c $configuration /nologo }
 }
 
 function Restore {
@@ -43,7 +43,7 @@ function Restore {
 }
 
 function Build {
-    exec { dotnet build src -c $configuration }
+    exec { dotnet build src -c $configuration /nologo }
 }
 
 function Test-Console-x64 {

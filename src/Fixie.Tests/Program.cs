a2f66a08d7f59db5218756909afc0f31938d183e
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-18 08:11:08 -0500

    Fixie.Tests and Fixie.Samples are console applications with a trivial Main method invoking AssemblyRunner.

---
+++ src/Fixie.Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Fixie.Tests
+{
+    using System;
+    using Fixie.Execution;
+
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] arguments)
+        {
+            return AssemblyRunner.Main(arguments);
+        }
+    }
+}

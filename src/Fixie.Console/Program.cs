64478098d212138e8849ecf3f76c7aeb21681290
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-18 07:37:06 -0500

    Extract the body of the console runner Main method as AssemblyRunner.Main(...), as a step towards allowing test assemblies to be executables themselves.

--- src/Fixie.Console/Program.cs
+++ src/Fixie.Console/Program.cs
@@ -1,32 +1,14 @@
 ﻿namespace Fixie.Console
 {
     using System;
-    using Cli;
     using Execution;
-    using static System.Console;
 
     class Program
     {
-        const int FatalError = -1;
-
         [STAThread]
         static int Main(string[] arguments)
         {
-            try
-            {
-                var options = CommandLine.Parse<Options>(arguments);
-                options.Validate();
-
-                using (var environment = new ExecutionEnvironment(options.AssemblyPath))
-                    return environment.RunAssembly(arguments);
-            }
-            catch (Exception exception)
-            {
-                using (Foreground.Red)
-                    WriteLine($"Fatal Error: {exception}");
-
-                return FatalError;
-            }
+            return AssemblyRunner.Main(arguments);
         }
     }
 }
---
+++ src/Fixie/Execution/AssemblyRunner.cs
@@ -0,0 +1,29 @@
+﻿namespace Fixie.Execution
+{
+    using System;
+    using Cli;
+
+    public class AssemblyRunner
+    {
+        const int FatalError = -1;
+
+        public static int Main(string[] arguments)
+        {
+            try
+            {
+                var options = CommandLine.Parse<Options>(arguments);
+                options.Validate();
+
+                using (var environment = new ExecutionEnvironment(options.AssemblyPath))
+                    return environment.RunAssembly(arguments);
+            }
+            catch (Exception exception)
+            {
+                using (Foreground.Red)
+                    Console.WriteLine($"Fatal Error: {exception}");
+
+                return FatalError;
+            }
+        }
+    }
+}

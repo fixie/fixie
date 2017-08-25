19d4fd6724bdc56579953fa72199b75c7d1c79f8
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-24 21:17:23 -0500

    ConsoleExecutionEnvironment is no longer responsibile for changing the current directory, as the proper working directory is already set by `dotnet fixie`.

--- src/Fixie/Execution/ConsoleExecutionEnvironment.cs
+++ src/Fixie/Execution/ConsoleExecutionEnvironment.cs
@@ -6,27 +6,21 @@
     public class ConsoleExecutionEnvironment : IDisposable
     {
         readonly string assemblyFullPath;
-        readonly string previousWorkingDirectory;
         readonly ExecutionProxy executionProxy;
 
         public ConsoleExecutionEnvironment(string assemblyPath)
         {
             assemblyFullPath = Path.GetFullPath(assemblyPath);
 
-            previousWorkingDirectory = Directory.GetCurrentDirectory();
             var assemblyDirectory = Path.GetDirectoryName(assemblyFullPath);
-            Directory.SetCurrentDirectory(assemblyDirectory);
 
-            executionProxy = new ExecutionProxy(previousWorkingDirectory);
+            executionProxy = new ExecutionProxy(assemblyDirectory);
         }
 
         public int RunAssembly(string[] arguments)
             => executionProxy.RunAssembly(assemblyFullPath, arguments);
 
         public void Dispose()
-        {
-            executionProxy.Dispose();
-            Directory.SetCurrentDirectory(previousWorkingDirectory);
-        }
+            => executionProxy.Dispose();
     }
 }

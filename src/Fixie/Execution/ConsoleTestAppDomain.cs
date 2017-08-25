33e5f1cccbc4df2b5c4d9a6bc06002a3817f11e2
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-24 21:17:23 -0500

    Now that the net452 and netstandard implementations of ConsoleTestAppDomain are identical, define it a single time.

--- src/Fixie/Execution/ConsoleTestAppDomain.cs
+++ src/Fixie/Execution/ConsoleTestAppDomain.cs
@@ -1,6 +1,5 @@
 ﻿namespace Fixie.Execution
 {
-#if NET452
     using System;
 
     class ConsoleTestAppDomain : IDisposable
@@ -15,20 +14,4 @@
 
         public void Dispose() { }
     }
-#else
-    using System;
-
-    class ConsoleTestAppDomain : IDisposable
-    {
-        public ConsoleTestAppDomain(string assemblyFullPath) { }
-
-        public T CreateFrom<T>() where T : LongLivedMarshalByRefObject, new()
-            => new T();
-
-        public T Create<T>(params object[] arguments) where T : LongLivedMarshalByRefObject
-            => (T)Activator.CreateInstance(typeof(T), arguments);
-
-        public void Dispose() { }
-    }
-#endif
 }

d324fe0f202790030a4deb99080e0c427cee52e4
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-24 21:17:23 -0500

    Remove irrelevant AppDomain usages from ConsoleTestAppDomain's net452 implementation. These usages are irrelevant now that the test assembly *is* the console application, so all assembly loading and config file concerns are already correct in the primary/default AppDomain already in effect.

--- src/Fixie/Execution/ConsoleTestAppDomain.cs
+++ src/Fixie/Execution/ConsoleTestAppDomain.cs
@@ -2,47 +2,18 @@
 {
 #if NET452
     using System;
-    using System.IO;
-    using System.Security;
-    using System.Security.Permissions;
 
     class ConsoleTestAppDomain : IDisposable
     {
-        readonly AppDomain appDomain;
-
-        public ConsoleTestAppDomain(string assemblyFullPath)
-        {
-            var setup = new AppDomainSetup
-            {
-                ApplicationBase = Path.GetDirectoryName(assemblyFullPath),
-                ApplicationName = Guid.NewGuid().ToString(),
-                ConfigurationFile = GetOptionalConfigFullPath(assemblyFullPath)
-            };
-
-            appDomain = AppDomain.CreateDomain(setup.ApplicationName, null, setup, new PermissionSet(PermissionState.Unrestricted));
-        }
+        public ConsoleTestAppDomain(string assemblyFullPath) { }
 
         public T CreateFrom<T>() where T : LongLivedMarshalByRefObject, new()
-        {
-            return (T)appDomain.CreateInstanceFromAndUnwrap(typeof(T).Assembly.Location, typeof(T).FullName, false, 0, null, null, null, null);
-        }
+            => new T();
 
         public T Create<T>(params object[] arguments) where T : LongLivedMarshalByRefObject
-        {
-            return (T)appDomain.CreateInstanceAndUnwrap(typeof(T).Assembly.FullName, typeof(T).FullName, false, 0, null, arguments, null, null);
-        }
+            => (T)Activator.CreateInstance(typeof(T), arguments);
 
-        static string GetOptionalConfigFullPath(string assemblyFullPath)
-        {
-            var configFullPath = assemblyFullPath + ".config";
-
-            return File.Exists(configFullPath) ? configFullPath : null;
-        }
-
-        public void Dispose()
-        {
-            AppDomain.Unload(appDomain);
-        }
+        public void Dispose() { }
     }
 #else
     using System;

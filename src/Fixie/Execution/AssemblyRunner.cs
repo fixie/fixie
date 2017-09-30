764520381830cca66338dd5da2d3e32e8031d999
Patrick Lioi <patrick.lioi@gmail.com>
2017-09-30 14:08:39 -0500

    AssemblyRunner reads instructions for what execution path to use when FIXIE_NAMED_PIPE is defined.

--- src/Fixie/Execution/AssemblyRunner.cs
+++ src/Fixie/Execution/AssemblyRunner.cs
@@ -2,11 +2,14 @@
 {
     using System;
     using System.IO;
+    using System.IO.Pipes;
     using System.Reflection;
     using Cli;
+    using Listeners;
 
     public class AssemblyRunner
     {
+        const int Success = 0;
         const int FatalError = -1;
 
         public static int Main(string[] arguments)
@@ -21,7 +24,35 @@
                 var assemblyDirectory = Path.GetDirectoryName(assemblyFullPath);
 
                 using (var executionProxy = new ExecutionProxy(assemblyDirectory))
-                    return executionProxy.RunAssembly(assemblyFullPath, arguments);
+                {
+                    var pipeName = Environment.GetEnvironmentVariable("FIXIE_NAMED_PIPE");
+
+                    if (pipeName == null)
+                        return executionProxy.RunAssembly(assemblyFullPath, arguments);
+
+                    using (var clientPipe = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut))
+                    {
+                        clientPipe.ReadMode = PipeTransmissionMode.Message;
+                        clientPipe.Connect();
+
+                        var command = clientPipe.ReceiveMessage();
+
+                        if (command == "DiscoverMethods")
+                        {
+                            executionProxy.DiscoverMethods(assemblyFullPath, arguments);
+                            return Success;
+                        }
+
+                        if (command == "RunMethods")
+                        {
+                            var runMethods = clientPipe.Receive<RunMethods>();
+
+                            return executionProxy.RunMethods(assemblyFullPath, arguments, runMethods.Methods);
+                        }
+
+                        return executionProxy.RunAssembly(assemblyFullPath, arguments);
+                    }
+                }
             }
             catch (Exception exception)
             {
@@ -31,5 +62,10 @@
                 return FatalError;
             }
         }
+    }
+
+    public class RunMethods
+    {
+        public string[] Methods { get; set; }
     }
 }

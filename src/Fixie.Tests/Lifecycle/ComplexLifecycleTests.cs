303c66aebbc330c8f456eae76f829f7eb8631cdc
Patrick Lioi <patrick.lioi@gmail.com>
2018-01-15 21:46:23 -0600

    Remove tests in ComplexLifecycleTests which are better represented already in LifecycleTests.

--- src/Fixie.Tests/Lifecycle/ComplexLifecycleTests.cs
+++ src/Fixie.Tests/Lifecycle/ComplexLifecycleTests.cs
@@ -1,86 +1,9 @@
 namespace Fixie.Tests.Lifecycle
 {
-    using System;
     using static System.Environment;
 
     public class ComplexLifecycleTests : BaseLifecycleTests
     {
-        public void ShouldPerformCompleteLifecyclePerCaseWhenConstructingPerCase()
-        {
-            Convention.ClassExecution
-                      .CreateInstancePerCase()
-                      .Wrap<ClassSetUpTearDown>();
-
-            Convention.FixtureExecution
-                      .Wrap<FixtureSetUpTearDown>();
-
-            Convention.CaseExecution
-                      .Wrap<CaseSetUpTearDown>();
-
-            var output = Run();
-
-            output.ShouldHaveResults(
-                "SampleTestClass.Pass passed",
-                "SampleTestClass.Fail failed: 'Fail' failed!");
-
-            output.ShouldHaveLifecycle(
-                "ClassSetUp",
-
-                ".ctor",
-                "FixtureSetUp",
-                "CaseSetUp",
-                "Pass",
-                "CaseTearDown",
-                "FixtureTearDown",
-                "Dispose",
-
-                ".ctor",
-                "FixtureSetUp",
-                "CaseSetUp",
-                "Fail",
-                "CaseTearDown",
-                "FixtureTearDown",
-                "Dispose",
-
-                "ClassTearDown");
-        }
-
-        public void ShouldPerformCompleteLifecyclePerClassWhenConstructingPerClass()
-        {
-            Convention.ClassExecution
-                      .CreateInstancePerClass()
-                      .Wrap<ClassSetUpTearDown>();
-
-            Convention.FixtureExecution
-                      .Wrap<FixtureSetUpTearDown>();
-
-            Convention.CaseExecution
-                      .Wrap<CaseSetUpTearDown>();
-
-            var output = Run();
-
-            output.ShouldHaveResults(
-                "SampleTestClass.Pass passed",
-                "SampleTestClass.Fail failed: 'Fail' failed!");
-
-            output.ShouldHaveLifecycle(
-                "ClassSetUp",
-                ".ctor",
-
-                "FixtureSetUp",
-                "CaseSetUp",
-                "Pass",
-                "CaseTearDown",
-
-                "CaseSetUp",
-                "Fail",
-                "CaseTearDown",
-                "FixtureTearDown",
-
-                "Dispose",
-                "ClassTearDown");
-        }
-
         public void ShouldIncludeAllTearDownAndDisposalExceptionsInResultWhenConstructingPerCase()
         {
             FailDuring("ClassTearDown", "FixtureTearDown", "CaseTearDown", "Dispose");

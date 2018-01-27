6aad267bee18716ebf4b34f24c39ab1f080cf6ac
Patrick Lioi <patrick.lioi@gmail.com>
2018-01-27 09:25:12 -0600

    Inline ExecuteCases to the one place it is used.

--- src/Fixie/Execution/LifecycleRunner.cs
+++ src/Fixie/Execution/LifecycleRunner.cs
@@ -2,7 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
-    using Behaviors;
+    using System.Diagnostics;
 
     class LifecycleRunner
     {
@@ -17,13 +17,43 @@
             {
                 lifecycle.Execute(testClass, caseLifecycle =>
                 {
-                    ExecuteCases.Execute(cases, caseLifecycle);
+                    ExecuteCases(cases, caseLifecycle);
                 });
             }
             catch (Exception exception)
             {
                 foreach (var @case in cases)
                     @case.Fail(exception);
+            }
+        }
+
+        static void ExecuteCases(IReadOnlyList<Case> cases, CaseAction caseLifecycle)
+        {
+            foreach (var @case in cases)
+            {
+                string consoleOutput;
+                using (var console = new RedirectedConsole())
+                {
+                    var stopwatch = Stopwatch.StartNew();
+
+                    try
+                    {
+                        caseLifecycle(@case);
+                    }
+                    catch (Exception exception)
+                    {
+                        @case.Fail(exception);
+                    }
+
+                    stopwatch.Stop();
+
+                    @case.Duration += stopwatch.Elapsed;
+
+                    consoleOutput = console.Output;
+                    @case.Output += consoleOutput;
+                }
+
+                Console.Write(consoleOutput);
             }
         }
     }

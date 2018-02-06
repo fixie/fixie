b2d6605832a0fa230eccedf6bbe49de43a43375e
Patrick Lioi <patrick.lioi@gmail.com>
2018-02-05 20:51:27 -0600

    Rephrase early return from ClassRunner.Run(...) so that it is written in terms of the MethodInfo array being processed, instead of the corresponding Case array. This is functionally equivalent because the two collections are either both empty or both nonempty by this point. The benefit is that this breaks the dependency between the early return and the up-front Case building logic, freeing that logic to move in subsequent commits.

--- src/Fixie/Execution/ClassRunner.cs
+++ src/Fixie/Execution/ClassRunner.cs
@@ -89,7 +89,7 @@
 
             var summary = new ExecutionSummary();
 
-            if (!cases.Any())
+            if (!orderedMethods.Any())
                 return summary;
 
             Start(testClass);

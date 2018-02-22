1cf07a7b1c9d834bfce2f41333f4184a9869623e
Patrick Lioi <patrick.lioi@gmail.com>
2018-02-21 19:39:37 -0600

    Extract MethodInfo-specific parts of the Case.Execute(...) extension method, as as step toward exposing the MethodInfo execution publicly.

--- src/Fixie/CaseExtensions.cs
+++ src/Fixie/CaseExtensions.cs
@@ -11,61 +11,65 @@
         {
             try
             {
-                var method = @case.Method;
-
-                bool isDeclaredAsync = method.IsAsync();
-
-                if (isDeclaredAsync && method.IsVoid())
-                    throw new NotSupportedException(
-                        "Async void methods are not supported. Declare async methods with a " +
-                        "return type of Task to ensure the task actually runs to completion.");
-
-                if (method.ContainsGenericParameters)
-                    throw new Exception("Could not resolve type parameters for generic method.");
-
-                object returnValue;
-
-                try
-                {
-                    returnValue = method.Invoke(instance, @case.Parameters);
-                }
-                catch (TargetInvocationException exception)
-                {
-                    throw new PreservedException(exception.InnerException);
-                }
-
-                if (isDeclaredAsync)
-                {
-                    var task = (Task)returnValue;
-                    try
-                    {
-                        task.Wait();
-                    }
-                    catch (AggregateException exception)
-                    {
-                        throw new PreservedException(exception.InnerExceptions.First());
-                    }
-
-                    if (method.ReturnType.IsGenericType)
-                    {
-                        var property = task.GetType().GetProperty("Result", BindingFlags.Instance | BindingFlags.Public);
-
-                        returnValue = property.GetValue(task, null);
-                    }
-                    else
-                    {
-                        returnValue = null;
-                    }
-                }
-
+                var result = @case.Method.Execute(instance, @case.Parameters);
                 @case.Pass();
-                return returnValue;
+                return result;
             }
             catch (Exception exception)
             {
                 @case.Fail(exception);
                 return null;
             }
+        }
+
+        static object Execute(this MethodInfo method, object instance, object[] parameters)
+        {
+            bool isDeclaredAsync = method.IsAsync();
+
+            if (isDeclaredAsync && method.IsVoid())
+                throw new NotSupportedException(
+                    "Async void methods are not supported. Declare async methods with a " +
+                    "return type of Task to ensure the task actually runs to completion.");
+
+            if (method.ContainsGenericParameters)
+                throw new Exception("Could not resolve type parameters for generic method.");
+
+            object returnValue;
+
+            try
+            {
+                returnValue = method.Invoke(instance, parameters);
+            }
+            catch (TargetInvocationException exception)
+            {
+                throw new PreservedException(exception.InnerException);
+            }
+
+            if (isDeclaredAsync)
+            {
+                var task = (Task) returnValue;
+                try
+                {
+                    task.Wait();
+                }
+                catch (AggregateException exception)
+                {
+                    throw new PreservedException(exception.InnerExceptions.First());
+                }
+
+                if (method.ReturnType.IsGenericType)
+                {
+                    var property = task.GetType().GetProperty("Result", BindingFlags.Instance | BindingFlags.Public);
+
+                    returnValue = property.GetValue(task, null);
+                }
+                else
+                {
+                    returnValue = null;
+                }
+            }
+
+            return returnValue;
         }
     }
 }

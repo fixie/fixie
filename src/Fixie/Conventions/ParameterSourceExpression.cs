f476fe12b0e9ca77e49cb1936b0af97bbdf818ab
Patrick Lioi <patrick.lioi@gmail.com>
2018-02-26 21:30:12 -0600

    Remove the delegate-accepting overload of ParamterSourceExpression.Add(...), because it does not aid API discoverability, cannot support efficient "yield" parameter generation, and has no compelling use case.

--- src/Fixie/Conventions/ParameterSourceExpression.cs
+++ src/Fixie/Conventions/ParameterSourceExpression.cs
@@ -1,9 +1,5 @@
 ﻿namespace Fixie.Conventions
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Reflection;
-
     public class ParameterSourceExpression
     {
         readonly Configuration config;
@@ -31,38 +27,6 @@
         {
             config.AddParameterSource(parameterSource);
             return this;
-        }
-
-        /// <summary>
-        /// Includes the given delegate as an generator of test method parameters.
-        /// All such registered parameter sources will be asked to contribute parameters to test methods.
-        /// 
-        /// <para>
-        /// Given a test method, yields zero or more sets
-        /// of method parameters to be passed into the test method.
-        /// Each object array returned represents a distinct
-        /// invocation of the test method.
-        /// </para>
-        /// </summary>
-        public ParameterSourceExpression Add(ParameterSourceFunc getParameters)
-        {
-            config.AddParameterSource(new LambdaParameterSource(getParameters));
-            return this;
-        }
-
-        class LambdaParameterSource : ParameterSource
-        {
-            readonly ParameterSourceFunc getParameters;
-
-            public LambdaParameterSource(ParameterSourceFunc getParameters)
-            {
-                this.getParameters = getParameters;
-            }
-
-            public IEnumerable<object[]> GetParameters(MethodInfo method)
-            {
-                return getParameters(method);
-            }
         }
     }
 }

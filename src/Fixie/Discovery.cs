0defdd10d1cda7ccf02dd1f2176f0a2e1334d4f5
Patrick Lioi <patrick.lioi@gmail.com>
2018-05-06 11:45:55 -0500

    Extract base class Discovery from Convention, separate from the execution concerns of the Lifecycle interface.

---
+++ src/Fixie/Discovery.cs
@@ -0,0 +1,41 @@
+﻿namespace Fixie
+{
+    using Conventions;
+
+    /// <summary>
+    /// Subclass Discovery to customize test discovery and execution.
+    /// </summary>
+    public abstract class Discovery
+    {
+        protected Discovery()
+        {
+            Config = new Configuration();
+
+            Classes = new ClassExpression(Config);
+            Methods = new MethodExpression(Config);
+            Parameters = new ParameterSourceExpression(Config);
+        }
+
+        /// <summary>
+        /// The current state describing the convention. This state can be manipulated through
+        /// the other properties on Convention.
+        /// </summary>
+        internal Configuration Config { get; }
+
+        /// <summary>
+        /// Defines the set of conditions that describe which classes are test classes.
+        /// </summary>
+        public ClassExpression Classes { get; }
+
+        /// <summary>
+        /// Defines the set of conditions that describe which test class methods are test methods,
+        /// and what order to run them in.
+        /// </summary>
+        public MethodExpression Methods { get; }
+
+        /// <summary>
+        /// Defines the set of parameter sources, which provide inputs to parameterized test methods.
+        /// </summary>
+        public ParameterSourceExpression Parameters { get; }
+    }
+}

d4118fdcfd16e6a162253284aa193fa2455d1e23
Patrick Lioi <patrick.lioi@gmail.com>
2018-05-08 21:16:00 -0500

    Inline method which was only used once.

--- src/Fixie/Internal/Discoverer.cs
+++ src/Fixie/Internal/Discoverer.cs
@@ -20,11 +20,6 @@
         {
             var discovery = new BehaviorDiscoverer(assembly, customArguments).GetDiscovery();
 
-            DiscoverMethods(assembly, discovery);
-        }
-
-        public void DiscoverMethods(Assembly assembly, Discovery discovery)
-        {
             var classDiscoverer = new ClassDiscoverer(discovery);
             var candidateTypes = assembly.GetTypes();
             var testClasses = classDiscoverer.TestClasses(candidateTypes);

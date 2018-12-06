def0521b988269d9c807683a03f26d2e8a75bcac
Patrick Lioi <patrick.lioi@gmail.com>
2018-12-05 18:17:38 -0600

    Literate stack traces omit the trailing section which describes Fixie's internal use of ExceptionDispatchInfo.Throw(), as this is needlessly verbose when users need to diagnose a test failure.

--- src/Fixie/Internal/Listeners/ExceptionExtensions.cs
+++ src/Fixie/Internal/Listeners/ExceptionExtensions.cs
@@ -1,17 +1,24 @@
 ﻿namespace Fixie.Internal.Listeners
 {
     using System;
+    using System.Diagnostics;
     using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.ExceptionServices;
 
     static class ExceptionExtensions
     {
+        static readonly MethodInfo CaseExecuteMethod = typeof(Case).GetMethod("Execute");
+        static readonly MethodInfo ExceptionRethrowMethod = typeof(ExceptionDispatchInfo).GetMethod("Throw", new Type[] { });
+
         public static string LiterateStackTrace(this Exception exception)
         {
             using (var console = new StringWriter())
             {
                 var ex = exception;
 
-                console.Write(ex.StackTrace);
+                console.Write(StackTraceOmittingInternalRethrow(ex));
 
                 var walk = ex;
                 while (walk.InnerException != null)
@@ -26,6 +33,100 @@
 
                 return console.ToString();
             }
+        }
+
+        public static string StackTraceOmittingInternalRethrow(Exception exception)
+        {
+            // If the stack trace ends with a well-known internal segment caused by
+            // ExceptionDispatchInfo.Throw(), attempt to omit that section.
+            //
+            // Example: Synchronous test failures end with the lines to omit:
+            //
+            //  --- End of stack trace from previous location where exception was thrown ---
+            //    at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+            //    at Fixie.MethodInfoExtensions.Execute(MethodInfo method, Object instance, Object[] parameters)
+            //    at Fixie.Case.Execute(Object instance)
+            //
+            // Example: Asynchronous test failures end with the following lines to omit:
+            //
+            // --- End of stack trace from previous location where exception was thrown ---
+            //    at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+            //    at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+            //    at System.Runtime.CompilerServices.TaskAwaiter.GetResult()
+            //    at Fixie.MethodInfoExtensions.Execute(MethodInfo method, Object instance, Object[] parameters)
+            //    at Fixie.Case.Execute(Object instance)
+            //
+            // This assumes that stack trace lines use the "---" formatting of the rethrow hint
+            // across cultures, and that the stack frame lines do *not* contain "---" across cultures.
+            //
+            // When in doubt, return the original stack trace.
+
+            if (exception.StackTrace == null)
+                return null;
+
+            var stackTrace = new StackTrace(exception);
+            var frames = stackTrace.GetFrames();
+
+            if (frames == null || frames.Length == 0)
+                return exception.StackTrace;
+
+            var lastFrame = frames.Last();
+
+            if (lastFrame.GetMethod() != CaseExecuteMethod)
+                return exception.StackTrace;
+
+            if (TryCountFramesToRemove(frames, out var numberOfTrailingStackFramesToRemove))
+            {
+                //Verify that the last numberOfTrailingStackFramesToRemove stack trace lines
+                //do NOT start or end with "---", and that once removed, the last line DOES.
+
+                var lines = exception.StackTrace.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+
+                if (lines.Count >= numberOfTrailingStackFramesToRemove + 1)
+                {
+                    for (int i = 1; i <= numberOfTrailingStackFramesToRemove; i++)
+                    {
+                        var lastLine = lines.Last();
+
+                        if (lastLine.Contains("---"))
+                        {
+                            // Expected an " at ..." line. Fail safe.
+                            return exception.StackTrace;
+                        }
+
+                        lines.RemoveAt(lines.Count - 1);
+                    }
+                }
+
+                var rethrowMarker = lines.Last().Trim();
+
+                if (!rethrowMarker.StartsWith("---"))
+                {
+                    // Expected an "--- ... ---" rethrow marker line. Fail safe.
+                    return exception.StackTrace;
+                }
+
+                lines.RemoveAt(lines.Count - 1);
+
+                return String.Join(Environment.NewLine, lines);
+            }
+
+            return exception.StackTrace;
+        }
+
+        static bool TryCountFramesToRemove(StackFrame[] frames, out int numberOfTrailingStackFramesToRemove)
+        {
+            numberOfTrailingStackFramesToRemove = 0;
+
+            for (int i = frames.Length - 1; i >= 0; i--)
+            {
+                numberOfTrailingStackFramesToRemove++;
+
+                if (frames[i].GetMethod() == ExceptionRethrowMethod)
+                    return true;
+            }
+
+            return false;
         }
     }
 }

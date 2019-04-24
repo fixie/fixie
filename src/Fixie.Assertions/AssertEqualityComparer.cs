c7a43d3ac9ae4278d0b203e11ffe2db76a79998d
Patrick Lioi <patrick.lioi@gmail.com>
2019-04-24 09:24:31 -0500

    Inline EnumerableEqualityComparer into AssertEqualityComparer since it is only used there.

--- src/Fixie.Assertions/AssertEqualityComparer.cs
+++ src/Fixie.Assertions/AssertEqualityComparer.cs
@@ -31,10 +31,60 @@
 
             // Enumerable?
             if (x is IEnumerable enumerableX && y is IEnumerable enumerableY)
-                return new EnumerableEqualityComparer().EnumerableEqual(enumerableX, enumerableY);
+                return EnumerableEqual(enumerableX, enumerableY);
 
             // Last case, rely on object.Equals
             return object.Equals(x, y);
+        }
+
+        static bool EnumerableEqual(IEnumerable x, IEnumerable y)
+        {
+            var enumeratorX = x.GetEnumerator();
+            var enumeratorY = y.GetEnumerator();
+
+            while (true)
+            {
+                bool hasNextX = enumeratorX.MoveNext();
+                bool hasNextY = enumeratorY.MoveNext();
+
+                if (!hasNextX || !hasNextY)
+                    return hasNextX == hasNextY;
+
+                if (enumeratorX.Current != null || enumeratorY.Current != null)
+                {
+                    if (enumeratorX.Current != null && enumeratorY.Current == null)
+                        return false;
+
+                    if (enumeratorX.Current == null)
+                        return false;
+
+                    var xType = enumeratorX.Current.GetType();
+                    var yType = enumeratorY.Current.GetType();
+
+                    if (xType.IsAssignableFrom(yType))
+                    {
+                        if (!ItemsEqual(enumeratorX.Current, enumeratorY.Current, xType))
+                            return false;
+                    }
+                    else if (yType.IsAssignableFrom(xType))
+                    {
+                        if (!ItemsEqual(enumeratorY.Current, enumeratorX.Current, yType))
+                            return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        static bool ItemsEqual(object a, object b, Type baseType)
+        {
+            var assertComparerType = typeof(AssertEqualityComparer);
+            var equal = assertComparerType.GetMethod("Equal");
+            var specificEqual = equal.MakeGenericMethod(baseType);
+            return (bool)specificEqual.Invoke(null, new[] { a, b });
         }
     }
 }

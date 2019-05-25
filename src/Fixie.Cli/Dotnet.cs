1e84cc183a824843ad0efa4cbaba3e117041d08a
Patrick Lioi <patrick.lioi@gmail.com>
2019-05-25 16:41:27 -0500

    Although unused so far by .NET Framework code paths, the Dotnet helper class works for both .NET Core and .NET Framework, by minimizing the code affected by #if conditional compilation.

--- src/Fixie.Cli/Dotnet.cs
+++ src/Fixie.Cli/Dotnet.cs
@@ -1,6 +1,4 @@
-﻿#if !NET452
-
-namespace Fixie.Cli
+﻿namespace Fixie.Cli
 {
     using System;
     using System.Diagnostics;
@@ -13,23 +11,33 @@ namespace Fixie.Cli
 
         static string FindDotnet()
         {
-            var fileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "dotnet.exe" : "dotnet";
+            var fileName = OsPlatformIsWindows() ? "dotnet.exe" : "dotnet";
 
             //If `dotnet` is the currently running process, return the full path to that executable.
 
             using (var currentProcess = Process.GetCurrentProcess())
             {
                 var mainModule = currentProcess.MainModule;
+
                 var currentProcessIsDotNet =
                     !string.IsNullOrEmpty(mainModule?.FileName) &&
                     System.IO.Path.GetFileName(mainModule.FileName)
                         .Equals(fileName, StringComparison.OrdinalIgnoreCase);
+
                 if (currentProcessIsDotNet)
                     return mainModule.FileName;
 
                 return "dotnet";
             }
         }
+
+        static bool OsPlatformIsWindows()
+        {
+            #if NET452
+            return true;
+            #else
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            #endif
+        }
     }
 }
-#endif

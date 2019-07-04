88ea5ba6b72bcceec2d5be5d89af55d0dbe4f95c
Patrick Lioi <patrick.lioi@gmail.com>
2019-07-04 13:47:27 -0500

    Reorder members based on their internal dependencies.

--- src/Fixie/Internal/Runner.cs
+++ src/Fixie/Internal/Runner.cs
@@ -24,6 +24,29 @@
             return Run(assembly, assembly.GetTypes());
         }
 
+        public ExecutionSummary Run(Assembly assembly, Test[] tests)
+        {
+            var request = new Dictionary<string, HashSet<string>>();
+            var types = new List<Type>();
+
+            foreach (var test in tests)
+            {
+                if (!request.ContainsKey(test.Class))
+                {
+                    request.Add(test.Class, new HashSet<string>());
+
+                    var type = assembly.GetType(test.Class);
+
+                    if (type != null)
+                        types.Add(type);
+                }
+
+                request[test.Class].Add(test.Method);
+            }
+
+            return Run(assembly, types.ToArray(), method => request[method.ReflectedType.FullName].Contains(method.Name));
+        }
+
         public ExecutionSummary Run(Assembly assembly, Type[] candidateTypes, Func<MethodInfo, bool> methodCondition = null)
         {
             new BehaviorDiscoverer(assembly, customArguments)
@@ -69,29 +92,6 @@
             bus.Publish(new AssemblyCompleted(assembly, assemblySummary, stopwatch.Elapsed));
 
             return assemblySummary;
-        }
-
-        public ExecutionSummary Run(Assembly assembly, Test[] tests)
-        {
-            var request = new Dictionary<string, HashSet<string>>();
-            var types = new List<Type>();
-
-            foreach (var test in tests)
-            {
-                if (!request.ContainsKey(test.Class))
-                {
-                    request.Add(test.Class, new HashSet<string>());
-
-                    var type = assembly.GetType(test.Class);
-
-                    if (type != null)
-                        types.Add(type);
-                }
-
-                request[test.Class].Add(test.Method);
-            }
-
-            return Run(assembly, types.ToArray(), method => request[method.ReflectedType.FullName].Contains(method.Name));
         }
     }
 }

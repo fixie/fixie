4cda749268df7fab847753430e3d3fc1b81f374e
Patrick Lioi <patrick.lioi@gmail.com>
2020-04-26 21:47:24 -0500

    Remove dead code.

--- src/Fixie/InternalsVisibleTo.cs
+++ src/Fixie/InternalsVisibleTo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("dotnet-fixie")]
 [assembly:InternalsVisibleTo("Fixie.Tests")]
 [assembly:InternalsVisibleTo("Fixie.TestAdapter")]

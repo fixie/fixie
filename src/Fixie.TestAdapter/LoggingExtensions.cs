88aef7d64ec7febf86f3bca6de3ba0ef4f1be192
Patrick Lioi <patrick.lioi@gmail.com>
2020-04-27 21:04:58 -0500

    Remove unused overload.

--- src/Fixie.TestAdapter/LoggingExtensions.cs
+++ src/Fixie.TestAdapter/LoggingExtensions.cs
@@ -1,6 +1,5 @@
 ﻿namespace Fixie.TestAdapter
 {
-    using System;
     using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
 
     public static class LoggingExtensions
@@ -10,9 +9,6 @@
 
         public static void Error(this IMessageLogger logger, string message)
             => logger.SendMessage(TestMessageLevel.Error, message);
-
-        public static void Error(this IMessageLogger logger, Exception exception)
-            => logger.SendMessage(TestMessageLevel.Error, exception.ToString());
 
         public static void Version(this IMessageLogger logger)
             => logger.Info(Framework.Version);

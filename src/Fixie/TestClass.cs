fc6c07b82e5759035261231eceadb3e6f57c36a8
Patrick Lioi <patrick.lioi@gmail.com>
2020-05-09 16:19:19 -0500

    Declare nullability in TestClass.

--- src/Fixie/TestClass.cs
+++ src/Fixie/TestClass.cs
@@ -13,7 +13,7 @@
         readonly bool isStatic;
         internal TestClass(Type type, Action<Action<Case>> runCases) : this(type, runCases, null) { }
 
-        internal TestClass(Type type, Action<Action<Case>> runCases, MethodInfo targetMethod)
+        internal TestClass(Type type, Action<Action<Case>> runCases, MethodInfo? targetMethod)
         {
             this.runCases = runCases;
             Type = type;
@@ -31,7 +31,7 @@
         /// test runner as the sole method to be executed.
         /// Null under normal test execution.
         /// </summary>
-        public MethodInfo TargetMethod { get; }
+        public MethodInfo? TargetMethod { get; }
 
         /// <summary>
         /// Constructs an instance of the test class type, using its default constructor.
@@ -48,7 +48,7 @@
             }
             catch (TargetInvocationException exception)
             {
-                ExceptionDispatchInfo.Capture(exception.InnerException).Throw();
+                ExceptionDispatchInfo.Capture(exception.InnerException!).Throw();
                 throw; //Unreachable.
             }
         }

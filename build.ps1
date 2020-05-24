ffbed38584999f1a53efa1cf23a45660ebedaba7
Patrick Lioi <patrick.lioi@gmail.com>
2020-05-24 15:19:04 -0500

    Protect build script from potentially-ambiguous relative paths in calls to .NET methods, by determining absolute paths based on the scripts' own containing folder.

--- build.ps1
+++ build.ps1
@@ -13,7 +13,7 @@ $versionSuffix = if ($prerelease) { "beta-{0:D4}" -f $buildNumber } else { "" }
 function Build {
     mit-license $copyright
 
-    generate "src\Directory.build.props" @"
+    generate "$PSScriptRoot\src\Directory.build.props" @"
 <Project>
     <PropertyGroup>
         <Product>Fixie</Product>

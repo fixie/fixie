cb5d6cdac530583105bca7caf319545c6d841721
Patrick Lioi <patrick.lioi@gmail.com>
2020-05-26 11:33:53 -0500

    Reorder members by category

--- src/Fixie.Tests/Assertions/AssertionExtensions.cs
+++ src/Fixie.Tests/Assertions/AssertionExtensions.cs
@@ -13,51 +13,12 @@ namespace Fixie.Tests.Assertions
             WriteIndented = true
         };
 
-        public static void ShouldBeGreaterThan<T>(this T actual, T minimum) where T: IComparable<T>
+        public static void ShouldBe(this string? actual, string? expected)
         {
-            if (actual.CompareTo(minimum) <= 0)
-                throw new AssertException(ComparisonFailure(actual, minimum, ">"));
-        }
-
-        public static void ShouldBeGreaterThanOrEqualTo<T>(this T actual, T minimum) where T: IComparable<T>
-        {
-            if (actual.CompareTo(minimum) < 0)
-                throw new AssertException(ComparisonFailure(actual, minimum, ">="));
-        }
-
-        static string ComparisonFailure(object left, object right, string operation)
-        {
-            return $"Expected: {left} {operation} {right}{NewLine}but it was not";
-        }
-
-        public static T ShouldBe<T>(this object? actual)
-        {
-            if (actual is T typed)
-                return typed;
-
-            throw new AssertException(typeof(T), actual?.GetType());
-        }
-
-        public static void ShouldBe<T>(this IEnumerable<T> actual, params T[] expected)
-        {
-            actual.ToArray().ShouldBe(expected);
-        }
-
-        public static void ShouldBe<T>(this T[] actual, T[] expected)
-        {
-            if (actual.Length != expected.Length)
+            if (actual != expected)
                 throw new AssertException(expected, actual);
-
-            for (var i = 0; i < actual.Length; i++)
-                if (!Equals(actual[i], expected[i]))
-                    throw new AssertException(expected, actual);
         }
 
-        public static void ShouldBe(this IEnumerable<string> actual, params string[] expected)
-        {
-            actual.ToArray().ShouldBe(expected);
-        }
-        
         public static void ShouldBe(this string[] actual, string[] expected)
         {
             if (actual.Length != expected.Length)
@@ -68,20 +29,9 @@ namespace Fixie.Tests.Assertions
                     throw new AssertException(Json(expected), Json(actual));
         }
 
-        public static void ShouldMatch<T>(this T actual, T expected)
+        public static void ShouldBe(this IEnumerable<string> actual, params string[] expected)
         {
-            Json(actual).ShouldBe(Json(expected));
-        }
-
-        public static void ShouldMatch<T>(this IEnumerable<T> actual, params T[] expected)
-        {
-            actual.ToArray().ShouldMatch(expected);
-        }
-
-        public static void ShouldBe(this string? actual, string? expected)
-        {
-            if (actual != expected)
-                throw new AssertException(expected, actual);
+            actual.ToArray().ShouldBe(expected);
         }
 
         public static void ShouldBe<T>(this IEquatable<T> actual, IEquatable<T> expected)
@@ -94,6 +44,29 @@ namespace Fixie.Tests.Assertions
         {
             if (!Equals(actual, expected))
                 throw new AssertException(expected, actual);
+        }
+
+        public static void ShouldBe<T>(this T[] actual, T[] expected)
+        {
+            if (actual.Length != expected.Length)
+                throw new AssertException(expected, actual);
+
+            for (var i = 0; i < actual.Length; i++)
+                if (!Equals(actual[i], expected[i]))
+                    throw new AssertException(expected, actual);
+        }
+
+        public static void ShouldBe<T>(this IEnumerable<T> actual, params T[] expected)
+        {
+            actual.ToArray().ShouldBe(expected);
+        }
+
+        public static T ShouldBe<T>(this object? actual)
+        {
+            if (actual is T typed)
+                return typed;
+
+            throw new AssertException(typeof(T), actual?.GetType());
         }
 
         public static void ShouldBeEmpty<T>(this IEnumerable<T> collection)
@@ -117,6 +90,33 @@ namespace Fixie.Tests.Assertions
             }
 
             throw new AssertException("Expected an exception to be thrown.");
+        }
+
+        public static void ShouldBeGreaterThan<T>(this T actual, T minimum) where T: IComparable<T>
+        {
+            if (actual.CompareTo(minimum) <= 0)
+                throw new AssertException(ComparisonFailure(actual, minimum, ">"));
+        }
+
+        public static void ShouldBeGreaterThanOrEqualTo<T>(this T actual, T minimum) where T: IComparable<T>
+        {
+            if (actual.CompareTo(minimum) < 0)
+                throw new AssertException(ComparisonFailure(actual, minimum, ">="));
+        }
+
+        static string ComparisonFailure(object left, object right, string operation)
+        {
+            return $"Expected: {left} {operation} {right}{NewLine}but it was not";
+        }
+
+        public static void ShouldMatch<T>(this T actual, T expected)
+        {
+            Json(actual).ShouldBe(Json(expected));
+        }
+
+        public static void ShouldMatch<T>(this IEnumerable<T> actual, params T[] expected)
+        {
+            actual.ToArray().ShouldMatch(expected);
         }
 
         static string Json<T>(T actual)

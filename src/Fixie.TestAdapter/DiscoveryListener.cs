2bc6af33da7bd4bb0eb6cacce483a4eef61d3ecc
Patrick Lioi <patrick.lioi@gmail.com>
2020-06-06 12:05:30 -0500

    Add DiscoveryListener as the intended replacement for the deprecated DiscoveryRecorder, performing equivalent work but as a Listener type.

---
+++ src/Fixie.TestAdapter/DiscoveryListener.cs
@@ -0,0 +1,54 @@
+﻿namespace Fixie.TestAdapter
+{
+    using System;
+    using Internal;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+    public class DiscoveryListener : Handler<MethodDiscovered>
+    {
+        readonly IMessageLogger log;
+        readonly ITestCaseDiscoverySink discoverySink;
+        readonly string assemblyPath;
+        readonly SourceLocationProvider sourceLocationProvider;
+
+        public DiscoveryListener(IMessageLogger log, ITestCaseDiscoverySink discoverySink, string assemblyPath)
+        {
+            this.log = log;
+            this.discoverySink = discoverySink;
+            this.assemblyPath = assemblyPath;
+
+            sourceLocationProvider = new SourceLocationProvider(assemblyPath);
+        }
+
+        public void Handle(MethodDiscovered message)
+        {
+            var test = new Test(message.Method);
+
+            SourceLocation? sourceLocation = null;
+
+            try
+            {
+                sourceLocationProvider.TryGetSourceLocation(test.Class, test.Method, out sourceLocation);
+            }
+            catch (Exception exception)
+            {
+                log.Error(exception.ToString());
+            }
+
+            var discoveredTest = new TestCase(test.Name, VsTestExecutor.Uri, assemblyPath)
+            {
+                DisplayName = test.Name
+            };
+
+            if (sourceLocation != null)
+            {
+                discoveredTest.CodeFilePath = sourceLocation.CodeFilePath;
+                discoveredTest.LineNumber = sourceLocation.LineNumber;
+            }
+
+            discoverySink.SendTestCase(discoveredTest);
+        }
+    }
+}

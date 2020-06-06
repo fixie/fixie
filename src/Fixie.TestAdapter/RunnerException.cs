6e57e8ab749bd9cdea4b9b0bf9eb41f1fe2c8263
Patrick Lioi <patrick.lioi@gmail.com>
2020-06-06 15:53:04 -0500

    Drop unused PipeMessage contracts.

--- src/Fixie.TestAdapter/RunnerException.cs
+++ src/Fixie.TestAdapter/RunnerException.cs
@@ -2,22 +2,9 @@
 {
     using System;
     using System.Text;
-    using Internal.Listeners;
 
     public class RunnerException : Exception
     {
-        public RunnerException(PipeMessage.ExceptionSummary exception)
-            : base(new StringBuilder()
-                .AppendLine()
-                .AppendLine()
-                .AppendLine(exception.Message)
-                .AppendLine()
-                .AppendLine(exception.Type)
-                .AppendLine(exception.StackTrace)
-                .ToString())
-        {
-        }
-
         public RunnerException(Exception exception)
             : base(new StringBuilder()
                 .AppendLine()

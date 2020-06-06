2f2d2878fb5cd869e3748812331d9a811d7ddb16
Patrick Lioi <patrick.lioi@gmail.com>
2020-06-06 12:07:35 -0500

    The TestAssemblyLoadContext is a baseline implementation of AssemblyLoadContext, capable of loading the indicated test assembly and that test assembly's own dependencies present in the same folder and in locations defined by the test assembly's own "deps" metadata.

---
+++ src/Fixie.TestAdapter/TestAssemblyLoadContext.cs
@@ -0,0 +1,36 @@
+﻿namespace Fixie.TestAdapter
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.Loader;
+
+    class TestAssemblyLoadContext : AssemblyLoadContext
+    {
+        readonly AssemblyDependencyResolver resolver;
+
+        public TestAssemblyLoadContext(string testAssemblyPath)
+        {
+            resolver = new AssemblyDependencyResolver(testAssemblyPath);
+        }
+
+        protected override Assembly? Load(AssemblyName assemblyName)
+        {
+            var assemblyPath = resolver.ResolveAssemblyToPath(assemblyName);
+
+            if (assemblyPath != null)
+                return LoadFromAssemblyPath(assemblyPath);
+
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            var libraryPath = resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+
+            if (libraryPath != null)
+                return LoadUnmanagedDllFromPath(libraryPath);
+
+            return IntPtr.Zero;
+        }
+    }
+}

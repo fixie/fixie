b018e4623c145c693367f9cd63936d37ad5666f7
Patrick Lioi <patrick.lioi@gmail.com>
2020-06-06 12:05:48 -0500

    Add ExecutionListener as the intended replacement for the deprecated ExecutionRecorder, performing equivalent work but as a Listener type.

---
+++ src/Fixie.TestAdapter/ExecutionListener.cs
@@ -0,0 +1,89 @@
+﻿namespace Fixie.TestAdapter
+{
+    using System;
+    using Internal;
+    using Internal.Listeners;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+
+    class ExecutionListener :
+        Handler<CaseStarted>,
+        Handler<CaseSkipped>,
+        Handler<CasePassed>,
+        Handler<CaseFailed>
+    {
+        readonly ITestExecutionRecorder log;
+        readonly string assemblyPath;
+
+        public ExecutionListener(ITestExecutionRecorder log, string assemblyPath)
+        {
+            this.log = log;
+            this.assemblyPath = assemblyPath;
+        }
+
+        public void Handle(CaseStarted message)
+        {
+            var testCase = ToVsTestCase(message.Name);
+
+            log.RecordStart(testCase);
+        }
+
+        public void Handle(CaseSkipped message)
+        {
+            Record(message, x =>
+            {
+                x.Outcome = TestOutcome.Skipped;
+                x.ErrorMessage = message.Reason;
+            });
+        }
+
+        public void Handle(CasePassed message)
+        {
+            Record(message, x =>
+            {
+                x.Outcome = TestOutcome.Passed;
+            });
+        }
+
+        public void Handle(CaseFailed message)
+        {
+            Record(message, x =>
+            {
+                x.Outcome = TestOutcome.Failed;
+                x.ErrorMessage = message.Exception.Message;
+                x.ErrorStackTrace = message.Exception.GetType().FullName +
+                                    Environment.NewLine +
+                                    message.Exception.LiterateStackTrace();
+            });
+        }
+
+        void Record(CaseCompleted result, Action<TestResult> customize)
+        {
+            var testCase = ToVsTestCase(result.Name);
+
+            var testResult = new TestResult(testCase)
+            {
+                DisplayName = result.Name,
+                Duration = result.Duration,
+                ComputerName = Environment.MachineName
+            };
+
+            customize(testResult);
+
+            AttachCapturedConsoleOutput(result.Output, testResult);
+
+            log.RecordResult(testResult);
+        }
+
+        TestCase ToVsTestCase(string name)
+        {
+            return new TestCase(name, VsTestExecutor.Uri, assemblyPath);
+        }
+
+        static void AttachCapturedConsoleOutput(string output, TestResult testResult)
+        {
+            if (!string.IsNullOrEmpty(output))
+                testResult.Messages.Add(new TestResultMessage(TestResultMessage.StandardOutCategory, output));
+        }
+    }
+}

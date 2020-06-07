b3dd85c1c5024cdfc76333631941da55f1678c73
Patrick Lioi <patrick.lioi@gmail.com>
2020-06-07 07:51:55 -0500

    Ensure that the Fixie assembly is loaded once, in the default AssemblyLoadContext just like framework assemblies, so that types can be shared between the Test Adapter and the test assembly.

--- src/Fixie.TestAdapter/TestAssemblyLoadContext.cs
+++ src/Fixie.TestAdapter/TestAssemblyLoadContext.cs
@@ -15,6 +15,19 @@
 
         protected override Assembly? Load(AssemblyName assemblyName)
         {
+            if (assemblyName.Name == "Fixie")
+            {
+                // The Fixie assembly itself must be loaded only
+                // into the AssemblyLoadContext.Default context,
+                // so that the Test Adapter can share types with
+                // those loaded by the test assembly such as Listener,
+                // Handler<T>, Execution, and Discovery. Otherwise,
+                // both contexts would have distinct occurrences of
+                // those types, and they would be mistaken as unrelated.
+            
+                return null;
+            }
+
             var assemblyPath = resolver.ResolveAssemblyToPath(assemblyName);
 
             if (assemblyPath != null)

d0db273214ac9602a6a5d04edd6549dcac889263
Patrick Lioi <patrick.lioi@gmail.com>
2020-06-27 08:11:20 -0500

    Rename the `dotnet pack` output folder from "packages" to "artifacts" to make it clear that it contains build outputs rather than packages to be consumed by the solution.

--- build.ps1
+++ build.ps1
@@ -8,11 +8,11 @@ function step($command) {
 
 $fixie = "src/Fixie.Console/bin/Release/netcoreapp3.1/Fixie.Console.dll"
 
-if (test-path packages) { remove-item packages -Recurse }
+if (test-path artifacts) { remove-item artifacts -Recurse }
 
 step { dotnet clean src -c Release --nologo -v minimal }
 step { dotnet build src -c Release --nologo }
 step { dotnet $fixie *.Tests --configuration Release --no-build }
-step { dotnet pack src/Fixie -o packages -c Release --no-build --nologo }
-step { dotnet pack src/Fixie.Console -o packages -c Release --no-build --nologo }
-step { dotnet pack src/Fixie.TestAdapter -o packages -c Release --no-build --nologo }
+step { dotnet pack src/Fixie -o artifacts -c Release --no-build --nologo }
+step { dotnet pack src/Fixie.Console -o artifacts -c Release --no-build --nologo }
+step { dotnet pack src/Fixie.TestAdapter -o artifacts -c Release --no-build --nologo }

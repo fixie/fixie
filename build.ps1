305d4272ae55e614c8d125cfe519de7aeed5e125
Patrick Lioi <patrick.lioi@gmail.com>
2020-06-27 11:19:31 -0500

    Ensure that the MinVer-calculated PackageVersion works for Fixie.TestAdapter, which still uses a NuSpec.

--- build.ps1
+++ build.ps1
@@ -15,4 +15,7 @@ step { dotnet build src -c Release --nologo }
 step { dotnet $fixie *.Tests --configuration Release --no-build }
 step { dotnet pack src/Fixie -o artifacts -c Release --no-build --nologo }
 step { dotnet pack src/Fixie.Console -o artifacts -c Release --no-build --nologo }
-step { dotnet pack src/Fixie.TestAdapter -o artifacts -c Release --no-build --nologo }
+
+# While Fixie.TestAdapter packs with a nuspec file, we cannot include --no-build here.
+# If we did, the MinVer-calculated version would fail to apply to the package.
+step { dotnet pack src/Fixie.TestAdapter -o artifacts -c Release --nologo }

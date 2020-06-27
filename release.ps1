b85b1b7c2606932dd1b3cfbbbd8b59476b576036
Patrick Lioi <patrick.lioi@gmail.com>
2020-06-27 16:39:48 -0500

    Automated releases access the NuGet API key from the corresponding secret.

--- release.ps1
+++ release.ps1
@@ -1,2 +1,2 @@
-﻿dotnet nuget push artifacts/*.nupkg --source https://api.nuget.org/v3/index.json --api-key $env.NUGET_API_KEY
+﻿dotnet nuget push artifacts/*.nupkg --source https://api.nuget.org/v3/index.json --api-key $env:NUGET_API_KEY
 if ($lastexitcode -ne 0) { throw $lastexitcode }

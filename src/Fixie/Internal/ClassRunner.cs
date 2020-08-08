8dce93b7042e19beaadfef2632191be4bd6fcf69
Patrick Lioi <patrick.lioi@gmail.com>
2020-08-08 11:10:42 -0500

    Move ExecutionRecorder to its own file.

--- src/Fixie/Internal/ClassRunner.cs
+++ src/Fixie/Internal/ClassRunner.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Linq;
     using System.Reflection;
 
@@ -126,92 +125,6 @@
                 recorder.Fail(new Case(@case, caseLifecycleFailure));
             else if (!caseHasNormalResult)
                 recorder.Skip(@case, output);
-        }
-    }
-
-    class ExecutionRecorder
-    {
-        static readonly object[] EmptyParameters = { };
-
-        readonly Bus bus;
-        readonly ExecutionSummary assemblySummary;
-        ExecutionSummary classSummary;
-        readonly Stopwatch classStopwatch;
-        readonly Stopwatch caseStopwatch;
-
-        public ExecutionRecorder(Bus bus, ExecutionSummary assemblySummary)
-        {
-            this.bus = bus;
-            this.assemblySummary = assemblySummary;
-
-            classSummary = new ExecutionSummary();
-            classStopwatch = new Stopwatch();
-            caseStopwatch = new Stopwatch();
-        }
-
-        public void Start(Type testClass)
-        {
-            classSummary = new ExecutionSummary();
-            bus.Publish(new ClassStarted(testClass));
-            classStopwatch.Restart();
-            caseStopwatch.Restart();
-        }
-
-        public void Start(MethodInfo testMethod)
-        {
-            var test = new Test(testMethod);
-            bus.Publish(new TestStarted(test));
-        }
-
-        public void Skip(Case @case, string output = "")
-        {
-            var duration = caseStopwatch.Elapsed;
-            caseStopwatch.Restart();
-
-            var message = new CaseSkipped(@case, duration, output);
-            classSummary.Add(message);
-            bus.Publish(message);
-        }
-
-        public void Skip(MethodInfo testMethod)
-        {
-            var @case = new Case(testMethod, EmptyParameters);
-            Skip(@case);
-        }
-
-        public void Pass(Case @case, string output)
-        {
-            var duration = caseStopwatch.Elapsed;
-            caseStopwatch.Restart();
-
-            var message = new CasePassed(@case, duration, output);
-            classSummary.Add(message);
-            bus.Publish(message);
-        }
-
-        public void Fail(Case @case, string output = "")
-        {
-            var duration = caseStopwatch.Elapsed;
-            caseStopwatch.Restart();
-
-            var message = new CaseFailed(@case, duration, output);
-            classSummary.Add(message);
-            bus.Publish(message);
-        }
-
-        public void Fail(MethodInfo testMethod, Exception exception)
-        {
-            var @case = new Case(testMethod, EmptyParameters);
-            @case.Fail(exception);
-            Fail(@case);
-        }
-
-        public void Complete(Type testClass)
-        {
-            var duration = classStopwatch.Elapsed;
-            classStopwatch.Stop();
-            bus.Publish(new ClassCompleted(testClass, classSummary, duration));
-            assemblySummary.Add(classSummary);
         }
     }
 }
---
+++ src/Fixie/Internal/ExecutionRecorder.cs
@@ -0,0 +1,92 @@
+﻿namespace Fixie.Internal
+{
+    using System;
+    using System.Diagnostics;
+    using System.Reflection;
+
+    class ExecutionRecorder
+    {
+        static readonly object[] EmptyParameters = { };
+
+        readonly Bus bus;
+        readonly ExecutionSummary assemblySummary;
+        ExecutionSummary classSummary;
+        readonly Stopwatch classStopwatch;
+        readonly Stopwatch caseStopwatch;
+
+        public ExecutionRecorder(Bus bus, ExecutionSummary assemblySummary)
+        {
+            this.bus = bus;
+            this.assemblySummary = assemblySummary;
+
+            classSummary = new ExecutionSummary();
+            classStopwatch = new Stopwatch();
+            caseStopwatch = new Stopwatch();
+        }
+
+        public void Start(Type testClass)
+        {
+            classSummary = new ExecutionSummary();
+            bus.Publish(new ClassStarted(testClass));
+            classStopwatch.Restart();
+            caseStopwatch.Restart();
+        }
+
+        public void Start(MethodInfo testMethod)
+        {
+            var test = new Test(testMethod);
+            bus.Publish(new TestStarted(test));
+        }
+
+        public void Skip(Case @case, string output = "")
+        {
+            var duration = caseStopwatch.Elapsed;
+            caseStopwatch.Restart();
+
+            var message = new CaseSkipped(@case, duration, output);
+            classSummary.Add(message);
+            bus.Publish(message);
+        }
+
+        public void Skip(MethodInfo testMethod)
+        {
+            var @case = new Case(testMethod, EmptyParameters);
+            Skip(@case);
+        }
+
+        public void Pass(Case @case, string output)
+        {
+            var duration = caseStopwatch.Elapsed;
+            caseStopwatch.Restart();
+
+            var message = new CasePassed(@case, duration, output);
+            classSummary.Add(message);
+            bus.Publish(message);
+        }
+
+        public void Fail(Case @case, string output = "")
+        {
+            var duration = caseStopwatch.Elapsed;
+            caseStopwatch.Restart();
+
+            var message = new CaseFailed(@case, duration, output);
+            classSummary.Add(message);
+            bus.Publish(message);
+        }
+
+        public void Fail(MethodInfo testMethod, Exception exception)
+        {
+            var @case = new Case(testMethod, EmptyParameters);
+            @case.Fail(exception);
+            Fail(@case);
+        }
+
+        public void Complete(Type testClass)
+        {
+            var duration = classStopwatch.Elapsed;
+            classStopwatch.Stop();
+            bus.Publish(new ClassCompleted(testClass, classSummary, duration));
+            assemblySummary.Add(classSummary);
+        }
+    }
+}

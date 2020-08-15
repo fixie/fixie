03bac3b86e4ecfbe8b28b266cb5bede621ae9624
Patrick Lioi <patrick.lioi@gmail.com>
2020-08-15 09:08:06 -0500

    A TestMethod wraps a test's discovered MethodInfo.

---
+++ src/Fixie/TestMethod.cs
@@ -0,0 +1,12 @@
+﻿namespace Fixie
+{
+    using System.Reflection;
+
+    public class TestMethod
+    {
+        public MethodInfo Method { get; }
+
+        internal TestMethod(MethodInfo method)
+            => Method = method;
+    }
+}

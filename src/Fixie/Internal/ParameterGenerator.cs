e29664e00f50240b4f32c2952f6e5fccd77772de
Patrick Lioi <patrick.lioi@gmail.com>
2020-08-23 10:14:33 -0500

    ParameterGenerator implements ParameterSource, because it naturally already satisfies that abstraction by combining other ParameterSources.

--- src/Fixie/Internal/ParameterGenerator.cs
+++ src/Fixie/Internal/ParameterGenerator.cs
@@ -4,7 +4,7 @@
     using System.Linq;
     using System.Reflection;
 
-    class ParameterGenerator
+    class ParameterGenerator : ParameterSource
     {
         readonly IReadOnlyList<ParameterSource> parameterSources;
 

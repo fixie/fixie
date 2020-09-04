0b435baae9bfaacd39b29b18268d02e0697a89a2
Patrick Lioi <patrick.lioi@gmail.com>
2020-09-03 21:53:34 -0500

    Test conventions can opt into test order randomization with the IEnumerable<TestMethod>.Shuffle(...) methods.

---
+++ src/Fixie/ShuffleExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace Fixie
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class ShuffleExtensions
+    {
+        /// <summary>
+        /// Randomizes the order of the given tests.
+        /// </summary>
+        public static IReadOnlyList<TestMethod> Shuffle(this IEnumerable<TestMethod> tests)
+        {
+            return tests.Shuffle(new Random());
+        }
+
+        /// <summary>
+        /// Randomizes the order of the given tests, using the given pseudo-random number generator.
+        /// </summary>
+        public static IReadOnlyList<TestMethod> Shuffle(this IEnumerable<TestMethod> tests, Random random)
+        {
+            var array = tests.ToArray();
+
+            FisherYatesShuffle(array, random);
+
+            return array;
+        }
+
+        static void FisherYatesShuffle<T>(T[] array, Random random)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = random.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
+}

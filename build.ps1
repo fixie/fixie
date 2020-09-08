532c7839046023d65601cefd8cee7caa328b0000
Patrick Lioi <patrick.lioi@gmail.com>
2020-09-08 09:19:56 -0500

    Replace all occurrences of `netcoreapp3.1` with `net5.0`.

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ function step($command) {
     if ($lastexitcode -ne 0) { throw $lastexitcode }
 }
 
-$fixie = "src/Fixie.Console/bin/Release/netcoreapp3.1/Fixie.Console.dll"
+$fixie = "src/Fixie.Console/bin/Release/net5.0/Fixie.Console.dll"
 
 if (test-path artifacts) { remove-item artifacts -Recurse }
 

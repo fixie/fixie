3a9c989c521ee8bdfddde8d745f392f5c27f2c36
Patrick Lioi <patrick.lioi@gmail.com>
2020-09-12 20:10:37 -0500

    XML reports no longer rely on the ClassCompleted event.

--- src/Fixie/Internal/Listeners/ReportListener.cs
+++ src/Fixie/Internal/Listeners/ReportListener.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Xml.Linq;
     using static System.Environment;
 
@@ -10,13 +11,12 @@
         Handler<CaseSkipped>,
         Handler<CasePassed>,
         Handler<CaseFailed>,
-        Handler<ClassCompleted>,
         Handler<AssemblyCompleted>
     {
         readonly Action<XDocument> save;
 
-        readonly List<XElement> currentClass = new List<XElement>();
-        readonly List<XElement> classes = new List<XElement>();
+        ClassResult? currentClass;
+        readonly List<ClassResult> report = new List<ClassResult>();
 
         internal static ReportListener? Create()
         {
@@ -44,58 +44,23 @@
         }
 
         public void Handle(CaseSkipped message)
-        {
-            currentClass.Add(
-                new XElement("test",
-                    new XAttribute("name", message.Name),
-                    new XAttribute("type", message.Test.Class),
-                    new XAttribute("method", message.Test.Method),
-                    new XAttribute("result", "Skip"),
-                    new XAttribute("time", Seconds(message.Duration)),
-                    message.Reason != null
-                        ? new XElement("reason", new XCData(message.Reason))
-                        : null));
-        }
+            => TrackCurrentClass(message).Add(message);
 
         public void Handle(CasePassed message)
-        {
-            currentClass.Add(
-                new XElement("test",
-                    new XAttribute("name", message.Name),
-                    new XAttribute("type", message.Test.Class),
-                    new XAttribute("method", message.Test.Method),
-                    new XAttribute("result", "Pass"),
-                    new XAttribute("time", Seconds(message.Duration))));
-        }
+            => TrackCurrentClass(message).Add(message);
 
         public void Handle(CaseFailed message)
-        {
-            currentClass.Add(
-                new XElement("test",
-                    new XAttribute("name", message.Name),
-                    new XAttribute("type", message.Test.Class),
-                    new XAttribute("method", message.Test.Method),
-                    new XAttribute("result", "Fail"),
-                    new XAttribute("time", Seconds(message.Duration)),
-                    new XElement("failure",
-                        new XAttribute("exception-type", message.Exception.GetType().FullName),
-                        new XElement("message", new XCData(message.Exception.Message)),
-                        new XElement("stack-trace", new XCData(message.Exception.LiterateStackTrace())))));
-        }
+            => TrackCurrentClass(message).Add(message);
 
-        public void Handle(ClassCompleted message)
+        ClassResult TrackCurrentClass(CaseCompleted message)
         {
-            classes.Add(
-                new XElement("collection",
-                    new XAttribute("time", Seconds(message.Duration)),
-                    new XAttribute("name", message.Class.FullName),
-                    new XAttribute("total", message.Total),
-                    new XAttribute("passed", message.Passed),
-                    new XAttribute("failed", message.Failed),
-                    new XAttribute("skipped", message.Skipped),
-                    currentClass));
+            if (currentClass == null || currentClass.Name != message.Test.Class)
+            {
+                currentClass = new ClassResult(message.Test.Class);
+                report.Add(currentClass);
+            }
 
-            currentClass.Clear();
+            return currentClass;
         }
 
         public void Handle(AssemblyCompleted message)
@@ -115,9 +80,9 @@
                         new XAttribute("skipped", message.Skipped),
                         new XAttribute("environment", $"{IntPtr.Size * 8}-bit .NET {Framework}"),
                         new XAttribute("test-framework", Fixie.Framework.Version),
-                        classes))));
+                        report.Select(x => x.ToElement())))));
 
-            classes.Clear();
+            report.Clear();
         }
 
         static string Framework => Environment.Version.ToString();
@@ -140,6 +105,75 @@
             using var stream = new FileStream(path, FileMode.Create);
             using var writer = new StreamWriter(stream);
             report.Save(writer, SaveOptions.None);
+        }
+
+        class ClassResult
+        {
+            TimeSpan duration = TimeSpan.Zero;
+            readonly List<XElement> results = new List<XElement>();
+            readonly ExecutionSummary summary = new ExecutionSummary();
+
+            public ClassResult(string name) => Name = name;
+
+            public string Name { get; }
+
+            public void Add(CaseSkipped message)
+            {
+                duration += message.Duration;
+                summary.Add(message);
+                results.Add(
+                    new XElement("test",
+                        new XAttribute("name", message.Name),
+                        new XAttribute("type", message.Test.Class),
+                        new XAttribute("method", message.Test.Method),
+                        new XAttribute("result", "Skip"),
+                        new XAttribute("time", Seconds(message.Duration)),
+                        message.Reason != null
+                            ? new XElement("reason", new XCData(message.Reason))
+                            : null));
+            }
+
+            public void Add(CasePassed message)
+            {
+                duration += message.Duration;
+                summary.Add(message);
+                results.Add(
+                    new XElement("test",
+                        new XAttribute("name", message.Name),
+                        new XAttribute("type", message.Test.Class),
+                        new XAttribute("method", message.Test.Method),
+                        new XAttribute("result", "Pass"),
+                        new XAttribute("time", Seconds(message.Duration))));
+            }
+
+            public void Add(CaseFailed message)
+            {
+                duration += message.Duration;
+                summary.Add(message);
+                results.Add(
+                    new XElement("test",
+                        new XAttribute("name", message.Name),
+                        new XAttribute("type", message.Test.Class),
+                        new XAttribute("method", message.Test.Method),
+                        new XAttribute("result", "Fail"),
+                        new XAttribute("time", Seconds(message.Duration)),
+                        new XElement("failure",
+                            new XAttribute("exception-type", message.Exception.GetType().FullName!),
+                            new XElement("message", new XCData(message.Exception.Message)),
+                            new XElement("stack-trace", new XCData(message.Exception.LiterateStackTrace())))));
+            }
+
+            public XElement ToElement()
+            {
+                return new XElement("collection",
+                    new XAttribute("time", Seconds(duration)),
+                    new XAttribute("name", Name),
+                    new XAttribute("total", summary.Total),
+                    new XAttribute("passed", summary.Passed),
+                    new XAttribute("failed", summary.Failed),
+                    new XAttribute("skipped", summary.Skipped),
+                    results);
+            }
         }
     }
 }

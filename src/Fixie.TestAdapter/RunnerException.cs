bdcf7cc11fe2be2c30463229b41945bfeb8a30b6
Patrick Lioi <patrick.lioi@gmail.com>
2020-09-17 20:35:47 -0500

    Ensure that when an otherwise-unhandled exception is thrown by the test adapter, all of the exception details arrive in the IDE's Output pane including all stack trace information for the outer and inner exceptions.

--- src/Fixie.TestAdapter/RunnerException.cs
+++ src/Fixie.TestAdapter/RunnerException.cs
@@ -1,19 +1,11 @@
 ﻿namespace Fixie.TestAdapter
 {
     using System;
-    using System.Text;
 
     class RunnerException : Exception
     {
         public RunnerException(Exception exception)
-            : base(new StringBuilder()
-                .AppendLine()
-                .AppendLine()
-                .AppendLine(exception.Message)
-                .AppendLine()
-                .AppendLine(exception.GetType().FullName)
-                .AppendLine(exception.StackTrace)
-                .ToString())
+            : base(exception.ToString())
         {
         }
     }

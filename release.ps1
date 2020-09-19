9fa71800664fc5e0f0104342c131514549d1cc95
Patrick Lioi <patrick.lioi@gmail.com>
2020-09-19 12:44:44 -0500

    When pushing nuget packages, push the primary package and avoid redundantly attempting to push the symbols packages.

--- release.ps1
+++ release.ps1
@@ -1,2 +1,6 @@
-﻿dotnet nuget push 'artifacts/*.nupkg' --source https://api.nuget.org/v3/index.json --api-key $env:NUGET_API_KEY
-if ($lastexitcode -ne 0) { throw $lastexitcode }
+﻿$packages = get-childitem artifacts/*.nupkg | where-object {!$_.Name.EndsWith(".symbols.nupkg")}
+
+foreach ($package in $packages) {
+    dotnet nuget push $package --source https://api.nuget.org/v3/index.json --api-key $env:NUGET_API_KEY
+    if ($lastexitcode -ne 0) { throw $lastexitcode }
+}

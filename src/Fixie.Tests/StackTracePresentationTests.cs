10c55e45006c8eec49a23073c8451d3071e940a2
Patrick Lioi <patrick.lioi@gmail.com>
2020-09-19 09:50:56 -0500

    Provide clean stack traces for async test failures.

---
+++ src/Fixie.Tests/StackTracePresentationTests.cs
@@ -0,0 +1,118 @@
+﻿namespace Fixie.Tests
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Assertions;
+    using Fixie.Internal;
+    using Fixie.Internal.Listeners;
+    using static Utility;
+
+    public class StackTracePresentationTests
+    {
+        public void ShouldProvideCleanStackTraceForImplicitTestClassConstructionFailures()
+        {
+            Run<ConstructionFailureTestClass, ImplicitConstruction>()
+                .ShouldBe(
+                    "Test '" + FullName<ConstructionFailureTestClass>() + ".UnreachableTest' failed:",
+                    "",
+                    "'.ctor' failed!",
+                    "",
+                    "Fixie.Tests.FailureException",
+                    At<ConstructionFailureTestClass>(".ctor()"),
+                    "",
+                    "1 failed, took 1.23 seconds");
+        }
+        
+        public void ShouldProvideCleanStackTraceForExplicitTestClassConstructionFailures()
+        {
+            Run<ConstructionFailureTestClass, ExplicitConstruction>()
+                .ShouldBe(
+                    "Test '" + FullName<ConstructionFailureTestClass>() + ".UnreachableTest' skipped:",
+                    "This test did not run.",
+                    "",
+                    "Test '" + FullName<ConstructionFailureTestClass>() + ".UnreachableTest' failed:",
+                    "",
+                    "'.ctor' failed!",
+                    "",
+                    "Fixie.Tests.FailureException",
+                    At<ConstructionFailureTestClass>(".ctor()"),
+                    "",
+                    "1 failed, 1 skipped, took 1.23 seconds");
+        }
+
+        public void ShouldProvideCleanStackTraceTestMethodFailures()
+        {
+            Run<FailureTestClass, ImplicitConstruction>()
+                .ShouldBe(
+                    "Test '" + FullName<FailureTestClass>() + ".Asynchronous' failed:",
+                    "",
+                    "'Asynchronous' failed!",
+                    "",
+                    "Fixie.Tests.FailureException",
+                    At<FailureTestClass>("Asynchronous()"),
+                    "",
+                    "Test '" + FullName<FailureTestClass>() + ".Synchronous' failed:",
+                    "",
+                    "'Synchronous' failed!",
+                    "",
+                    "Fixie.Tests.FailureException",
+                    At<FailureTestClass>("Synchronous()"),
+                    "",
+                    "2 failed, took 1.23 seconds");
+        }
+
+        static IEnumerable<string> Run<TSampleTestClass, TExecution>() where TExecution : Execution, new()
+        {
+            var listener = new ConsoleListener();
+            var discovery = new SelfTestDiscovery();
+            var execution = new TExecution();
+            
+            using var console = new RedirectedConsole();
+
+            Utility.Run(listener, discovery, execution, typeof(TSampleTestClass));
+
+            return console.Lines()
+                .CleanStackTraceLineNumbers()
+                .CleanDuration();
+        }
+
+        class ImplicitConstruction : Execution
+        {
+            public void Execute(TestClass testClass)
+            {
+                foreach (var test in testClass.Tests)
+                    test.Run();
+            }
+        }
+
+        class ExplicitConstruction : Execution
+        {
+            public void Execute(TestClass testClass)
+            {
+                var instance = testClass.Construct();
+                foreach (var test in testClass.Tests)
+                    test.Run(instance);
+            }
+        }
+
+        class ConstructionFailureTestClass
+        {
+            public ConstructionFailureTestClass() => throw new FailureException();
+            public void UnreachableTest() => throw new ShouldBeUnreachableException();
+        }
+
+        class FailureTestClass
+        {
+            public void Synchronous()
+            {
+                throw new FailureException();
+            }
+
+            public async Task Asynchronous()
+            {
+                await Task.Yield();
+                throw new FailureException();
+            }
+        }
+    }
+}
--- src/Fixie/Internal/Listeners/ExceptionExtensions.cs
+++ src/Fixie/Internal/Listeners/ExceptionExtensions.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Text;
+    using static System.Environment;
 
     static class ExceptionExtensions
     {
@@ -9,7 +10,7 @@
         {
             var console = new StringBuilder();
 
-            console.Append(exception.StackTrace);
+            console.Append(StackTraceOmittingInternalTestMethodFrames(exception));
 
             var walk = exception;
             while (walk.InnerException != null)
@@ -23,6 +24,32 @@
             }
 
             return console.ToString();
+        }
+
+        static string? StackTraceOmittingInternalTestMethodFrames(Exception exception)
+        {
+            // Test method invocation usually produces clean stack traces.
+            // Async test stack traces, however, include test runner implementation
+            // details which distract the user in their diagnosing of a test failure.
+            // Similar to the way that assertion libraries simplify their assertion
+            // exception stack traces to omit implementation details of the assertion
+            // library, we omit these test method invocation implementation details as
+            // well.
+
+            if (exception.StackTrace == null)
+                return null;
+
+            var lines = exception.StackTrace.Split(NewLine);
+
+            if (lines.Length >= 2)
+            {
+                if (lines[^2].Contains(" Fixie.MethodInfoExtensions.Execute(MethodInfo method, Object instance, Object[] parameters)") &&
+                    lines[^1].Contains(" Fixie.Case.Execute(Object instance)"))
+
+                    return string.Join(NewLine, lines[..^2]);
+            }
+
+            return exception.StackTrace;
         }
     }
 }

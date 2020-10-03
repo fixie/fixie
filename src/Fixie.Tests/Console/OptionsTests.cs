48e0b21f4f47cfe6ebcc4872f573a68b0d5a74ef
Patrick Lioi <patrick.lioi@gmail.com>
2020-10-02 20:12:19 -0500

    `dotnet fixie` accepts a --tests pattern argument.

--- src/Fixie.Tests/Console/OptionsTests.cs
+++ src/Fixie.Tests/Console/OptionsTests.cs
@@ -8,13 +8,13 @@
     {
         public void DemandsValidReportFileNameWhenProvided()
         {
-            Action noReport = new Options(null, false, null, report: null).Validate;
+            Action noReport = new Options(null, false, null, report: null, tests: null).Validate;
             noReport();
 
-            Action validReport = new Options(null, false, null, report: "Report.xml").Validate;
+            Action validReport = new Options(null, false, null, report: "Report.xml", tests: null).Validate;
             validReport();
 
-            Action invalidReport = new Options(null, false, null, report: "\0").Validate;
+            Action invalidReport = new Options(null, false, null, report: "\0", tests: null).Validate;
             invalidReport.ShouldThrow<CommandLineException>(
                 "Specified report name is invalid: \0");
         }

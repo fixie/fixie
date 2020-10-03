2cc82b03384fefe90a487161fd199e0eb6faffc9
Patrick Lioi <patrick.lioi@gmail.com>
2020-10-03 17:06:44 -0500

    Consistent environment variable naming across workflows.

--- release.ps1
+++ release.ps1
@@ -1,6 +1,6 @@
 ﻿$packages = get-childitem artifacts/*.nupkg | where-object {!$_.Name.EndsWith(".symbols.nupkg")}
 
 foreach ($package in $packages) {
-    dotnet nuget push $package --source $env:NUGET_URL --api-key $env:NUGET_API_KEY
+    dotnet nuget push $package --source $env:PACKAGE_URL --api-key $env:PACKAGE_API_KEY
     if ($lastexitcode -ne 0) { throw $lastexitcode }
 }

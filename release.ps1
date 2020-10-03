e496815b5e2941b7b936ed3dc8a04ddeb68239b5
Patrick Lioi <patrick.lioi@gmail.com>
2020-10-03 16:50:57 -0500

    The release.ps1 script relies on an environment variable for the NuGet package feed URL to push to.

--- release.ps1
+++ release.ps1
@@ -1,6 +1,6 @@
 ﻿$packages = get-childitem artifacts/*.nupkg | where-object {!$_.Name.EndsWith(".symbols.nupkg")}
 
 foreach ($package in $packages) {
-    dotnet nuget push $package --source https://api.nuget.org/v3/index.json --api-key $env:NUGET_API_KEY
+    dotnet nuget push $package --source $env:NUGET_URL --api-key $env:NUGET_API_KEY
     if ($lastexitcode -ne 0) { throw $lastexitcode }
 }

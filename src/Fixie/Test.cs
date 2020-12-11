3e93dd543d0ce3827ac9bf069824bee617cfb464
Patrick Lioi <patrick.lioi@gmail.com>
2020-12-11 12:14:30 -0600

    Test construction is internal.

--- src/Fixie/Test.cs
+++ src/Fixie/Test.cs
@@ -8,21 +8,21 @@ namespace Fixie
         public string Method { get; }
         public string Name { get; }
 
-        public Test(MethodInfo method)
+        internal Test(MethodInfo method)
         {
             Class = method.ReflectedType!.FullName!;
             Method = method.Name;
             Name = Class + "." + Method;
         }
 
-        public Test(string @class, string method)
+        internal Test(string @class, string method)
         {
             Class = @class;
             Method = method;
             Name = Class + "." + Method;
         }
 
-        public Test(string name)
+        internal Test(string name)
         {
             var indexOfMemberSeparator = name.LastIndexOf(".");
             var className = name.Substring(0, indexOfMemberSeparator);

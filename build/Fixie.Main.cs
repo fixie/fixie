5afc1882bb54cff8ad42abd3e91584a28eb4f9ba
Patrick Lioi <patrick.lioi@gmail.com>
2021-04-03 09:24:15 -0500

    Rename the package-included Main() wrapper class so that it is difficult to navigate to accidentally, and document the nature of the included file for those who do navigate here.

---
+++ build/Fixie.Main.cs
@@ -0,0 +1,15 @@
+﻿namespace Fixie.Internal
+{
+    using System;
+    using System.Threading.Tasks;
+
+    // The 'Fixie' package includes this file in test projects so
+    // that their tests can be executed. Do not modify this file.
+
+    class ___
+    {
+        [STAThread]
+        static async Task<int> Main(string[] customArguments)
+            => await EntryPoint.Main(typeof(___).Assembly, customArguments);
+    }
+}
--- build/Fixie.Main.fs
+++ build/Fixie.Main.fs
@@ -1,7 +1,10 @@
-﻿module Fixie.Internal.Program
+﻿module Fixie.Internal.___
 
 open System
 open System.Reflection
+
+// The 'Fixie' package includes this file in test projects so
+// that their tests can be executed. Do not modify this file.
 
 [<STAThread; EntryPoint>]
 let main customArguments =

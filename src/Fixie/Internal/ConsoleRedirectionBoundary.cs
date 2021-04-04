6522d893ee7ac923db82b7b557a820614cbce604
Patrick Lioi <patrick.lioi@gmail.com>
2021-04-04 13:05:39 -0500

    A console redirection boundary identifies a code block in which the standard output stream may be redirected even by untrusted code. When the boundary completes, though, standard out will be reverted to the TextWriter in effect at the start of the boundary.

---
+++ src/Fixie/Internal/ConsoleRedirectionBoundary.cs
@@ -0,0 +1,16 @@
+﻿namespace Fixie.Internal
+{
+    using System;
+    using System.IO;
+
+    class ConsoleRedirectionBoundary : IDisposable
+    {
+        readonly TextWriter original;
+
+        public ConsoleRedirectionBoundary() => original = Console.Out;
+
+        void Revert() => Console.SetOut(original);
+
+        public void Dispose() => Revert();
+    }
+}

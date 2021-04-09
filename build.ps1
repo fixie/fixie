7925b8ce0c1841f188439f1416ec729d1ad279bf
Patrick Lioi <patrick.lioi@gmail.com>
2021-04-08 22:03:56 -0500

    The build script --pack option allows developers to avoid packing by default throughout development.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,6 @@
-﻿$ErrorActionPreference = "Stop"
+﻿param([switch]$pack)
+
+$ErrorActionPreference = "Stop"
 
 function step($command) {
     write-host ([Environment]::NewLine + $command.ToString().Trim()) -fore CYAN
@@ -13,9 +15,12 @@ if (test-path artifacts) { remove-item artifacts -Recurse }
 step { dotnet clean src -c Release --nologo -v minimal }
 step { dotnet build src -c Release --nologo }
 step { dotnet $fixie *.Tests -c Release --no-build }
-step { dotnet pack src/Fixie -o artifacts -c Release --no-build --nologo }
-step { dotnet pack src/Fixie.Console -o artifacts -c Release --no-build --nologo }
 
-# While Fixie.TestAdapter packs with a nuspec file, we cannot include --no-build here.
-# If we did, the MinVer-calculated version would fail to apply to the package.
-step { dotnet pack src/Fixie.TestAdapter -o artifacts -c Release --nologo }
+if ($pack) {
+    step { dotnet pack src/Fixie -o artifacts -c Release --no-build --nologo }
+    step { dotnet pack src/Fixie.Console -o artifacts -c Release --no-build --nologo }
+
+    # While Fixie.TestAdapter packs with a nuspec file, we cannot include --no-build here.
+    # If we did, the MinVer-calculated version would fail to apply to the package.
+    step { dotnet pack src/Fixie.TestAdapter -o artifacts -c Release --nologo }
+}

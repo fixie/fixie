cb6f4354b9294838daec1fa07d49ef7f938f6514
Patrick Lioi <patrick.lioi@gmail.com>
2021-04-11 20:40:17 -0500

    A TestEnvironment wraps the test Assembly, the custom arguments, and the primary standard output console TextWriter.

---
+++ src/Fixie/Internal/TestEnvironment.cs
@@ -0,0 +1,19 @@
+﻿namespace Fixie.Internal
+{
+    using System.IO;
+    using System.Reflection;
+
+    class TestEnvironment
+    {
+        public TestEnvironment(Assembly assembly, string[] customArguments, TextWriter console)
+        {
+            Assembly = assembly;
+            CustomArguments = customArguments;
+            Console = console;
+        }
+
+        public Assembly Assembly { get; }
+        public string[] CustomArguments { get; }
+        public TextWriter Console { get; }
+    }
+}

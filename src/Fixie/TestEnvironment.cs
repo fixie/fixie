2ca89de37bd24d78ebff22b215383ad567dcf3f0
Patrick Lioi <patrick.lioi@gmail.com>
2021-04-12 22:11:43 -0500

    TestEnvironment is public, now that it can be injected into Discovery and Execution types.

--- src/Fixie/TestEnvironment.cs
+++ src/Fixie/TestEnvironment.cs
@@ -1,14 +1,16 @@
-﻿namespace Fixie.Internal
+﻿namespace Fixie
 {
+    using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Reflection;
 
-    class TestEnvironment
+    public class TestEnvironment
     {
         public TestEnvironment(Assembly assembly, TextWriter console, string rootDirectory)
-            : this(assembly, new string[] {}, console, rootDirectory) { }
+            : this(assembly, Array.Empty<string>(), console, rootDirectory) { }
 
-        public TestEnvironment(Assembly assembly, string[] customArguments, TextWriter console, string rootDirectory)
+        public TestEnvironment(Assembly assembly, IReadOnlyList<string> customArguments, TextWriter console, string rootDirectory)
         {
             Assembly = assembly;
             CustomArguments = customArguments;
@@ -17,7 +19,7 @@
         }
 
         public Assembly Assembly { get; }
-        public string[] CustomArguments { get; }
+        public IReadOnlyList<string> CustomArguments { get; }
         public TextWriter Console { get; }
         public string RootDirectory { get; }
     }

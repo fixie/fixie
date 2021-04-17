3eefc7cf822029499e9d43a3a10f04817c30984a
Patrick Lioi <patrick.lioi@gmail.com>
2021-04-17 16:36:02 -0500

    Reimplement case name tests using modern self testing patterns, primarily so that the tests no longer depend directly on the deprecated Case class, and also to provide coverage of *all* 4 code paths which currently rely on Case for naming (actually running a test vs explicitly emitting pass/fail/skip results).

---
+++ src/Fixie.Tests/CaseNameTests.cs
@@ -0,0 +1,290 @@
+﻿namespace Fixie.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Assertions;
+    using static Utility;
+
+    public class CaseNameTests
+    {
+        public async Task ShouldBeNamedAfterTheUnderlyingMethod()
+        {
+            var output = await RunScriptAsync<NoParametersTestClass>(async test =>
+            {
+                await RunAsync(test);
+            });
+
+            ShouldHaveNames(output, "NoParametersTestClass.NoParameters");
+        }
+
+        public async Task ShouldIncludeParameterValuesInNameWhenTheUnderlyingMethodHasParameters()
+        {
+            var output = await RunScriptAsync<ParameterizedTestClass>(async test =>
+            {
+                await RunAsync(test,
+                    123, true, 'a', "with \"quotes\"", "long \"string\" gets truncated",
+                    null, this, new ObjectWithNullStringRepresentation());
+            });
+
+            ShouldHaveNames(output,
+                "ParameterizedTestClass.Parameterized(123, True, 'a', \"with \\\"quotes\\\"\", \"long \\\"string\\\" g...\", null, Fixie.Tests.CaseNameTests, Fixie.Tests.CaseNameTests+ObjectWithNullStringRepresentation)");
+        }
+
+        public async Task ShouldIncludeEscapeSequencesInNameWhenTheUnderlyingMethodHasCharParameters()
+        {
+            // Unicode characters 0085, 2028, and 2029 represent line endings Next Line, Line Separator, and Paragraph Separator, respectively.
+            // Just like \r and \n, we escape these in order to present a readable string literal. All other unicode sequences pass through
+            // with no additional special treatment.
+
+            // \uxxxx - Unicode escape sequence for character with hex value xxxx.
+            // \xn[n][n][n] - Unicode escape sequence for character with hex value nnnn (variable length version of \uxxxx).
+            // \Uxxxxxxxx - Unicode escape sequence for character with hex value xxxxxxxx (for generating surrogates).
+
+            var output = await RunScriptAsync<CharParametersTestClass>(async test =>
+            {
+                foreach (var c in new[] {'\"', '"', '\''})
+                    await RunAsync(test, c);
+                
+                foreach (var c in new[] {'\\', '\0', '\a', '\b', '\f', '\n', '\r', '\t', '\v'})
+                    await RunAsync(test, c);
+                
+                foreach (var c in new[] {'\u0000', '\u0085', '\u2028', '\u2029', '\u263A'})
+                    await RunAsync(test, c);
+                
+                foreach (var c in new[] {'\x0000', '\x000', '\x00', '\x0'})
+                    await RunAsync(test, c);
+                
+                foreach (var c in new[] {'\x0085', '\x085', '\x85', '\x2028', '\x2029', '\x263A'})
+                    await RunAsync(test, c);
+                
+                foreach (var c in new[] {'\U00000000', '\U00000085', '\U00002028', '\U00002029', '\U0000263A'})
+                    await RunAsync(test, c);
+            });
+
+            ShouldHaveNames(output,
+                "CharParametersTestClass.Char('\"')",
+                "CharParametersTestClass.Char('\"')",
+                "CharParametersTestClass.Char('\\'')",
+
+                "CharParametersTestClass.Char('\\\\')",
+                "CharParametersTestClass.Char('\\0')",
+                "CharParametersTestClass.Char('\\a')",
+                "CharParametersTestClass.Char('\\b')",
+                "CharParametersTestClass.Char('\\f')",
+                "CharParametersTestClass.Char('\\n')",
+                "CharParametersTestClass.Char('\\r')",
+                "CharParametersTestClass.Char('\\t')",
+                "CharParametersTestClass.Char('\\v')",
+                
+                "CharParametersTestClass.Char('\\0')",
+                "CharParametersTestClass.Char('\\u0085')",
+                "CharParametersTestClass.Char('\\u2028')",
+                "CharParametersTestClass.Char('\\u2029')",
+                "CharParametersTestClass.Char('☺')",
+
+                "CharParametersTestClass.Char('\\0')",
+                "CharParametersTestClass.Char('\\0')",
+                "CharParametersTestClass.Char('\\0')",
+                "CharParametersTestClass.Char('\\0')",
+
+                "CharParametersTestClass.Char('\\u0085')",
+                "CharParametersTestClass.Char('\\u0085')",
+                "CharParametersTestClass.Char('\\u0085')",
+                "CharParametersTestClass.Char('\\u2028')",
+                "CharParametersTestClass.Char('\\u2029')",
+                "CharParametersTestClass.Char('☺')",
+
+                "CharParametersTestClass.Char('\\0')",
+                "CharParametersTestClass.Char('\\u0085')",
+                "CharParametersTestClass.Char('\\u2028')",
+                "CharParametersTestClass.Char('\\u2029')",
+                "CharParametersTestClass.Char('☺')"
+            );
+        }
+
+        public async Task ShouldIncludeEscapeSequencesInNameWhenTheUnderlyingMethodHasStringParameters()
+        {
+            // Unicode characters 0085, 2028, and 2029 represent line endings Next Line, Line Separator, and Paragraph Separator, respectively.
+            // Just like \r and \n, we escape these in order to present a readable string literal. All other unicode sequences pass through
+            // with no additional special treatment.
+
+            // \uxxxx - Unicode escape sequence for character with hex value xxxx.
+            // \xn[n][n][n] - Unicode escape sequence for character with hex value nnnn (variable length version of \uxxxx).
+            // \Uxxxxxxxx - Unicode escape sequence for character with hex value xxxxxxxx (for generating surrogates).
+
+            var output = await RunScriptAsync<StringParametersTestClass>(async test =>
+            {
+                await RunAsync(test, " \' ' \" ");
+                await RunAsync(test, " \\ \0 \a \b ");
+                await RunAsync(test, " \f \n \r \t \v ");
+                await RunAsync(test, " \u0000 \u0085 \u2028 \u2029 \u263A ");
+                await RunAsync(test, " \x0000 \x000 \x00 \x0 ");
+                await RunAsync(test, " \x0085 \x085 \x85 \x2028 \x2029 \x263A ");
+                await RunAsync(test, " \U00000000 \U00000085 \U00002028 \U00002029 \U0000263A ");
+            });
+
+            ShouldHaveNames(output,
+                "StringParametersTestClass.String(\" ' ' \\\" \")",
+                "StringParametersTestClass.String(\" \\\\ \\0 \\a \\b \")",
+                "StringParametersTestClass.String(\" \\f \\n \\r \\t \\v \")",
+                "StringParametersTestClass.String(\" \\0 \\u0085 \\u2028 \\u2029 ☺ \")",
+                "StringParametersTestClass.String(\" \\0 \\0 \\0 \\0 \")",
+                "StringParametersTestClass.String(\" \\u0085 \\u0085 \\u0085 \\u2028 \\u2029 ☺ \")",
+                "StringParametersTestClass.String(\" \\0 \\u0085 \\u2028 \\u2029 ☺ \")"
+            );
+        }
+
+        public async Task ShouldIncludeResolvedGenericArgumentsInNameWhenTheUnderlyingMethodIsGeneric()
+        {
+            var output = await RunScriptAsync<GenericTestClass>(async test =>
+            {
+                await RunAsync(test, 123, true, "a", "b");
+                await RunAsync(test, 123, true, 1, null);
+                await RunAsync(test, 123, 1.23m, "a", null);
+            });
+
+            ShouldHaveNames(output,
+                "GenericTestClass.Generic<System.Boolean, System.String>(123, True, \"a\", \"b\")",
+                "GenericTestClass.Generic<System.Boolean, System.Int32>(123, True, 1, null)",
+                "GenericTestClass.Generic<System.Decimal, System.String>(123, 1.23, \"a\", null)"
+            );
+        }
+
+        public async Task ShouldUseGenericTypeParametersInNameWhenGenericTypeParametersCannotBeResolved()
+        {
+            var output = await RunScriptAsync<ConstrainedGenericTestClass>(async test =>
+            {
+                await RunAsync(test, 1);
+                await RunAsync(test, true);
+                await RunAsync(test, "Incompatible");
+            });
+
+            ShouldHaveNames(output,
+                "ConstrainedGenericTestClass.ConstrainedGeneric<System.Int32>(1)",
+                "ConstrainedGenericTestClass.ConstrainedGeneric<System.Boolean>(True)",
+                "ConstrainedGenericTestClass.ConstrainedGeneric<T>(\"Incompatible\")"
+            );
+        }
+
+        public async Task ShouldInferAppropriateClassUnderInheritance()
+        {
+            var parent = await RunScriptAsync<ParentTestClass>(async test =>
+            {
+                await RunAsync(test);
+            });
+
+            ShouldHaveNames(parent,
+                "ParentTestClass.TestMethodDefinedWithinParentClass"
+            );
+
+            var child = await RunScriptAsync<ChildTestClass>(async test =>
+            {
+                await RunAsync(test);
+            });
+
+            ShouldHaveNames(child,
+                "ChildTestClass.TestMethodDefinedWithinChildClass",
+                "ChildTestClass.TestMethodDefinedWithinParentClass"
+            );
+        }
+
+        class ScriptedExecution : Execution
+        {
+            readonly Func<Test, Task> scriptAsync;
+
+            public ScriptedExecution(Func<Test, Task> scriptAsync)
+                => this.scriptAsync = scriptAsync;
+
+            public async Task RunAsync(TestAssembly testAssembly)
+            {
+                foreach (var test in testAssembly.Tests)
+                    await scriptAsync(test);
+            }
+        }
+
+        static Task<IEnumerable<string>> RunScriptAsync<TSampleTestClass>(Func<Test, Task> scriptAsync)
+            => RunAsync<TSampleTestClass>(new ScriptedExecution(scriptAsync));
+
+        static async Task RunAsync(Test test, params object?[] parameters)
+        {
+            await test.RunAsync(parameters);
+            await test.PassAsync(parameters);
+            await test.FailAsync(parameters, new FailureException());
+            await test.SkipAsync(parameters, reason: null);
+        }
+
+        void ShouldHaveNames(IEnumerable<string> actual, params string[] expected)
+        {
+            const int variants = 4;
+
+            var actualArray = actual.ToArray();
+            actualArray.Length.ShouldBe(expected.Length * variants);
+
+            var expectedVariants = expected.Select(name => new[]
+            {
+                name.Contains("Incompatible")
+                    ? $"{name} failed: Could not resolve type parameters for generic method."
+                    : $"{name} passed",
+                $"{name} passed",
+                $"{name} failed: 'RunAsync' failed!",
+                $"{name} skipped"
+            }).SelectMany(x => x);
+
+            var fullyQualifiedExpectation = expectedVariants.Select(x => GetType().FullName + "+" + x).ToArray();
+
+            foreach (var (first, second) in actualArray.Zip(fullyQualifiedExpectation))
+                first.ShouldBe(second);
+        }
+
+        class ObjectWithNullStringRepresentation
+        {
+            public override string? ToString() => null;
+        }
+
+        class NoParametersTestClass
+        {
+            public void NoParameters() { }
+        }
+
+        class ParameterizedTestClass
+        {
+            public void Parameterized(int i, bool b, char ch, string s1, string s2, object obj, CaseNameTests complex, ObjectWithNullStringRepresentation nullStringRepresentation) { }
+        }
+
+        class CharParametersTestClass
+        {
+            public void Char(char c) { }
+        }
+
+        class StringParametersTestClass
+        {
+            public void String(string s) { }
+        }
+
+        class GenericTestClass
+        {
+            public void Generic<T1, T2>(int i, T1 t1, T2 t2a, T2 t2b) { }
+        }
+
+        class ConstrainedGenericTestClass
+        {
+            public void ConstrainedGeneric<T>(T t) where T : struct { }
+        }
+
+        class ParentTestClass
+        {
+            public void TestMethodDefinedWithinParentClass()
+            {
+            }
+        }
+
+        class ChildTestClass : ParentTestClass
+        {
+            public void TestMethodDefinedWithinChildClass()
+            {
+            }
+        }
+    }
+}
--- src/Fixie.Tests/Internal/CaseTests.cs
+++ src/Fixie.Tests/Internal/CaseTests.cs
@@ -8,160 +8,13 @@
 
     public class CaseTests
     {
-        public void ShouldBeNamedAfterTheUnderlyingMethod()
-        {
-            var @case = Case("Returns");
-
-            @case.Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Returns");
-        }
-
-        public void ShouldIncludeParameterValuesInNameWhenTheUnderlyingMethodHasParameters()
-        {
-            var @case = Case("Parameterized", 123, true, 'a', "with \"quotes\"", "long \"string\" gets truncated", null, this, new ObjectWithNullStringRepresentation());
-
-            @case.Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Parameterized(123, True, 'a', \"with \\\"quotes\\\"\", \"long \\\"string\\\" g...\", null, Fixie.Tests.Internal.CaseTests, Fixie.Tests.Internal.CaseTests+ObjectWithNullStringRepresentation)");
-        }
-
-        public void ShouldIncludeEscapeSequencesInNameWhenTheUnderlyingMethodHasCharParameters()
-        {
-            Case("Char", '\"').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\"')");
-            Case("Char", '"').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\"')");
-            Case("Char", '\'').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\'')");
-
-            Case("Char", '\\').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\\\')");
-            Case("Char", '\0').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\0')");
-            Case("Char", '\a').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\a')");
-            Case("Char", '\b').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\b')");
-            Case("Char", '\f').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\f')");
-            Case("Char", '\n').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\n')");
-            Case("Char", '\r').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\r')");
-            Case("Char", '\t').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\t')");
-            Case("Char", '\v').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\v')");
-
-            // Unicode characters 0085, 2028, and 2029 represent line endings Next Line, Line Separator, and Paragraph Separator, respectively.
-            // Just like \r and \n, we escape these in order to present a readable string literal.  All other unicode sequences pass through
-            // with no additional special treatment.
-
-            // \uxxxx - Unicode escape sequence for character with hex value xxxx.
-            Case("Char", '\u0000').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\0')");
-            Case("Char", '\u0085').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u0085')");
-            Case("Char", '\u2028').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u2028')");
-            Case("Char", '\u2029').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u2029')");
-            Case("Char", '\u263A').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('☺')");
-
-            // \xn[n][n][n] - Unicode escape sequence for character with hex value nnnn (variable length version of \uxxxx).
-            Case("Char", '\x0000').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\0')");
-            Case("Char", '\x000').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\0')");
-            Case("Char", '\x00').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\0')");
-            Case("Char", '\x0').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\0')");
-            Case("Char", '\x0085').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u0085')");
-            Case("Char", '\x085').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u0085')");
-            Case("Char", '\x85').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u0085')");
-            Case("Char", '\x2028').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u2028')");
-            Case("Char", '\x2029').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u2029')");
-            Case("Char", '\x263A').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('☺')");
-
-            //\Uxxxxxxxx - Unicode escape sequence for character with hex value xxxxxxxx (for generating surrogates).
-            Case("Char", '\U00000000').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\0')");
-            Case("Char", '\U00000085').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u0085')");
-            Case("Char", '\U00002028').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u2028')");
-            Case("Char", '\U00002029').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('\\u2029')");
-            Case("Char", '\U0000263A').Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Char('☺')");
-        }
-
-        public void ShouldIncludeEscapeSequencesInNameWhenTheUnderlyingMethodHasStringParameters()
-        {
-            Case("String", "\'").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"'\")");
-            Case("String", "'").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"'\")");
-            Case("String", "\"").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\\"\")");
-
-            Case("String", "\\").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\\\\")");
-            Case("String", "\0").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\0\")");
-            Case("String", "\a").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\a\")");
-            Case("String", "\b").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\b\")");
-            Case("String", "\f").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\f\")");
-            Case("String", "\n").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\n\")");
-            Case("String", "\r").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\r\")");
-            Case("String", "\t").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\t\")");
-            Case("String", "\v").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\v\")");
-
-            // Unicode characters 0085, 2028, and 2029 represent line endings Next Line, Line Separator, and Paragraph Separator, respectively.
-            // Just like \r and \n, we escape these in order to present a readable string literal.  All other unicode sequences pass through
-            // with no additional special treatment.
-
-            // \uxxxx - Unicode escape sequence for character with hex value xxxx.
-            Case("String", "\u0000").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\0\")");
-            Case("String", "\u0085").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u0085\")");
-            Case("String", "\u2028").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u2028\")");
-            Case("String", "\u2029").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u2029\")");
-            Case("String", "\u263A").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"☺\")");
-
-            // \xn[n][n][n] - Unicode escape sequence for character with hex value nnnn (variable length version of \uxxxx).
-            Case("String", "\x0000").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\0\")");
-            Case("String", "\x000").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\0\")");
-            Case("String", "\x00").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\0\")");
-            Case("String", "\x0").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\0\")");
-            Case("String", "\x0085").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u0085\")");
-            Case("String", "\x085").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u0085\")");
-            Case("String", "\x85").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u0085\")");
-            Case("String", "\x2028").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u2028\")");
-            Case("String", "\x2029").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u2029\")");
-            Case("String", "\x263A").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"☺\")");
-
-            //\Uxxxxxxxx - Unicode escape sequence for character with hex value xxxxxxxx (for generating surrogates).
-            Case("String", "\U00000000").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\0\")");
-            Case("String", "\U00000085").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u0085\")");
-            Case("String", "\U00002028").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u2028\")");
-            Case("String", "\U00002029").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"\\u2029\")");
-            Case("String", "\U0000263A").Name.ShouldBe("Fixie.Tests.Internal.CaseTests.String(\"☺\")");
-        }
-
-        public void ShouldIncludeResolvedGenericArgumentsInNameWhenTheUnderlyingMethodIsGeneric()
-        {
-            Case("Generic", 123, true, "a", "b")
-                .Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Generic<System.Boolean, System.String>(123, True, \"a\", \"b\")");
-
-            Case("Generic", 123, true, 1, null)
-                .Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Generic<System.Boolean, System.Int32>(123, True, 1, null)");
-
-            Case("Generic", 123, 1.23m, "a", null)
-                .Name.ShouldBe("Fixie.Tests.Internal.CaseTests.Generic<System.Decimal, System.String>(123, 1.23, \"a\", null)");
-
-            Case("ConstrainedGeneric", 1)
-                .Name.ShouldBe("Fixie.Tests.Internal.CaseTests.ConstrainedGeneric<System.Int32>(1)");
-
-            Case("ConstrainedGeneric", true)
-                .Name.ShouldBe("Fixie.Tests.Internal.CaseTests.ConstrainedGeneric<System.Boolean>(True)");
-        }
-
-        public void ShouldUseGenericTypeParametersInNameWhenGenericTypeParametersCannotBeResolved()
-        {
-            Case("ConstrainedGeneric", "Incompatible")
-                .Name.ShouldBe("Fixie.Tests.Internal.CaseTests.ConstrainedGeneric<T>(\"Incompatible\")");
-        }
-
-        public void ShouldInferAppropriateClassGivenCaseMethod()
-        {
-            var methodDeclaredInChildClass =
-                Case<SampleChildTestClass>("TestMethodDefinedWithinChildClass");
-            methodDeclaredInChildClass.Name.ShouldBe("Fixie.Tests.Internal.CaseTests+SampleChildTestClass.TestMethodDefinedWithinChildClass");
-
-            var methodDeclaredInParentClass =
-                Case<SampleParentTestClass>("TestMethodDefinedWithinParentClass");
-            methodDeclaredInParentClass.Name.ShouldBe("Fixie.Tests.Internal.CaseTests+SampleParentTestClass.TestMethodDefinedWithinParentClass");
-
-            var parentMethodInheritedByChildClass =
-                Case<SampleChildTestClass>("TestMethodDefinedWithinParentClass");
-            parentMethodInheritedByChildClass.Name.ShouldBe("Fixie.Tests.Internal.CaseTests+SampleChildTestClass.TestMethodDefinedWithinParentClass");
-        }
-
         public void ShouldHaveMethodInfoIncludingResolvedGenericArguments()
         {
             var method = Case("Returns").Method;
             method.Name.ShouldBe("Returns");
             method.GetParameters().ShouldBeEmpty();
 
-            method = Case("Parameterized", 123, true, 'a', "s", null, this, new ObjectWithNullStringRepresentation()).Method;
+            method = Case("Parameterized", 123, true, 'a', "s", null, this).Method;
             method.Name.ShouldBe("Parameterized");
             method.GetParameters()
                 .Select(x => x.ParameterType)
@@ -169,8 +22,7 @@
                     typeof(int), typeof(bool),
                     typeof(char), typeof(string),
                     typeof(string), typeof(object),
-                    typeof(CaseTests),
-                    typeof(ObjectWithNullStringRepresentation));
+                    typeof(CaseTests));
 
             method = Case("Generic", 123, true, "a", "b").Method;
             method.Name.ShouldBe("Generic");
@@ -208,20 +60,6 @@
             unresolvedParameterType.IsGenericParameter.ShouldBe(true);
         }
 
-        class SampleParentTestClass
-        {
-            public void TestMethodDefinedWithinParentClass()
-            {
-            }
-        }
-
-        class SampleChildTestClass : SampleParentTestClass
-        {
-            public void TestMethodDefinedWithinChildClass()
-            {
-            }
-        }
-
         static Case Case(string methodName, params object?[] parameters)
             => Case<CaseTests>(methodName, parameters);
 
@@ -240,20 +78,7 @@
         {
         }
 
-        void Throws()
-        {
-            throw new FailureException();
-        }
-
-        void Parameterized(int i, bool b, char ch, string s1, string s2, object obj, CaseTests complex, ObjectWithNullStringRepresentation nullStringRepresentation)
-        {
-        }
-
-        void Char(char ch)
-        {
-        }
-
-        void String(string s)
+        void Parameterized(int i, bool b, char ch, string s1, string s2, object obj, CaseTests complex)
         {
         }
 
@@ -263,11 +88,6 @@
 
         void ConstrainedGeneric<T>(T t) where T : struct
         {
-        }
-
-        class ObjectWithNullStringRepresentation
-        {
-            public override string? ToString() => null;
         }
     }
 }

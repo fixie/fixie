a9d2f4ffedd53f960b5dc47460334bc39ab27342
Patrick Lioi <patrick.lioi@gmail.com>
2021-06-13 08:39:51 -0500

    Documentation for public ReflectionExtensions.

--- src/Fixie/ReflectionExtensions.cs
+++ src/Fixie/ReflectionExtensions.cs
@@ -9,23 +9,24 @@
     public static class ReflectionExtensions
     {
         internal static string TestName(this MethodInfo method)
-        {
-            return method.ReflectedType!.FullName! + "." + method.Name;
-        }
+            => method.ReflectedType!.FullName! + "." + method.Name;
 
         internal static bool IsStatic(this Type type)
-        {
-            return type.IsAbstract && type.IsSealed;
-        }
+            => type.IsAbstract && type.IsSealed;
 
+        /// <summary>
+        /// Determines whether any attributes of the specified type are applied to a
+        /// specified member including the ancestors of that member.
+        /// </summary>
         public static bool Has<TAttribute>(this MemberInfo member) where TAttribute : Attribute
-        {
-            return member.GetCustomAttributes<TAttribute>(true).Any();
-        }
+            => member.GetCustomAttributes<TAttribute>(true).Any();
 
+        /// <summary>
+        /// Determines whether an attribute of the specified type is applied to a
+        /// specified member including the ancestors of that member, providing that
+        /// attribute as an `out` parameter when found.
+        /// </summary>
         public static bool Has<TAttribute>(this MemberInfo member, [NotNullWhen(true)] out TAttribute? matchingAttribute) where TAttribute : Attribute
-        {
-            return Try(() => member.GetCustomAttribute<TAttribute>(true), out matchingAttribute);
-        }
+            => Try(() => member.GetCustomAttribute<TAttribute>(true), out matchingAttribute);
     }
 }

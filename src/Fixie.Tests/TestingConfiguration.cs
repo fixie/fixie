27a09f6c5e1a9801e46635bf0fdcc285a491808a
Patrick Lioi <patrick.lioi@gmail.com>
2021-07-24 19:38:28 -0500

    Discovery of custom behaviors happens through seeking a Configuration subclass in the test assembly, rather than directly seeking IDiscovery/IExecution types in teh test assembly, allowing the actual IDiscovery/IExecution types to be declared in other assemblies and therefore shared/reused.

---
+++ src/Fixie.Tests/TestingConfiguration.cs
@@ -0,0 +1,79 @@
+﻿namespace Fixie.Tests
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Assertions;
+    using static System.Environment;
+    using static Fixie.Internal.Maybe;
+
+    class TestingConfiguration : Configuration
+    {
+        public TestingConfiguration()
+        {
+            Use<DefaultDiscovery, DiffToolExecution>();
+        }
+
+        class DiffToolExecution : IExecution
+        {
+            public async Task RunAsync(TestSuite testSuite)
+            {
+                int failures = 0;
+                Exception? singleFailure = null;
+
+                foreach (var test in testSuite.Tests)
+                {
+                    var result = await test.RunAsync();
+
+                    if (result is Failed failure)
+                    {
+                        failures++;
+
+                        singleFailure = failures == 1 ? failure.Reason : null;
+                    }
+                }
+
+                if (singleFailure is AssertException exception)
+                    if (!exception.HasCompactRepresentations)
+                        LaunchDiffTool(exception);
+            }
+
+            static void LaunchDiffTool(AssertException exception)
+            {
+                var tempPath = Path.GetTempPath();
+                var expectedPath = Path.Combine(tempPath, "expected.txt");
+                var actualPath = Path.Combine(tempPath, "actual.txt");
+
+                if (Try(() => DiffCommand(expectedPath, actualPath), out var diffCommand))
+                {
+                    File.WriteAllText(expectedPath, exception.Expected);
+                    File.WriteAllText(actualPath, exception.Actual);
+
+                    using (Process.Start("cmd", $"/c \"{diffCommand}\""))  {  }
+                }
+            }
+
+            static string? DiffCommand(string expectedPath, string actualPath)
+            {
+                var gitconfig = Path.Combine(GetFolderPath(SpecialFolder.UserProfile), ".gitconfig");
+
+                if (!File.Exists(gitconfig))
+                    return null;
+
+                return File.ReadAllLines(gitconfig)
+                    .SkipWhile(x => !x.StartsWith("[difftool "))
+                    .Skip(1)
+                    .TakeWhile(x => !x.StartsWith("["))
+                    .Select(x => x.Split(new[] {'='}, 2))
+                    .Where(x => x[0].Trim() == "cmd")
+                    .Select(x => x[1].Trim()
+                        .Replace("\\\"", "\"")
+                        .Replace("$LOCAL", expectedPath)
+                        .Replace("$REMOTE", actualPath))
+                    .SingleOrDefault();
+            }
+        }
+    }
+}
--- src/Fixie/Internal/BehaviorDiscoverer.cs
+++ src/Fixie/Internal/BehaviorDiscoverer.cs
@@ -17,77 +17,43 @@
 
         public IDiscovery GetDiscovery()
         {
-            return (IDiscovery) Construct(DiscoveryType());
+            return GetConfiguration().Convention.Discovery;
         }
 
         public void GetBehaviors(out IDiscovery discovery, out IExecution execution)
         {
-            var discoveryType = DiscoveryType();
-            var executionType = ExecutionType();
+            var convention = GetConfiguration().Convention;
 
-            discovery = (IDiscovery) Construct(discoveryType);
-            execution = discoveryType == executionType
-                ? (IExecution) discovery
-                : (IExecution) Construct(executionType);
+            discovery = convention.Discovery;
+            execution = convention.Execution;
         }
 
-        Type DiscoveryType()
+        Configuration GetConfiguration()
         {
-            var customDiscoveryTypes = assembly
+            var customConfigurationTypes = assembly
                 .GetTypes()
-                .Where(type => IsDiscovery(type) && !type.IsAbstract)
+                .Where(type => type.IsSubclassOf(typeof(Configuration)) && !type.IsAbstract)
                 .ToArray();
 
-            if (assembly.GetName().Name == "Fixie.Tests")
-                customDiscoveryTypes = customDiscoveryTypes
-                    .Where(type => type.Name == "PrimaryConvention")
-                    .ToArray();
-
-            if (customDiscoveryTypes.Length > 1)
+            if (customConfigurationTypes.Length > 1)
             {
                 throw new Exception(
-                    "A test assembly can have at most one IDiscovery implementation, " +
+                    "A test assembly can have at most one Configuration implementation, " +
                     "but the following implementations were discovered:" + Environment.NewLine +
                     string.Join(Environment.NewLine,
-                        customDiscoveryTypes
+                        customConfigurationTypes
                             .Select(x => $"\t{x.FullName}")));
             }
 
-            return customDiscoveryTypes.SingleOrDefault() ?? typeof(DefaultDiscovery);
+            var configurationType = customConfigurationTypes.SingleOrDefault();
+            
+            if (configurationType is null)
+                return new DefaultConfiguration();
+
+            return (Configuration) ConstructWithOptionalContext(configurationType);
         }
 
-        Type ExecutionType()
-        {
-            var customExecutionTypes = assembly
-                .GetTypes()
-                .Where(type => IsExecution(type) && !type.IsAbstract)
-                .ToArray();
-
-            if (assembly.GetName().Name == "Fixie.Tests")
-                customExecutionTypes = customExecutionTypes
-                    .Where(type => type.Name == "PrimaryConvention")
-                    .ToArray();
-
-            if (customExecutionTypes.Length > 1)
-            {
-                throw new Exception(
-                    "A test assembly can have at most one IExecution implementation, " +
-                    "but the following implementations were discovered:" + Environment.NewLine +
-                    string.Join(Environment.NewLine,
-                        customExecutionTypes
-                            .Select(x => $"\t{x.FullName}")));
-            }
-
-            return customExecutionTypes.SingleOrDefault() ?? typeof(DefaultExecution);
-        }
-
-        static bool IsDiscovery(Type type)
-            => type.GetInterfaces().Contains(typeof(IDiscovery));
-
-        static bool IsExecution(Type type)
-            => type.GetInterfaces().Contains(typeof(IExecution));
-
-        object Construct(Type type)
+        object ConstructWithOptionalContext(Type type)
         {
             try
             {

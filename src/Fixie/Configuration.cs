03b7d8fbf07ca718a99dbdd1d174ecfb97ae2fcf
Patrick Lioi <patrick.lioi@gmail.com>
2021-07-25 16:46:46 -0500

    Configuration exposes an appendable collection of reports.

--- src/Fixie/Configuration.cs
+++ src/Fixie/Configuration.cs
@@ -3,5 +3,6 @@
     public abstract class Configuration
     {
         public ConventionCollection Conventions { get; } = new ConventionCollection();
+        public ReportCollection Reports { get; } = new ReportCollection();
     }
 }
---
+++ src/Fixie/ReportCollection.cs
@@ -0,0 +1,13 @@
+﻿namespace Fixie
+{
+    using System.Collections.Generic;
+    using Reports;
+
+    public class ReportCollection
+    {
+        internal List<IReport> Items { get; } = new List<IReport>();
+
+        public void Add(IReport report)
+            => Items.Add(report);
+    }
+}

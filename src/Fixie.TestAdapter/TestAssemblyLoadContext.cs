964859ee4144cbf649de0815b1015186adefce23
Patrick Lioi <patrick.lioi@gmail.com>
2021-08-23 21:14:04 -0500

    Revert "Remove TestAssemblyLoadContext now that it is no longer used."
    This reverts commit 297694b503d65bb6676032b92ae5a836f8aab5b1.

---
+++ src/Fixie.TestAdapter/TestAssemblyLoadContext.cs
@@ -0,0 +1,49 @@
+﻿namespace Fixie.TestAdapter
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.Loader;
+
+    class TestAssemblyLoadContext : AssemblyLoadContext
+    {
+        readonly AssemblyDependencyResolver resolver;
+
+        public TestAssemblyLoadContext(string testAssemblyPath)
+        {
+            resolver = new AssemblyDependencyResolver(testAssemblyPath);
+        }
+
+        protected override Assembly? Load(AssemblyName assemblyName)
+        {
+            if (assemblyName.Name == "Fixie")
+            {
+                // The Fixie assembly itself must be loaded only
+                // into the AssemblyLoadContext.Default context,
+                // so that the Test Adapter can share types with
+                // those loaded by the test assembly such as IReport,
+                // Handler<T>, IExecution, and IDiscovery. Otherwise,
+                // both contexts would have distinct occurrences of
+                // those types, and they would be mistaken as unrelated.
+            
+                return null;
+            }
+
+            var assemblyPath = resolver.ResolveAssemblyToPath(assemblyName);
+
+            if (assemblyPath != null)
+                return LoadFromAssemblyPath(assemblyPath);
+
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            var libraryPath = resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+
+            if (libraryPath != null)
+                return LoadUnmanagedDllFromPath(libraryPath);
+
+            return IntPtr.Zero;
+        }
+    }
+}

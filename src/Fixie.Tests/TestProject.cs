682de92bf12b9f7b4d9286ff849451bae52ca26b
Patrick Lioi <patrick.lioi@gmail.com>
2022-10-04 12:54:38 -0500

    Revert temporary changes which only served to prove that the substantive change of this branch works as intended in a build environment.

--- src/Fixie.Tests/TestProject.cs
+++ src/Fixie.Tests/TestProject.cs
@@ -1,27 +1,11 @@
 ﻿namespace Fixie.Tests
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-
-    class BrokenDiscovery : IDiscovery
-    {
-        public IEnumerable<Type> TestClasses(IEnumerable<Type> concreteClasses)
-            => concreteClasses.Where(x => false);
-
-        public IEnumerable<MethodInfo> TestMethods(IEnumerable<MethodInfo> publicMethods)
-            => publicMethods.Where(x => false);
-    }
-
     class TestProject : ITestProject
     {
         public void Configure(TestConfiguration configuration, TestEnvironment environment)
         {
             if (environment.IsDevelopment())
                 configuration.Reports.Add<DiffToolReport>();
-
-            configuration.Conventions.Add<BrokenDiscovery, DefaultExecution>();
         }
     }
 }

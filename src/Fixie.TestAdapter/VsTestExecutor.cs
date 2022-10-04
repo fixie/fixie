86e4943d7b8eadab9775bccabe11ea55ad7e24df
Patrick Lioi <patrick.lioi@gmail.com>
2022-10-04 14:34:16 -0500

    Document surprising VsTest behavior in the event the user is running all available tests:
    See https://github.com/microsoft/vstest/issues/2474

--- src/Fixie.TestAdapter/VsTestExecutor.cs
+++ src/Fixie.TestAdapter/VsTestExecutor.cs
@@ -21,7 +21,15 @@
         public static readonly Uri Uri = new Uri(Id);
 
         /// <summary>
-        /// Called when running all tests.
+        /// This method was originally intended to be called by VsTest when running
+        /// all tests. However, VsTest no longer appears to call this method, instead
+        /// favoring its overload with all individual tests specified as if they were
+        /// all selected by the user. The stated reason is for performance, but in
+        /// fact requires far larger messages to be passed between processes and far
+        /// more cross-referencing of test names within each specific test framework
+        /// at execution time. This overload is maintained optimistically and for
+        /// protection in the event that VsTest changes back to the more efficient
+        /// approach.
         /// </summary>
         public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
         {
@@ -59,7 +67,14 @@
         }
 
         /// <summary>
-        /// Called when running selected tests.
+        /// This method was originally intended to be called by VsTest only when running
+        /// a selected subset of previously discovered tests. However, VsTest appears to
+        /// call this method even in the event the user is running *all* tests, with all
+        /// individual tests specified in the input as if all were individually selected
+        /// by the user. The stated reason is for performance, but in fact requires far
+        /// larger messages to be passed between processes and far more cross-referencing
+        /// of test names within each specific test framework at execution time. Still,
+        /// this overload is functionally correct even when all tests are passed to it.
         /// </summary>
         public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
         {

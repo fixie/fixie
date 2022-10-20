274e7bd91e96015b2bd1962410b085327a01a698
Patrick Lioi <patrick.lioi@gmail.com>
2022-10-20 14:42:32 -0500

    Inline Serialization methods which were only used by other public methods in the class.

--- src/Fixie/Internal/Serialization.cs
+++ src/Fixie/Internal/Serialization.cs
@@ -7,15 +7,9 @@
     static class Serialization
     {
         public static string Serialize<TMessage>(TMessage message)
-            => Encoding.UTF8.GetString(SerializeToBytes(message));
-
-        public static byte[] SerializeToBytes<TMessage>(TMessage message)
-            => JsonSerializer.SerializeToUtf8Bytes(message);
+            => Encoding.UTF8.GetString(JsonSerializer.SerializeToUtf8Bytes(message));
 
         public static TMessage Deserialize<TMessage>(string message)
-            => Deserialize<TMessage>(Encoding.UTF8.GetBytes(message));
-
-        public static TMessage Deserialize<TMessage>(byte[] bytes)
-            => JsonSerializer.Deserialize<TMessage>(new ReadOnlySpan<byte>(bytes))!;
+            => JsonSerializer.Deserialize<TMessage>(new ReadOnlySpan<byte>(Encoding.UTF8.GetBytes(message)))!;
     }
 }

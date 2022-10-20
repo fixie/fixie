0ff53e0f2d7de5c57d1af98c2c1b5786b4433e4d
Patrick Lioi <patrick.lioi@gmail.com>
2022-10-20 15:10:31 -0500

    Avoid performing byte[ ] / string transformations explicitly when interacting with JsonSerializer, since the string overloads accomplish the same thing while giving JsonSerializer's implementation the opportunity for fine tuning performance. All usages of our Serialization deal with strings directly so there is not a realistic opportunity to remain in byte[ ] end to end.

--- src/Fixie/Internal/Serialization.cs
+++ src/Fixie/Internal/Serialization.cs
@@ -1,14 +1,13 @@
 ﻿namespace Fixie.Internal
 {
-    using System.Text;
     using System.Text.Json;
 
     static class Serialization
     {
         public static string Serialize<TMessage>(TMessage message)
-            => Encoding.UTF8.GetString(JsonSerializer.SerializeToUtf8Bytes(message));
+            => JsonSerializer.Serialize(message);
 
         public static TMessage Deserialize<TMessage>(string message)
-            => JsonSerializer.Deserialize<TMessage>(Encoding.UTF8.GetBytes(message))!;
+            => JsonSerializer.Deserialize<TMessage>(message)!;
     }
 }

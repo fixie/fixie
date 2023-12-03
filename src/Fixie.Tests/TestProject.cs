13fc7aff844fa35ec5adecf36c05c3b9c53d4ee3
Patrick Lioi <patrick.lioi@gmail.com>
2023-12-03 13:20:52 -0600

    Reveal that end users cannot yet fully replicate the test.RunCore(...) logic, by attempting to with a custom IExecution. Because the user cannot explicitly emit the TestStarted event, long running tests no longer display a spinner.

--- src/Fixie.Tests/TestProject.cs
+++ src/Fixie.Tests/TestProject.cs
@@ -1,13 +1,46 @@
-﻿namespace Fixie.Tests
+﻿using System.Threading.Tasks;
+
+namespace Fixie.Tests
 {
+    using System;
+
     class TestProject : ITestProject
     {
         public void Configure(TestConfiguration configuration, TestEnvironment environment)
         {
+            configuration.Conventions.Add<DefaultDiscovery, CustomExecution>();
+
             if (environment.IsDevelopment())
                 configuration.Reports.Add<DiffToolReport>();
             else
                 configuration.Reports.Add(new GitHubReport(environment));
+        }
+
+        class CustomExecution : IExecution
+        {
+            public async Task Run(TestSuite testSuite)
+            {
+                foreach (var testClass in testSuite.TestClasses)
+                {
+                    foreach (var test in testClass.Tests)
+                    {
+                        //TODO: await test.Start();
+
+                        try
+                        {
+                            var instance = testClass.Construct();
+
+                            await test.Method.Call(instance);
+
+                            await test.Pass();
+                        }
+                        catch (Exception failureReason)
+                        {
+                            await test.Fail(failureReason);
+                        }
+                    }
+                }
+            }
         }
     }
 }

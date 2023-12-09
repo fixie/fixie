b03da8f608617788d135dd7e4136f7e7982c644e
Patrick Lioi <patrick.lioi@gmail.com>
2023-12-08 20:37:14 -0600

    Whitespace corrections for lines left too far indended during automated refactoring.

--- src/Fixie.Tests/TestAdapter/SourceLocationSamples.cs
+++ src/Fixie.Tests/TestAdapter/SourceLocationSamples.cs
@@ -9,58 +9,58 @@ public class SourceLocationSamples
 
     public void Empty_TwoLines()
     { // Debug = 11
-        } // Release = 12
+    } // Release = 12
 
     public void Empty_ThreeLines()
     { // Debug = 15
 
-        } // Release = 17
+    } // Release = 17
 
     public void Simple()
     { // Debug = 20
-            int answer = 42; // Release = 21
-            Console.Write(answer);
-        }
+        int answer = 42; // Release = 21
+        Console.Write(answer);
+    }
 
     public void Generic<T>(T x)
     { // Debug = 26
-            Console.WriteLine(); // Release = 27
-        }
+        Console.WriteLine(); // Release = 27
+    }
 
     public async void AsyncMethod_Void()
     { // Debug = 31
-            int answer = 42; // Release = 32
-            await Task.Delay(0);
-            Console.Write(answer);
-        }
+        int answer = 42; // Release = 32
+        await Task.Delay(0);
+        Console.Write(answer);
+    }
 
     public async Task AsyncMethod_Task()
     { // Debug = 38
-            int answer = 42; // Release = 39
-            await Task.Delay(0);
-            Console.Write(answer);
-        }
+        int answer = 42; // Release = 39
+        await Task.Delay(0);
+        Console.Write(answer);
+    }
 
     public async Task<int> AsyncMethod_TaskOfT()
     { // Debug = 45
-            int answer = 42; // Release = 46
-            await Task.Delay(0);
-            return answer;
-        }
+        int answer = 42; // Release = 46
+        await Task.Delay(0);
+        return answer;
+    }
 
     public class NestedClass
     {
         public void NestedMethod()
         { // Debug = 54
-                int answer = 42; // Release = 55
-                Console.Write(answer);
-            }
+            int answer = 42; // Release = 55
+            Console.Write(answer);
+        }
     }
 
 #line hidden
     public void Hidden()
     {
-        }
+    }
 #line default
 
     public void Overloaded() { } // Debug = Release = 66

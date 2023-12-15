ab8f382b2079fe511f3fae3a09c51863219a95f5
Patrick Lioi <patrick.lioi@gmail.com>
2023-12-14 20:52:44 -0600

    Remove now-unused using statements from Fixie.Main.cs.

--- build/Fixie.Main.cs
+++ build/Fixie.Main.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-
-// The 'Fixie' package includes this file in test projects so
+﻿// The 'Fixie' package includes this file in test projects so
 // that their tests can be executed. Do not modify this file.
 
 return await Fixie.Internal.EntryPoint.Main(typeof(Program).Assembly, customArguments: args);

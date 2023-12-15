0d8931d00afefeef28ceb7af34f8b67e2239fcd2
Patrick Lioi <patrick.lioi@gmail.com>
2023-12-14 20:52:05 -0600

    Switch Fixie.Main.cs to use top level statements.

--- build/Fixie.Main.cs
+++ build/Fixie.Main.cs
@@ -4,10 +4,4 @@ using System.Threading.Tasks;
 // The 'Fixie' package includes this file in test projects so
 // that their tests can be executed. Do not modify this file.
 
-file class Program
-{
-    static async Task<int> Main(string[] args)
-    {
-        return await Fixie.Internal.EntryPoint.Main(typeof(Program).Assembly, customArguments: args);
-    }
-}
+return await Fixie.Internal.EntryPoint.Main(typeof(Program).Assembly, customArguments: args);

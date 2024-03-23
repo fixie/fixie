715d62ab8f88872ca47f66f79f7f18b4f1e37d4c
Patrick Lioi <patrick.lioi@gmail.com>
2024-03-23 12:43:40 -0500

    Allow the self testing convention to opt into running tests in parallel.
    The option is left disabled, though, because parallel execution is not yet supported. Console runs would show inconsistent results including unintended changes in console color mid-line along with many unexpected test failures, and Test Explorer runs crash early while attempting to parse interleaved JSON messages sent between processes.

--- src/Fixie.Tests/TestProject.cs
+++ src/Fixie.Tests/TestProject.cs
@@ -4,9 +4,28 @@ class TestProject : ITestProject
 {
     public void Configure(TestConfiguration configuration, TestEnvironment environment)
     {
+        bool enableParallelism = false;
+
+        if (enableParallelism)
+            configuration.Conventions.Add<DefaultDiscovery, ParallelExecution>();
+        else
+            configuration.Conventions.Add<DefaultDiscovery, DefaultExecution>();
+
         if (environment.IsDevelopment())
             configuration.Reports.Add<DiffToolReport>();
         else
             configuration.Reports.Add(new GitHubReport(environment));
+    }
+
+    class ParallelExecution : IExecution
+    {
+        public async Task Run(TestSuite testSuite)
+        {
+            await Parallel.ForEachAsync(testSuite.TestClasses, async (testClass, cancellationToken) =>
+            {
+                foreach (var test in testClass.Tests)
+                    await test.Run();
+            });
+        }
     }
 }

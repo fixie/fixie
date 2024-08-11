2fca9bdc6bfe5d0b53df354a4255f3379fbd8059
Patrick Lioi <patrick.lioi@gmail.com>
2024-08-10 20:37:52 -0500

    Test coverage for numeric and other IEquatable<T> assertions.

--- src/Fixie.Tests/Assertions/AssertionTests.cs
+++ src/Fixie.Tests/Assertions/AssertionTests.cs
@@ -15,6 +15,70 @@ public class AssertionTests
         Contradiction(false, x => x.ShouldBe(true), "x should be true but was false");
     }
 
+    public void ShouldAssertIntegralNumbers()
+    {
+        sbyte.MinValue.ShouldBe(sbyte.MinValue);
+        sbyte.MaxValue.ShouldBe(sbyte.MaxValue);
+        Contradiction((sbyte)1, x => x.ShouldBe((sbyte)2), "x should be 2 but was 1");
+        
+        byte.MinValue.ShouldBe(byte.MinValue);
+        byte.MaxValue.ShouldBe(byte.MaxValue);
+        Contradiction((byte)2, x => x.ShouldBe((byte)3), "x should be 3 but was 2");
+        
+        short.MinValue.ShouldBe(short.MinValue);
+        short.MaxValue.ShouldBe(short.MaxValue);
+        Contradiction((short)3, x => x.ShouldBe((short)4), "x should be 4 but was 3");
+        
+        ushort.MinValue.ShouldBe(ushort.MinValue);
+        ushort.MaxValue.ShouldBe(ushort.MaxValue);
+        Contradiction((ushort)4, x => x.ShouldBe((ushort)5), "x should be 5 but was 4");
+
+        int.MinValue.ShouldBe(int.MinValue);
+        int.MaxValue.ShouldBe(int.MaxValue);
+        Contradiction((int)5, x => x.ShouldBe((int)6), "x should be 6 but was 5");
+
+        uint.MinValue.ShouldBe(uint.MinValue);
+        uint.MaxValue.ShouldBe(uint.MaxValue);
+        Contradiction((uint)6, x => x.ShouldBe((uint)7), "x should be 7 but was 6");
+        
+        long.MinValue.ShouldBe(long.MinValue);
+        long.MaxValue.ShouldBe(long.MaxValue);
+        Contradiction((long)7, x => x.ShouldBe((long)8), "x should be 8 but was 7");
+
+        ulong.MinValue.ShouldBe(ulong.MinValue);
+        ulong.MaxValue.ShouldBe(ulong.MaxValue);
+        Contradiction((ulong)8, x => x.ShouldBe((ulong)9), "x should be 9 but was 8");
+
+        nint.MinValue.ShouldBe(nint.MinValue);
+        nint.MaxValue.ShouldBe(nint.MaxValue);
+        Contradiction((nint)9, x => x.ShouldBe((nint)10), "x should be 10 but was 9");
+
+        nuint.MinValue.ShouldBe(nuint.MinValue);
+        nuint.MaxValue.ShouldBe(nuint.MaxValue);
+        Contradiction((nuint)10, x => x.ShouldBe((nuint)11), "x should be 11 but was 10");
+    }
+
+    public void ShouldAssertFractionalNumbers()
+    {
+        decimal.MinValue.ShouldBe(decimal.MinValue);
+        decimal.MaxValue.ShouldBe(decimal.MaxValue);
+        Contradiction((decimal)1, x => x.ShouldBe((decimal)2), "x should be 2 but was 1");
+
+        double.MinValue.ShouldBe(double.MinValue);
+        double.MaxValue.ShouldBe(double.MaxValue);
+        Contradiction((double)2, x => x.ShouldBe((double)3), "x should be 3 but was 2");
+
+        float.MinValue.ShouldBe(float.MinValue);
+        float.MaxValue.ShouldBe(float.MaxValue);
+        Contradiction((float)3, x => x.ShouldBe((float)4), "x should be 4 but was 3");
+    }
+
+    public void ShouldAssertEquatables()
+    {
+        HttpMethod.Post.ShouldBe(HttpMethod.Post);
+        Contradiction(HttpMethod.Post, x => x.ShouldBe(HttpMethod.Get), "x should be GET but was POST");
+    }
+
     public void ShouldAssertChars()
     {
         'a'.ShouldBe('a');
@@ -94,30 +158,6 @@ public class AssertionTests
         }
     }
 
-    static IEnumerable<char> UnicodeEscapedCharacters()
-    {
-        // Code points from \u0000 to \u001F, \u007F, and from \u0080 to \u009F are
-        // "control characters". Some of these have single-character escape sequences
-        // like '\u000A' being equivalent to '\n'. When we omit code points better
-        // served by single-character escape sequences, we are left with those deserving
-        // '\uHHHH' hex escape sequences.
-
-        for (char c = '\u0001'; c <= '\u0006'; c++) yield return c;
-        for (char c = '\u000E'; c <= '\u001F'; c++) yield return c;
-        yield return '\u007F';
-        for (char c = '\u0080'; c <= '\u009F'; c++) yield return c;
-
-        // Several code points represent various kinds of whitespace. Some of these have
-        // single-character escape sequences like '\u0009' being equivalent to '\t', and
-        // the single space character ' ' is naturally represented with no need for any
-        // escape sequence. All other whitespace code points deserve '\uHHHH' hex escape
-        // sequences.
-
-        foreach (char c in (char[])['\u0085', '\u00A0', '\u1680']) yield return c;
-        for (char c = '\u2000'; c <= '\u2009'; c++) yield return c;
-        foreach (char c in (char[])['\u200A', '\u2028', '\u2029', '\u202F', '\u205F', '\u3000']) yield return c;
-    }
-
     static void Contradiction<T>(T actual, Action<T> shouldThrow, string expectedMessage, [CallerArgumentExpression(nameof(shouldThrow))] string? assertion = null)
     {
         try
@@ -150,5 +190,29 @@ public class AssertionTests
             $"\t{assertion}{Line}" +
             $"The actual value in question was:{Line}" +
             $"\t{actual}");
+    }
+
+    static IEnumerable<char> UnicodeEscapedCharacters()
+    {
+        // Code points from \u0000 to \u001F, \u007F, and from \u0080 to \u009F are
+        // "control characters". Some of these have single-character escape sequences
+        // like '\u000A' being equivalent to '\n'. When we omit code points better
+        // served by single-character escape sequences, we are left with those deserving
+        // '\uHHHH' hex escape sequences.
+
+        for (char c = '\u0001'; c <= '\u0006'; c++) yield return c;
+        for (char c = '\u000E'; c <= '\u001F'; c++) yield return c;
+        yield return '\u007F';
+        for (char c = '\u0080'; c <= '\u009F'; c++) yield return c;
+
+        // Several code points represent various kinds of whitespace. Some of these have
+        // single-character escape sequences like '\u0009' being equivalent to '\t', and
+        // the single space character ' ' is naturally represented with no need for any
+        // escape sequence. All other whitespace code points deserve '\uHHHH' hex escape
+        // sequences.
+
+        foreach (char c in (char[])['\u0085', '\u00A0', '\u1680']) yield return c;
+        for (char c = '\u2000'; c <= '\u2009'; c++) yield return c;
+        foreach (char c in (char[])['\u200A', '\u2028', '\u2029', '\u202F', '\u205F', '\u3000']) yield return c;
     }
 }

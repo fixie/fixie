e524cef354f4b3b899864aedb72da66c2177c97b
Patrick Lioi <patrick.lioi@gmail.com>
2024-08-17 22:31:14 -0500

    Remove deprecated AppVeyor integration.
    Users who wish to restore the integration can do so with a custom report equivalent to the one removed in his commit, though the plain ConsoleReport should be sufficient in this environment.
    
    AppVeyor has never extended their test results API beyond their Windows images, which we take to be a vote of no confidence in the relevance of the feature. Fixie focuses on cross platform .NET target frameworks, so non-Windows builds are recommended for speed and compute resource reasons.

--- src/Fixie/TestEnvironment.cs
+++ src/Fixie/TestEnvironment.cs
@@ -68,12 +68,11 @@ public class TestEnvironment
 
     /// <summary>
     /// Returns true when running in a recognized Continuous Integration environment:
-    /// AppVeyor, Azure DevOps, GitHub Actions, or TeamCity.
+    /// Azure DevOps, GitHub Actions, or TeamCity.
     /// </summary>
     public bool IsContinuousIntegration()
     {
         return 
-            GetEnvironmentVariable("APPVEYOR") == "True" ||          // AppVeyor
             GetEnvironmentVariable("TF_BUILD") == "True" ||          // Azure DevOps
             GetEnvironmentVariable("GITHUB_ACTIONS") == "true" ||    // GitHub Actions
             GetEnvironmentVariable("TEAMCITY_PROJECT_NAME") != null; // TeamCity

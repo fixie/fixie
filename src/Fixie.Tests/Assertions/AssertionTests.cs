67769a040ebcc7e23b6bcfa1df2aab7afbdb9eb0
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-10 18:03:52 -0500

    Extract assertions-testing utility methods to a dedicated helper class, so that AssertionTests can begin to be split up by topic.

--- src/Fixie.Tests/Assertions/AssertionTests.cs
+++ src/Fixie.Tests/Assertions/AssertionTests.cs
@@ -1,14 +1,10 @@
-﻿using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using static System.Environment;
+﻿using static Fixie.Tests.Assertions.Utility;
 using static Fixie.Tests.Utility;
 
 namespace Fixie.Tests.Assertions;
 
 public class AssertionTests
 {
-    static readonly string Line = NewLine + NewLine;
-
     public void ShouldAssertBools()
     {
         true.ShouldBe(true);
@@ -390,7 +386,7 @@ public class AssertionTests
     {
         typeof(int).ShouldBe(typeof(int));
         typeof(char).ShouldBe(typeof(char));
-        Contradiction(typeof(Utility), x => x.ShouldBe(typeof(AssertionTests)), "x should be typeof(Fixie.Tests.Assertions.AssertionTests) but was typeof(Fixie.Tests.Utility)");
+        Contradiction(typeof(Utility), x => x.ShouldBe(typeof(AssertionTests)), "x should be typeof(Fixie.Tests.Assertions.AssertionTests) but was typeof(Fixie.Tests.Assertions.Utility)");
         Contradiction(typeof(bool), x => x.ShouldBe(typeof(AssertionTests)), "x should be typeof(Fixie.Tests.Assertions.AssertionTests) but was typeof(bool)");
         Contradiction(typeof(sbyte), x => x.ShouldBe(typeof(AssertionTests)), "x should be typeof(Fixie.Tests.Assertions.AssertionTests) but was typeof(sbyte)");
         Contradiction(typeof(byte), x => x.ShouldBe(typeof(AssertionTests)), "x should be typeof(Fixie.Tests.Assertions.AssertionTests) but was typeof(byte)");
@@ -698,74 +694,6 @@ public class AssertionTests
     {
         public override string ToString() => $"Sample {name}";
     }
-
-    static void Contradiction<T>(T actual, Action<T> shouldThrow, string expectedMessage, [CallerArgumentExpression(nameof(shouldThrow))] string? assertion = null)
-    {
-        try
-        {
-            shouldThrow(actual);
-        }
-        catch (Exception exception)
-        {
-            ShouldBeAssertException(actual, expectedMessage, assertion, exception);
-            return;
-        }
-
-        ShouldHaveFailedAssertion(actual, assertion);
-
-        throw new UnreachableException();
-    }
-
-    static async Task Contradiction<T>(T actual, Func<T, Task> shouldThrowAsync, string expectedMessage, [CallerArgumentExpression(nameof(shouldThrowAsync))] string? assertion = null)
-    {
-        try
-        {
-            await shouldThrowAsync(actual);
-        }
-        catch (Exception exception)
-        {
-            ShouldBeAssertException(actual, expectedMessage, assertion, exception);
-            return;
-        }
-
-        ShouldHaveFailedAssertion(actual, assertion);
-
-        throw new UnreachableException();
-    }
-
-    static void ShouldBeAssertException<T>(T actual, string expectedMessage, string? assertion, Exception exception)
-    {
-        if (exception is AssertException)
-        {
-            if (exception.Message != expectedMessage)
-                throw new Exception(
-                    $"An example assertion failed as expected, but with the wrong message.{Line}" +
-                    $"Expected Message:{Line}{Indent(expectedMessage)}{Line}" +
-                    $"Actual Message:{Line}{Indent(exception.Message)}");
-
-            return;
-        }
-
-        throw new Exception(
-            $"An example assertion failed as expected, but with the wrong type.{Line}" +
-            $"\t{assertion}{Line}" +
-            $"The actual value in question was:{Line}" +
-            $"\t{actual}{Line}" +
-            $"The assertion threw {exception.GetType().FullName} with message:{Line}" +
-            $"\t{exception.Message}");
-    }
-
-    static void ShouldHaveFailedAssertion<T>(T actual, string? assertion)
-    {
-        throw new Exception(
-            $"An example assertion was expected to fail, but did not:{Line}" +
-            $"\t{assertion}{Line}" +
-            $"The actual value in question was:{Line}" +
-            $"\t{actual}");
-    }
-
-    static string Indent(string multiline) =>
-        string.Join(NewLine, multiline.Split(NewLine).Select(x => $"\t{x}"));
 
     static IEnumerable<char> UnicodeEscapedCharacters()
     {
---
+++ src/Fixie.Tests/Assertions/Utility.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using static System.Environment;
+
+namespace Fixie.Tests.Assertions;
+
+static class Utility
+{
+    static readonly string Line = NewLine + NewLine;
+
+    public static void Contradiction<T>(T actual, Action<T> shouldThrow, string expectedMessage, [CallerArgumentExpression(nameof(shouldThrow))] string? assertion = null)
+    {
+        try
+        {
+            shouldThrow(actual);
+        }
+        catch (Exception exception)
+        {
+            ShouldBeAssertException(actual, expectedMessage, assertion, exception);
+            return;
+        }
+
+        ShouldHaveFailedAssertion(actual, assertion);
+
+        throw new UnreachableException();
+    }
+
+    public static async Task Contradiction<T>(T actual, Func<T, Task> shouldThrowAsync, string expectedMessage, [CallerArgumentExpression(nameof(shouldThrowAsync))] string? assertion = null)
+    {
+        try
+        {
+            await shouldThrowAsync(actual);
+        }
+        catch (Exception exception)
+        {
+            ShouldBeAssertException(actual, expectedMessage, assertion, exception);
+            return;
+        }
+
+        ShouldHaveFailedAssertion(actual, assertion);
+
+        throw new UnreachableException();
+    }
+
+    static void ShouldBeAssertException<T>(T actual, string expectedMessage, string? assertion, Exception exception)
+    {
+        if (exception is AssertException)
+        {
+            if (exception.Message != expectedMessage)
+                throw new Exception(
+                    $"An example assertion failed as expected, but with the wrong message.{Line}" +
+                    $"Expected Message:{Line}{Indent(expectedMessage)}{Line}" +
+                    $"Actual Message:{Line}{Indent(exception.Message)}");
+
+            return;
+        }
+
+        throw new Exception(
+            $"An example assertion failed as expected, but with the wrong type.{Line}" +
+            $"\t{assertion}{Line}" +
+            $"The actual value in question was:{Line}" +
+            $"\t{actual}{Line}" +
+            $"The assertion threw {exception.GetType().FullName} with message:{Line}" +
+            $"\t{exception.Message}");
+    }
+
+    static void ShouldHaveFailedAssertion<T>(T actual, string? assertion)
+    {
+        throw new Exception(
+            $"An example assertion was expected to fail, but did not:{Line}" +
+            $"\t{assertion}{Line}" +
+            $"The actual value in question was:{Line}" +
+            $"\t{actual}");
+    }
+
+    static string Indent(string multiline) =>
+        string.Join(NewLine, multiline.Split(NewLine).Select(x => $"\t{x}"));
+}

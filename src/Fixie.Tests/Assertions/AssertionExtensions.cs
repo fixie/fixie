1c9aefb3dc1be95312e7e47c62a99c5fabbc6a8a
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-14 12:43:04 -0500

    Move custom assertions ShouldSatisfy and ShouldBeGenericType to the root of the Fixie.Tests project, so they are no longer colocated with methods deprecated by Fixie.Assertions.

--- src/Fixie.Tests/Assertions/AssertionExtensions.cs
+++ src/Fixie.Tests/Assertions/AssertionExtensions.cs
@@ -142,27 +142,6 @@ public static class AssertionExtensions
         return expectationBody;
     }
 
-    public static void ShouldSatisfy<T>(this IEnumerable<T> actual, Action<T>[] itemExpectations, [CallerArgumentExpression(nameof(actual))] string? expression = null)
-    {
-        var actualItems = actual.ToArray();
-
-        if (actualItems.Length != itemExpectations.Length)
-            throw new AssertException(
-                expression,
-                $"{itemExpectations.Length} items",
-                $"{actualItems.Length} items");
-
-        for (var i = 0; i < actualItems.Length; i++)
-            itemExpectations[i](actualItems[i]);
-    }
-
-    public static void ShouldBeGenericTypeParameter(this Type actual, string expectedName, [CallerArgumentExpression(nameof(actual))] string? expression = null)
-    {
-        actual.IsGenericParameter.ShouldBe(true);
-        actual.FullName.ShouldBe(null);
-        actual.Name.ShouldBe(expectedName);
-    }
-
     public static void ShouldNotBeNull([NotNull] this object? actual, [CallerArgumentExpression(nameof(actual))] string? expression = null)
     {
         if (actual == null)
---
+++ src/Fixie.Tests/CustomAssertions.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+using Fixie.Tests.Assertions;
+
+namespace Fixie.Tests;
+
+public static class CustomAssertions
+{
+    public static void ShouldSatisfy<T>(this IEnumerable<T> actual, Action<T>[] itemExpectations, [CallerArgumentExpression(nameof(actual))] string? expression = null)
+    {
+        var actualItems = actual.ToArray();
+
+        if (actualItems.Length != itemExpectations.Length)
+            throw new AssertException(
+                expression,
+                $"{itemExpectations.Length} items",
+                $"{actualItems.Length} items");
+
+        for (var i = 0; i < actualItems.Length; i++)
+            itemExpectations[i](actualItems[i]);
+    }
+
+    public static void ShouldBeGenericTypeParameter(this Type actual, string expectedName, [CallerArgumentExpression(nameof(actual))] string? expression = null)
+    {
+        actual.IsGenericParameter.ShouldBe(true);
+        actual.FullName.ShouldBe(null);
+        actual.Name.ShouldBe(expectedName);
+    }
+}

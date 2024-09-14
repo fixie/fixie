4871c39d90fb72d7739e2cc2278bee65c92dc373
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-14 13:01:29 -0500

    Remove unused deprecated assertion extensions replaced by Fixie.Assertions.

--- src/Fixie.Tests/Assertions/AssertionExtensions.cs
+++ src/Fixie.Tests/Assertions/AssertionExtensions.cs
@@ -1,7 +1,4 @@
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
 
 namespace Fixie.Tests.Assertions;
 
@@ -19,12 +16,6 @@ public static class AssertionExtensions
             throw AssertException.ForValues(expression, expected, actual);
     }
     
-    public static void ShouldBe(this char actual, char expected, [CallerArgumentExpression(nameof(actual))] string? expression = null)
-    {
-        if (actual != expected)
-            throw AssertException.ForValues(expression, expected, actual);
-    }
-
     public static void ShouldBe<T>(this IEquatable<T> actual, IEquatable<T> expected, [CallerArgumentExpression(nameof(actual))] string? expression = null)
     {
         if (!actual.Equals(expected))
@@ -53,98 +44,5 @@ public static class AssertionExtensions
         foreach (var (actualItem, expectedItem) in actualArray.Zip(expected))
             if (!Equals(actualItem, expectedItem))
                 throw AssertException.ForLists(expression, expected, actualArray);
-    }
-
-    public static T ShouldBe<T>(this object? actual, [CallerArgumentExpression(nameof(actual))] string? expression = null)
-    {
-        if (actual is T typed)
-            return typed;
-
-        throw AssertException.ForValues(expression, typeof(T), actual?.GetType());
-    }
-
-    public static TException ShouldThrow<TException>(this Action shouldThrow, string expectedMessage, [CallerArgumentExpression(nameof(shouldThrow))] string? expression = null) where TException : Exception
-    {
-        try
-        {
-            shouldThrow();
-        }
-        catch (Exception actual)
-        {
-            return ShouldBeException<TException>(expectedMessage, expression, actual);
-        }
-
-        ShouldHaveThrown<TException>(expression);
-
-        throw new UnreachableException();
-    }
-
-    public static async Task<TException> ShouldThrow<TException>(this Func<Task> shouldThrowAsync, string expectedMessage, [CallerArgumentExpression(nameof(shouldThrowAsync))] string? expression = null) where TException : Exception
-    {
-        try
-        {
-            await shouldThrowAsync();
-        }
-        catch (Exception actual)
-        {
-            return ShouldBeException<TException>(expectedMessage, expression, actual);
-        }
-
-        ShouldHaveThrown<TException>(expression);
-        
-        throw new UnreachableException();
-    }
-
-    static TException ShouldBeException<TException>(string expectedMessage, string? expression, Exception actual) where TException : Exception
-    {
-        if (actual is TException typed)
-        {
-            if (actual.Message != expectedMessage)
-                throw AssertException.ForException<TException>(expression, expectedMessage, actual.Message);
-
-            return typed;
-        }
-
-        throw AssertException.ForException(expression, typeof(TException), expectedMessage, actual.GetType(), actual.Message);
-    }
-
-    static void ShouldHaveThrown<TException>(string? expression) where TException : Exception
-    {
-        var expectedType = typeof(TException).FullName!;
-
-        throw AssertException.ForMessage(expression, expectedType, "no exception was thrown",
-            $"{expression} should have thrown {expectedType} but did not");
-    }
-
-    public static void Should<T>(this T actual, Func<T, bool> expectation, [CallerArgumentExpression(nameof(actual))] string? expression = default!, [CallerArgumentExpression(nameof(expectation))] string? expectationBody = default!)
-    {
-        if (!expectation(actual))
-        {
-            if (expectationBody != null)
-                expectationBody = DropTrivialLambdaPrefix(expectationBody);
-
-            throw new AssertException(expression, expectationBody ?? "null", actual?.ToString() ?? "null");
-        }
-    }
-
-    static string DropTrivialLambdaPrefix(string expectationBody)
-    {
-        var identifierMatch = Regex.Matches(expectationBody, @"^(\w+)");
-
-        if (identifierMatch.Count == 1)
-        {
-            var identifier = identifierMatch[0].Groups[1].Value;
-
-            expectationBody = Regex.Replace(expectationBody,
-                "^" + Regex.Escape(identifier) + @"\s*=>\s*" + Regex.Escape(identifier) + @"\s+", "");
-        }
-
-        return expectationBody;
-    }
-
-    public static void ShouldNotBeNull([NotNull] this object? actual, [CallerArgumentExpression(nameof(actual))] string? expression = null)
-    {
-        if (actual == null)
-            throw AssertException.ForMessage(expression, "not null", "null", $"{expression} should not be null but was null");
     }
 }

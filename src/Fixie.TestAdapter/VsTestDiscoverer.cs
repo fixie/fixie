ce6cfd9fc9722e23440e4010d58a4a82fa11f91c
Patrick Lioi <patrick.lioi@gmail.com>
2024-11-16 18:29:43 -0600

    Drop unreachable code from the VSTest adapter.

--- src/Fixie.TestAdapter/VsTestDiscoverer.cs
+++ src/Fixie.TestAdapter/VsTestDiscoverer.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
-using static Fixie.TestAdapter.TestAssembly;
 
 namespace Fixie.TestAdapter;
 
@@ -12,27 +11,5 @@ class VsTestDiscoverer : ITestDiscoverer
 {
     public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger log, ITestCaseDiscoverySink discoverySink)
     {
-        try
-        {
-            log.Version();
-
-            foreach (var assemblyPath in sources)
-                DiscoverTests(log, discoverySink, assemblyPath);
-        }
-        catch (Exception exception)
-        {
-            throw new RunnerException(exception);
-        }
-    }
-
-    static void DiscoverTests(IMessageLogger log, ITestCaseDiscoverySink discoverySink, string assemblyPath)
-    {
-        if (!IsTestAssembly(assemblyPath))
-        {
-            log.Info("Skipping " + assemblyPath + " because it is not a test assembly.");
-            return;
-        }
-
-        log.Info("Processing " + assemblyPath);
     }
 }
